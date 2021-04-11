0002fdb234a983d6bce092eb6552cef595e02a1a
Deng Yufan <2695375004@qq.com>
2021-04-11 17:18:02 +0800

    Normal

--- CF/cgr13C.cpp
+++ CF/cgr13C.cpp
@@ -3,26 +3,27 @@
 
 using namespace std;
 
-int t, n, h, ans, s[5001], v[5001];
+LL t, n, h, ans, a[5002], f[5002], g[5002];  //a:原数组; f:经过次数; g:前向差分
 
 int main() {
   cin.tie(0), cout.tie(0);
   cin >> t;
   while (t--) {
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-      cin >> s[i];
+    for (LL i = 1; i <= n; i++) {
+      cin >> a[i];
     }
-    fill(&v[1], &v[n] + 1, 0);
-    for (h = 1, ans = 0; s[h] == 1; h++) {
-    }
-    for (; h <= n; h++) {
-      v[h] += s[h] - 1;
-      for (int i = h + 2; i <= h + s[h]; i++) {
-        v[i]++;
+    ans = 0;
+    fill(&f[1], &f[n + 1] + 1, 0);
+    fill(&g[1], &g[n + 1] + 1, 0);
+    for (LL i = 1; i <= n; i++) {
+      f[i] += (g[i] += g[i - 1]);
+      if (f[i] < a[i] - 1) {
+        ans += a[i] - f[i] - 1;
+      } else {
+        f[i + 1] += f[i] - a[i] + 1;
       }
-      v[h + 1] += max(v[h] - s[h] + 1, 0);
-      ans += max(s[h] - v[h] - 1, 0);
+      g[i + 2]++, g[min(a[i] + i, n) + 1]--;
     }
     cout << ans << endl;
   }
