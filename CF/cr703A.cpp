7f57095de9161befe07018d5981bc761786cfe6d
Deng Yufan <2695375004@qq.com>
2021-04-11 17:38:55 +0800

    Normal

--- CF/cr703A.cpp
+++ CF/cr703A.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+LL t, n, h, tot;
+
 int main() {
   cin.tie(0), cout.tie(0);
   std::ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    tot = 0;
+    for (int i = 1; i <= n; i++) {
+      cin >> h;
+      tot += h;
+    }
+    cout << (tot >= n * (n - 1) / 2 ? "YES" : "NO") << endl;
+  }
   return 0;
 }
