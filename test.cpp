1f97c7c4a2a42416263adee2f58a1e07c8ba5e50
Deng Yufan <2695375004@qq.com>
2021-04-14 20:59:04 +0800

    Normal

--- test.cpp
+++ test.cpp
@@ -3,26 +3,9 @@
 
 using namespace std;
 
-int n, sum, m1, m2, m[4];
-
 int main() {
-  cin >> n;
-  if (n % 1111 == 0) {
-    cout << 0;
-    return 0;
-  }
-  while (sum != 6174) {
-    m1 = m2 = 0;
-    for (int i = 0; i < 4; i++) {
-      m[i] = n % 10;
-      n = n / 10;
-    }
-    sort(m, m + 4);
-    for (int i = 0; i < 4; i++) {
-      m1 = 10 * m1 + m[i], m2 = 10 * m2 + m[3 - i];
-    }
-    sum = m2 - m1, n = sum;
-    cout << m2 << " - " << m1 << " = " << sum << endl;
-  }
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
   return 0;
 }
