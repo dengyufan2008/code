61c5d36e9c681161cea1b1900adb0fe82eb75af1
Deng Yufan <2695375004@qq.com>
2021-04-14 12:29:56 +0800

    Normal

--- CF/cr529C.cpp
+++ CF/cr529C.cpp
@@ -3,9 +3,31 @@
 
 using namespace std;
 
+int n, k, m, a[200001];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> k;
+  for (int i = 1; i <= n; i <<= 1) {
+    if (i & n) {
+      a[++m] = i;
+    }
+  }
+  if (k > n || m > k) {
+    cout << "NO";
+    return 0;
+  }
+  cout << "YES\n";
+  for (int i = 1; m < k;) {
+    while (a[i] == 1) {
+      i++;
+    }
+    a[i] /= 2;
+    a[++m] = a[i];
+  }
+  for (int i = 1; i <= m; i++) {
+    cout << a[i] << " ";
+  }
   return 0;
 }
