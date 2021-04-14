afe0627ed30549576c4ae1f75c7e8e095fee155d
Deng Yufan <2695375004@qq.com>
2021-04-14 12:47:03 +0800

    Normal

--- CF/cr529D.cpp
+++ CF/cr529D.cpp
@@ -13,19 +13,6 @@ struct B {
 
 int n, m;
 
-void T(int f, int x, int d) {
-  if(!d) {
-    return;
-  }
-  cout << x << " ";
-  for(int i = v[x].et; i; i = e[i].p) {
-    if(e[i].d != f) {
-      T(x, e[i].d, d - 1);
-      break;
-    }
-  }
-}
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
@@ -35,6 +22,14 @@ int main() {
     e[++m * 2] = {v[x].et, y}, v[x].et = m * 2;
     e[m * 2 + 1] = {v[y].et, x}, v[y].et = m * 2 + 1;
   }
-  T(1, e[v[1].et].d, n);
+  for(int f = 1, x = e[v[1].et].d; n; n--) {
+    cout << x << " ";
+    for(int i = v[x].et; i; i = e[i].p) {
+      if(e[i].d != f) {
+        f = x, x = e[i].d;
+        break;
+      }
+    }
+  }
   return 0;
 }
