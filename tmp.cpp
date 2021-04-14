a41406ba9e36b4e5f210dd43b8ef7f56642c9bd4
Deng Yufan <2695375004@qq.com>
2021-04-14 20:11:51 +0800

    Normal

--- tmp.cpp
+++ tmp.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
-int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+int n, sum, m1, m2, m[4];
+
+int m2in() {
+  cin >> n;
+  if (n % 1111 == 0) {
+    cout << 0;
+    return 0;
+  }
+  while (sum != 6174) {
+    m1 = m2 = 0;
+    for (int i = 0; i < 4; i++) {
+      m[i] = n % 10;
+      n = n / 10;
+    }
+    sort(m, m + 4);
+    for (int i = 0; i < 4; i++) {
+      m1 = 10 * m1 + m[i], m2 = 10 * m2 + m[3 - i];
+    }
+    sum = m2 - m1, n = sum;
+    cout << m2 << " - " << m1 << " = " << sum << endl;
+  }
   return 0;
 }
