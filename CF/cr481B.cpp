97d5340d5f6af6db102e98552da7a8e51341391a
Deng Yufan <2695375004@qq.com>
2021-04-15 20:39:20 +0800

    Normal

--- CF/cr481B.cpp
+++ CF/cr481B.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+int n, num, ans;
+char ch;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> ch;
+    if (ch == 'x') {
+      num++;
+    } else {
+      ans += max(num - 2, 0);
+      num = 0;
+    }
+  }
+  ans += max(num - 2, 0);
+  cout << ans;
   return 0;
 }
