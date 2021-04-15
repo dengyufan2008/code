280f4b2df766a1d6945fd42fd521774713a3c16d
Deng Yufan <2695375004@qq.com>
2021-04-15 20:49:53 +0800

    Normal

--- CF/cr481C.cpp
+++ CF/cr481C.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+LL n, m, a[200001];
+
+void Find(LL x) {
+  LL ans = 0;
+  for (LL i = 1 << 20; i; i >>= 1) {  //倍增比二分好写[doge]
+    if (ans + i <= n && a[ans + i] < x) {
+      ans += i;
+    }
+  }
+  cout << ans + 1 << " " << x - a[ans] << endl;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> m;
+  for (LL i = 1, tmp; i <= n; i++) {
+    cin >> tmp;
+    a[i] = a[i - 1] + tmp;
+  }
+  for (LL i = 1, tmp; i <= m; i++) {
+    cin >> tmp;
+    Find(tmp);
+  }
   return 0;
 }
