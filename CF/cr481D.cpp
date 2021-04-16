4aa18abf13359c8dbdd0fcd3d765966b9c98bf6c
Deng Yufan <2695375004@qq.com>
2021-04-16 17:21:14 +0800

    Normal

--- CF/cr481D.cpp
+++ CF/cr481D.cpp
@@ -8,11 +8,11 @@ int n, ans, a[100001], b[100001];
 bool P(int d) {
   ans = 0;
   copy(&a[3], &a[n] + 1, &b[3]);
-  for (int i = 2, s = 0; i <= n; i++) {
+  for (int i = 2, s = 0; i < n; i++) {
     s = b[i + 1] - b[i] - d;
     if (s >= -1 && s <= 1) {
       ans++;
-      b[i + 1] += s;
+      b[i + 1] -= s;
     } else {
       return false;
     }
@@ -31,7 +31,7 @@ int main() {
     for (int j = -1; j <= 1; j++) {
       b[1] = a[1] + i, b[2] = a[2] + j;
       if (P(b[2] - b[1])) {
-        cout << ans << endl;
+        cout << ans + (i == 0) + (j == 0);
         return 0;
       }
     }
