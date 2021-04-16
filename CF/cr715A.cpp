86cb81270e9d348b7013316a05a71cb85ce739a0
Deng Yufan <2695375004@qq.com>
2021-04-16 23:33:20 +0800

    Normal

--- CF/cr715A.cpp
+++ CF/cr715A.cpp
@@ -3,18 +3,32 @@
 
 using namespace std;
 
-int t, n, a[2001];
+int x, n, h, t, a[2001];
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> t;
-  while (t--) {
+  cin >> x;
+  while (x--) {
     cin >> n;
     for (int i = 1; i <= n; i++) {
       cin >> a[i];
     }
-    
+    h = 1, t = n;
+    while (h <= t) {
+      for (; a[h] % 2; h++) {
+      }
+      for (; !(a[t] % 2); t--) {
+      }
+      if(h <= t) {
+        swap(a[h], a[t]);
+      }
+      h++, t--;
+    }
+    for (int i = 1; i <= n; i++) {
+      cout << a[i] << " ";
+    }
+    cout << endl;
   }
   return 0;
 }
