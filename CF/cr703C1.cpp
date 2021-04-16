5660ba482276d7c4d51b3cc4474540fdad9c7026
Deng Yufan <2695375004@qq.com>
2021-04-16 19:30:42 +0800

    Noraml

--- CF/cr703C1.cpp
+++ CF/cr703C1.cpp
@@ -3,9 +3,49 @@
 
 using namespace std;
 
+int n;
+
+void A(int x) {
+  cout << "! " << x << endl;
+  cout.flush();
+}
+
+int Q(int l, int r) {
+  int s;
+  cout << "? " << l << " " << r << endl;
+  cout.flush();
+  cin >> s;
+  return s;
+}
+
+void F(int l, int r) {
+  if (l == r || l + 1 == r && r == Q(l, r)) {
+    A(l);
+    return;
+  } else if (l + 1 == r) {
+    A(r);
+    return;
+  }
+  int s = Q(l, r), mid = (l + r) / 2;
+  if (l <= s && s <= mid) {
+    if (Q(l, mid) == s) {
+      F(l, mid);
+    } else {
+      F(mid, r);
+    }
+  } else {
+    if (Q(mid, r) == s) {
+      F(mid, r);
+    } else {
+      F(l, mid);
+    }
+  }
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  F(1, n);
   return 0;
 }
