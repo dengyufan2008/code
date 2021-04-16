079ad16334352089d379c74a4c20faaa034a34b2
Deng Yufan <2695375004@qq.com>
2021-04-16 18:50:57 +0800

    Normal

--- CF/cr703B.cpp
+++ CF/cr703B.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+int t, n, x[1001], y[1001];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+      cin >> x[i] >> y[i];
+    }
+    if (n % 2) {
+      cout << 1 << endl;
+    } else {
+      sort(&x[1], &x[n] + 1), sort(&y[1], &y[n] + 1);
+      cout << (x[n / 2 + 1] - x[n / 2] + 1) * (y[n / 2 + 1] - y[n / 2] + 1) << endl;
+    }
+  }
   return 0;
 }
