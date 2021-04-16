26056b76a224912f4ff77fa11cfa2cb19e1584af
Deng Yufan <2695375004@qq.com>
2021-04-16 23:01:29 +0800

    Normal

--- CF/cr715A.cpp
+++ CF/cr715A.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+struct A {
+  int d;
+  bool operator<(const A &a) const {
+    return d % 2 > a.d % 2 || d < a.d;
+  }
+} a[2001];
+
+int t, n;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+      cin >> a[i].d;
+    }
+    sort(a + 1, a + n + 1);
+    for (int i = 1; i <= n; i++) {
+      cout << a[i].d << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
