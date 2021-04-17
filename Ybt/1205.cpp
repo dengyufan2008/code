81abc82581e4e66b02d5e669b82ec9660f0d41f7
Deng Yufan <2695375004@qq.com>
2021-04-17 17:23:16 +0800

    Normal

--- Ybt/1205.cpp
+++ Ybt/1205.cpp
@@ -3,26 +3,22 @@
 
 using namespace std;
 
-int n, ans;
+int n;
+char a, b, c;
 
-void T(int l, int r, int n) {
-  if(n == 1) {
-    ans++;
-    cout << "Let No." << 1 << " item from " << l << " to " << r << endl;
+void T(char l, char r, char mid, int n) {
+  if(n == 0) {
     return;
   }
-  int mid = 6 - l - r;
-  T(l, mid, n - 1);
-  cout << "Let No." << n << " item from " << l << " to " << r << endl;
-  T(mid, r, n - 1);
-  ans++;
+  T(l, mid, r, n - 1);
+  cout << l << "->" << n << "->" << r << endl;
+  T(mid, r, l, n - 1);
 }
 
 int main() {
   cin.tie(0), cout.tie(0);
   std::ios::sync_with_stdio(false);
-  cin >> n;
-  T(1, 3, n);
-  cout << ans;
+  cin >> n >> a >> b >> c;
+  T(a, b, c, n);
   return 0;
 }
