74477a84500917faa89bb7debed390948b344384
Deng Yufan <2695375004@qq.com>
2021-04-17 18:17:29 +0800

    Normal

--- Ybt/1210.cpp
+++ Ybt/1210.cpp
@@ -3,9 +3,31 @@
 
 using namespace std;
 
+int n, s;
+bool flag;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 2; i <= n; i++, s = 0) {
+    while (!(n % i)) {
+      n /= i;
+      s++;
+    }
+    if (s > 1) {
+      if (flag) {
+        cout << "*";
+      }
+      cout << i << "^" << s;
+      flag = true;
+    } else if (s == 1) {
+      if (flag) {
+        cout << "*";
+      }
+      cout << i;
+      flag = true;
+    }
+  }
   return 0;
 }
