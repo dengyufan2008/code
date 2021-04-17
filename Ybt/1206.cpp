488fbaff32c6fb6626f8ece39e1ab95fdf0277e5
Deng Yufan <2695375004@qq.com>
2021-04-17 17:42:16 +0800

    Normal

--- Ybt/1206.cpp
+++ Ybt/1206.cpp
@@ -3,9 +3,24 @@
 
 using namespace std;
 
+int t, m, n;
+
+int C(int m, int n) {  //m个苹果放在n个篮子的分法
+  if (!m || n == 1) {
+    return 1;
+  } else if (n > m) {
+    return C(m, m);
+  }
+  return C(m - n, n) + C(m, n - 1);
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> m >> n;
+    cout << C(m, n) << endl;
+  }
   return 0;
 }
