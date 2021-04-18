a4b89e718b1adb141fefbb4724ae198a5125e75f
Deng Yufan <2695375004@qq.com>
2021-04-18 09:01:38 +0800

    Normal

--- m-DengYufan/reststops/reststops.cpp
+++ m-DengYufan/reststops/reststops.cpp
@@ -3,11 +3,30 @@
 
 using namespace std;
 
+struct A {
+  int x, c;
+  bool operator<(const A &a) const {
+    return c > a.c;
+  }
+} v[100001];
+
+int l, n, f, b;
+LL ans;
+
 int main() {
-  // freopen("reststops.in", "r", stdin);
-  // freopen("reststops.out", "w", stdout);
+  freopen("reststops.in", "r", stdin);
+  freopen("reststops.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> l >> n >> f >> b;
+  for (int i = 1; i <= n; i++) {
+    cin >> v[i].x >> v[i].c;
+  }
+  sort(v + 1, v + 1 + n);
+  for (int i = 1; i <= n; i++) {
+    if (v[i].x > v[i - 1].x) {
+      ans += (v[i].x - v[i - 1].x) * (f - b) * v[i].c;
+    }
+  }
   return 0;
 }
