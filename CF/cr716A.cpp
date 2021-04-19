d5cf8d65680c77122bea3799b8dcf646474e2f6f
Deng Yufan <2695375004@qq.com>
2021-04-19 22:34:53 +0800

    Normal

--- CF/cr716A.cpp
+++ CF/cr716A.cpp
@@ -3,26 +3,7 @@
 
 using namespace std;
 
-LL t, n, a[101], b[101] = {1};
-double tmp;
-
-bool C(void) {
-  for (LL i = 1; i <= n; i++) {
-    b[i] = b[i - 1] * a[i];
-    if (!a[i]) {
-      return false;
-    }
-  }
-  for (LL i = 1; i <= n; i++) {            //长度
-    for (LL j = 1; j <= n - i + 1; j++) {  //起始
-      tmp = sqrt(b[j + i - 1] / b[j - 1]);
-      if (tmp != (LL)tmp) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
+LL t, n, a[101];
 
 int main() {
   cin.tie(0), cout.tie(0);
@@ -33,7 +14,7 @@ int main() {
     for (LL i = 1; i <= n; i++) {
       cin >> a[i];
     }
-    cout << (C() ? "YES" : "NO") << endl;
+    
   }
   return 0;
 }
