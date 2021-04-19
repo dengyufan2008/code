0885eb8fdef87fda9b5682271d806a12345f5d5d
Deng Yufan <2695375004@qq.com>
2021-04-19 22:38:04 +0800

    Normal

--- CF/cr716A.cpp
+++ CF/cr716A.cpp
@@ -3,18 +3,23 @@
 
 using namespace std;
 
-LL t, n, a[101];
+int t, n, a;
+bool flag;
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> t;
   while (t--) {
+    flag = false;
     cin >> n;
-    for (LL i = 1; i <= n; i++) {
-      cin >> a[i];
+    for (int i = 1; i <= n; i++) {
+      cin >> a;
+      if (sqrt(a) != (int)sqrt(a)) {
+        flag = true;
+      }
     }
-    
+    cout << (flag ? "YES" : "NO") << endl;
   }
   return 0;
 }
