62ef33ff4cdb82773bdbe5d8c8257d4af2b9685d
Deng Yufan <2695375004@qq.com>
2021-04-19 22:28:23 +0800

    Normal

--- CF/cr716A.cpp
+++ CF/cr716A.cpp
@@ -3,22 +3,25 @@
 
 using namespace std;
 
-int t, n, a[101], b[101] = {1};
+LL t, n, a[101], b[101] = {1};
 double tmp;
 
 bool C(void) {
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     b[i] = b[i - 1] * a[i];
+    if (!a[i]) {
+      return false;
+    }
   }
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n - i + 1; j++) {
+  for (LL i = 1; i <= n; i++) {            //长度
+    for (LL j = 1; j <= n - i + 1; j++) {  //起始
       tmp = sqrt(b[j + i - 1] / b[j - 1]);
-      if (tmp != (int)tmp) {
-        return false;
+      if (tmp != (LL)tmp) {
+        return true;
       }
     }
   }
-  return true;
+  return false;
 }
 
 int main() {
@@ -27,10 +30,10 @@ int main() {
   cin >> t;
   while (t--) {
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (LL i = 1; i <= n; i++) {
       cin >> a[i];
     }
-    cout << (C() ? "NO" : "YES") << endl;
+    cout << (C() ? "YES" : "NO") << endl;
   }
   return 0;
 }
--- CF/cr716B.cpp
+++ CF/cr716B.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+LL t, n, k;
+
+LL P(LL n, LL k) {
+  if (n == 1) {
+    return 1;
+  }
+  LL num = 0;
+  for (int i = 0; i < k; i++) {
+    num += P(n - 1, i);
+  }
+  return num;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n >> k;
+    cout << P(n, k) << endl;
+  }
   return 0;
 }
