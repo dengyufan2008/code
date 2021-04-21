722429bbdc33aea2ca60ae875ebe7460fb31c6d9
Deng Yufan <2695375004@qq.com>
2021-04-21 19:25:47 +0800

    NOrmal

--- CF/cr697B.cpp
+++ CF/cr697B.cpp
@@ -3,9 +3,15 @@
 
 using namespace std;
 
+int t, n;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    cout << (n % 2020 * 2020 < n ? "YES" : "NO") << endl;
+  }
   return 0;
 }
