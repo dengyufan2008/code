0a8ab597937a21bc4d960f457c89875a333968bf
Deng Yufan <2695375004@qq.com>
2021-04-21 19:22:07 +0800

    NOrmal

--- CF/cr697A.cpp
+++ CF/cr697A.cpp
@@ -3,9 +3,17 @@
 
 using namespace std;
 
+LL t, n, i;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    for (i = 1; i <= n; i <<= 1) {
+    }
+    cout << (i == n ? "NO" : "YES") << endl;
+  }
   return 0;
 }
