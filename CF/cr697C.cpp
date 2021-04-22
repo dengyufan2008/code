92e9003c1b1e357899872b39669e9c23fe0af073
Deng Yufan <2695375004@qq.com>
2021-04-22 19:48:50 +0800

    Normal

--- CF/cr697C.cpp
+++ CF/cr697C.cpp
@@ -3,9 +3,31 @@
 
 using namespace std;
 
+struct A {
+  int b, g;
+} v[200001], c[200001];
+
+int t, na, nb, k;
+unsigned LL ans;
+
 int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> na >> nb >> k;
+    ans = 0;
+    memset(c, 0, sizeof(c));
+    for (int i = 1; i <= k; i++) {
+      cin >> v[i].b;
+      c[v[i].b].b++;
+    }
+    for (int i = 1; i <= k; i++) {
+      cin >> v[i].g;
+      c[v[i].g].g++;
+    }
+    for (int i = 1; i <= k; i++) {
+      ans += k - c[v[i].b].b - c[v[i].g].g + 1;
+    }
+    cout << ans / 2 << endl;
+  }
   return 0;
 }
