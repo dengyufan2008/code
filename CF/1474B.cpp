1efaa51a262a64b21733615945eebe42d981857d
Deng Yufan <2695375004@qq.com>
2021-04-23 07:21:38 +0800

    Normal

--- CF/1474B.cpp
+++ CF/1474B.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+int t, d, ans;
+
+bool C(int x) {
+  for (int i = d + 1; i <= sqrt(x); i++) {
+    if (!(x % i) && x / i - i >= d) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> d;
+    for (ans = 6; !C(ans); ans++) {
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
