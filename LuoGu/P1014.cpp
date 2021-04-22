99250494e594ce05d47dae3d10939f5227fefc63
Deng Yufan <2695375004@qq.com>
2021-04-22 20:51:41 +0800

    Normal

--- LuoGu/P1014.cpp
+++ LuoGu/P1014.cpp
@@ -3,9 +3,24 @@
 
 using namespace std;
 
+LL n, l, r, mid, tmp;
+
 int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+  cin >> n;
+  l = 1, r = n;
+  while (l < r) {
+    mid = (l + r) >> 1;
+    if (mid * (mid + 1) / 2 < n) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
+  tmp = n - l * (l - 1) / 2;
+  if (l % 2 == 0) {
+    cout << tmp << '/' << l + 1 - tmp;
+  } else {
+    cout << l + 1 - tmp << '/' << tmp;
+  }
   return 0;
 }
