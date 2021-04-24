03154e7cc9d96976b3be27ecdeadd8db88de4aef
Deng Yufan <2695375004@qq.com>
2021-04-24 18:02:50 +0800

    Normak

--- CF/1466B.cpp
+++ CF/1466B.cpp
@@ -3,9 +3,27 @@
 
 using namespace std;
 
+int t, n, ans, x[100001];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+      cin >> x[i];
+    }
+    ans = n;
+    for (int i = 1; i <= n; i++) {
+      if (x[i] == x[i - 1]) {
+        x[i]++;
+      }
+      if (x[i] < x[i - 1]) {
+        ans--;
+      }
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
