d713696c11cbbc5add084d26054e690698092eaa
Deng Yufan <2695375004@qq.com>
2021-04-24 17:35:28 +0800

    Normal

--- CF/1517A.cpp
+++ CF/1517A.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+LL t, n, s, ans;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    s = n / 2050;
+    if (s * 2050 != n) {
+      cout << -1 << endl;
+    } else {
+      ans = 0;
+      while (s) {
+        ans += s % 10;
+        s /= 10;
+      }
+      cout << ans << endl;
+    }
+  }
   return 0;
 }
