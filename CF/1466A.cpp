7d8ca337d399783716ec1b643b46a8203348fd1b
Deng Yufan <2695375004@qq.com>
2021-04-24 17:57:23 +0800

    Normal

--- CF/1466A.cpp
+++ CF/1466A.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+int t, n, ans, x[51], a[51];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+      cin >> x[i];
+    }
+    for (int i = 1; i <= n; i++) {
+      for (int j = i + 1; j <= n; j++) {
+        a[x[j] - x[i]] = t + 1;
+      }
+    }
+    ans = 0;
+    for (int i = 1; i <= 50; i++) {
+      if (a[i] == t + 1) {
+        ans++;
+      }
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
