3106d39d5767cf69c2f193c671f2eaaf5d61fc53
Deng Yufan <2695375004@qq.com>
2021-04-26 12:37:00 +0800

    Normal

--- CF/1472B.cpp
+++ CF/1472B.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+int t, n, a[3];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    a[2] = a[1] = a[0];
+    cin >> n;
+    for (int i = 1, tmp; i <= n; i++) {
+      cin >> tmp;
+      a[tmp]++;
+    }
+    cout << (!(a[1] % 2) && !(a[2] % 2) || a[1] >= 2 && !(a[1] % 2) && a[2] % 2 ? "YES" : "NO") << endl;
+  }
   return 0;
 }
