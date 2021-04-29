9424eb51ee5f4800aeb45fce85b1a927178defbf
Deng Yufan <2695375004@qq.com>
2021-04-29 19:40:21 +0800

    Normal

--- CF/1472E.cpp
+++ CF/1472E.cpp
@@ -4,13 +4,10 @@
 using namespace std;
 
 struct A {
-  int a, b, n;
-  bool operator<(const A &x) const {
-    return a < x.a;
-  }
+  int a, b;
 } v[200001];
 
-int t, n, ans[200001];
+int t, n, d[200001], ans[200001];
 
 int main() {
   cin.tie(0), cout.tie(0);
@@ -20,10 +17,18 @@ int main() {
     cin >> n;
     for (int i = 1, tmp1, tmp2; i <= n; i++) {
       cin >> tmp1 >> tmp2;
-      v[i].a = max(tmp1, tmp2), v[i].b = max(tmp1, tmp2), v[i].n = i;
+      v[i].a = max(tmp1, tmp2), v[i].b = max(tmp1, tmp2), d[i] = i;
     }
-    sort(v + 1, v + n + 1);
-    
+    sort(d + 1, d + n + 1, [](int i, int j) {
+      return v[i].b < v[j].b || v[i].b == v[j].b && v[i].a > v[j].a;
+    });
+    for (int i = 1, x = 1, q = d[1]; i <= n; i++) {
+      ans[q] = v[q].a > v[d[x]].a ? d[x] : -1;
+      x = ans[q] == -1 ? i : x;
+    }
+    for (int i = 1; i <= n; i++) {
+      cout << ans[i] << " ";
+    }
   }
   return 0;
 }
