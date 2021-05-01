44242ad54608d587c7191a2a1bf407e45b04c33a
Deng Yufan <2695375004@qq.com>
2021-05-01 17:34:05 +0800

    Normal

--- CF/1454B.cpp
+++ CF/1454B.cpp
@@ -3,9 +3,34 @@
 
 using namespace std;
 
+int t, n, mx, a[200001], d[200001];
+
+void W() {
+  for (int i = 1; i <= mx; i++) {
+    if (a[i] == 1) {
+      cout << d[i] << endl;
+      return;
+    }
+  }
+  cout << -1 << endl;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    fill(a + 1, a + mx + 1, 0), fill(d + 1, d + mx + 1, 0), mx = 0;
+    cin >> n;
+    for (int i = 1, tmp; i <= n; i++) {
+      cin >> tmp;
+      mx = max(mx, tmp);
+      a[tmp]++;
+      if (!d[tmp]) {
+        d[tmp] = i;
+      }
+    }
+    W();
+  }
   return 0;
 }
