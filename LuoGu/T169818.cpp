13f5f70832f6b0207934b09e26afb7383ed28536
Deng Yufan <2695375004@qq.com>
2021-05-01 17:26:02 +0800

    Normal

--- LuoGu/T169818.cpp
+++ LuoGu/T169818.cpp
@@ -1,4 +1,3 @@
-//链表写法
 #include <bits/stdc++.h>
 #define LL long long
 
@@ -48,49 +47,3 @@ int main() {
   }
   return 0;
 }
-/*
-vector写法
-
-#include <bits/stdc++.h>
-#define LL long long
-
-using namespace std;
-
-struct A {
-  int mx;     //最大儿子子树的节点数
-  int d = 1;  //所有子树节点数和
-  vector<int> s;
-} v[100001];
-
-int n;
-
-int T(int f, int x) {  //父亲是f,求x及子树的节点之和
-  int tmp;
-  for(int i = 0; i < v[x].s.size(); i++) {
-    if(v[x].s[i] != f) {
-      tmp = T(x, v[x].s[i]);
-      v[x].mx = max(v[x].mx, tmp);
-      v[x].d += tmp;
-    }
-  }
-  return v[x].d;
-}
-
-int main() {
-  cin.tie(0), cout.tie(0);
-  std::ios::sync_with_stdio(false);
-  cin >> n;
-  for (int i = 1, x, y; i < n; i++) {
-    cin >> x >> y;
-    v[x].s.push_back(y);
-    v[y].s.push_back(x);
-  }
-  T(0, 1);  //父亲是0,正在处理1
-  for(int i = 1; i <= n; i++) {
-    if(v[i].mx <= n / 2 && n - v[i].d <= n / 2) {
-      cout << i << " ";
-    }
-  }
-  return 0;
-}
-*/
