049d5f8402b3025edb0a4935e58cf7b5cda654d3
Deng Yufan <2695375004@qq.com>
2021-05-01 17:25:43 +0800

    Normal

--- CF/1454A.cpp
+++ CF/1454A.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+int t, n;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    if (n & 1) {
+      cout << n << " ";
+      for (int i = 1; i < n; i++) {
+        cout << i << " ";
+      }
+      cout << endl;
+    } else {
+      for (int i = n; i >= 1; i--) {
+        cout << i << " ";
+      }
+      cout << endl;
+    }
+  }
   return 0;
 }
