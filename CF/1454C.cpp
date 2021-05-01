50a38812eeeac574c3dcfdc579de26fdb30b07b6
Deng Yufan <2695375004@qq.com>
2021-05-01 18:02:39 +0800

    Normal

--- CF/1454C.cpp
+++ CF/1454C.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+int t, n, ans;
+
 int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    vector<int> a(n);
+    for (auto &it : a) {
+      cin >> it;
+    }
+    vector<int> res(n + 1, 1);
+    n = unique(a.begin(), a.end()) - a.begin();
+    a.resize(n);
+    for (int i = 0; i < n; i++) {
+      res[a[i]]++;
+    }
+    res[a[0]] -= 1;
+    res[a[n - 1]] -= 1;
+    ans = 1e9 + 1;
+    for (int i = 0; i < n; ++i) {
+      ans = min(ans, res[a[i]]);
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
