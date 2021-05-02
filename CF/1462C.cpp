76ccb7c82de15e6ae61f48d5d13a888ebe68f497
Deng Yufan <2695375004@qq.com>
2021-05-02 21:26:55 +0800

    Normal

--- CF/1462C.cpp
+++ CF/1462C.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+int t, x;
+
+int C() {
+  int r = 0;
+  for (int i = 9, b = 1; i >= 1; i--) {
+    if (x >= i) {
+      x -= i;
+      r += b * i;
+      b *= 10;
+    }
+  }
+  return x ? -1 : r;
+}
+
 int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> x;
+    cout << C() << endl;
+  }
   return 0;
 }
