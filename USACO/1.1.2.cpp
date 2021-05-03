abf959ef6401d93e80e4fd63e7069b3f66c9a243
Deng Yufan <2695375004@qq.com>
2021-05-03 19:32:59 +0800

    Normal

---
+++ USACO/1.1.2.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: gift1
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("gift1.in", "r", stdin);
+  // freopen("gift1.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
