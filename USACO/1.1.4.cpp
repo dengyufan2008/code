dcf878741cc2e96d8338faf745272ea9b8841bd9
Deng Yufan <2695375004@qq.com>
2021-05-03 21:59:48 +0800

    Normal

--- USACO/1.1.4.cpp
+++ USACO/1.1.4.cpp
@@ -8,34 +8,29 @@ LANG: C++
 
 using namespace std;
 
-int n, ans;
-char s[351];
-
-int M(int i) {
-  int x = 0, d = 0;
-  char c = 'a';
-  for (; d <= 2; i++) {
-    if (s[i] != c && s[i] != 'w') {
-      d++;
-    }
-    c = s[i] == 'w' ? c : s[i];
-    ans++;
-  }
-  return ans;
-}
+int n, a, b, w, ans;
+char s[700], c;
 
 int main() {
-  // freopen("beads.in", "r", stdin);
-  // freopen("beads.out", "w", stdout);
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  cin >> n;
-  for (int i = 1; i <= n; i++) {
-    cin >> s[i];
+  freopen("beads.in", "r", stdin);
+  freopen("beads.out", "w", stdout);
+  cin >> n >> s;
+  copy(&s[0], &s[n - 1] + 1, &s[n]);
+  for (int i = 0; i < (n << 1); i++) {
+    if (s[i] == 'w') {
+      b++;
+      w++;
+    } else if (s[i] == c) {
+      b++;
+      w = 0;
+    } else {
+      ans = max(ans, a + b);
+      a = b - w, b = w + 1;
+      w = 0;
+      c = s[i];
+    }
   }
-  for (int i = 1; i <= n; i++) {
-    ans = max(ans, M(i));
-  }
-  cout << ans << endl;
+  ans = max(ans, a + b);
+  cout << min(ans, n) << endl;
   return 0;
 }
