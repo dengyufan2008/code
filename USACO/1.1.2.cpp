17a57e02e987111695ac5f1244c9c9e15dffb6f2
Deng Yufan <2695375004@qq.com>
2021-05-03 20:46:24 +0800

    Normal

--- USACO/1.1.2.cpp
+++ USACO/1.1.2.cpp
@@ -8,11 +8,38 @@ LANG: C++
 
 using namespace std;
 
+int n, a[11];
+string tmp, s[11];
+
 int main() {
-  // freopen("gift1.in", "r", stdin);
-  // freopen("gift1.out", "w", stdout);
+  freopen("gift1.in", "r", stdin);
+  freopen("gift1.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+  }
+  for (int i = 1, m, k; i <= n; i++) {
+    cin >> tmp >> m >> k;
+    for (int j = 1; j <= n; j++) {
+      if (s[j] == tmp) {
+        a[j] -= m - (m % max(k, 1));
+        break;
+      }
+    }
+    for (int j = 1; j <= k; j++) {
+      cin >> tmp;
+      for (int l = 1; l <= n; l++) {
+        if (s[l] == tmp) {
+          a[l] += m / k;
+          break;
+        }
+      }
+    }
+  }
+  for (int i = 1; i <= n; i++) {
+    cout << s[i] << " " << a[i] << endl;
+  }
   return 0;
 }
---
+++ USACO/1.1.3.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: friday
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("friday.in", "r", stdin);
+  // freopen("friday.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
