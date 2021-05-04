b8e197615df3e7979279857db7a4b60fc0254c16
Deng Yufan <2695375004@qq.com>
2021-05-04 12:40:56 +0800

    Normal

--- Other/dui_sort.cpp
+++ Other/dui_sort.cpp
@@ -6,17 +6,48 @@ using namespace std;
 const int kMaxN = 1e5 + 1;
 
 struct A {
-  int _n, n, a[kMaxN];
+  int n, _n, a[kMaxN];
 
+  int Son(int x) {
+    return x + (x < n && a[x] < a[x + 1]);
+  }
+
+  void Up() {
+    for (int i = n, j = i / 2; j && a[i] > a[j]; i = j, j = i / 2) {
+      swap(a[i], a[j]);
+    }
+  }
+
+  void Down() {
+    for (int i = 1, j = Son(i * 2); j <= n && a[i] < a[j]; i = j, j = Son(i * 2)) {
+      swap(a[i], a[j]);
+    }
+  }
+
+  void Push(int v) {
+    a[++n] = v;
+    Up();
+  }
+
+  void Pop() {
+    swap(a[1], a[n--]);
+    Down();
+  }
+
+  void Sort() {
+    for (int i = 1; i <= _n; i++) {
+      Pop();
+    }
+  }
 } h;
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> h.n;
+  cin >> h._n;
   for (int i = 1, tmp; i <= h._n; i++) {
     cin >> tmp;
-
+    h.Push(tmp);
   }
   h.Sort();
   for (int i = 1; i <= h._n; i++) {
