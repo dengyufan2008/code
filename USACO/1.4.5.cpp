2aa45e0b1563916c942cd96b9da3536abc979688
Deng Yufan <2695375004@qq.com>
2021-05-04 18:43:27 +0800

    Normal

---
+++ USACO/1.4.5.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: wormhole
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("wormhole.in", "r", stdin);
+  // freopen("wormhole.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
