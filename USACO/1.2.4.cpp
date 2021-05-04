8442e64bc9fbcbe23ddeccd0e78d553c5c8dd81f
Deng Yufan <2695375004@qq.com>
2021-05-04 13:07:26 +0800

    Normal

--- USACO/1.2.4.cpp
+++ USACO/1.2.4.cpp
@@ -8,32 +8,47 @@ LANG: C++
 
 using namespace std;
 
-int b, l1, l2, a[10][2];
+int b;
 
-void M(int x, int d) {
+struct A {
+  int l, a[20];
 
-}
+  A(int x) {
+    for (l = 0; x; l++) {
+      a[l] = x % b;
+      x /= b;
+    }
+  }
 
-bool C() {
-  
-}
+  void Print() {
+    for (int i = l - 1; i >= 0; i--) {
+      cout << (a[i] >= 0 && a[i] <= 9 ? (char)(a[i] + '0') : (char)(a[i] - 10 + 'A'));
+    }
+  }
+
+  bool C() {
+    for (int i = 0, j = l - 1; i < j; i++, j--) {
+      if (a[i] != a[j]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
 
 int main() {
-  // freopen("palsquare.in", "r", stdin);
-  // freopen("palsquare.out", "w", stdout);
+  freopen("palsquare.in", "r", stdin);
+  freopen("palsquare.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> b;
   for (int i = 1; i <= 300; i++) {
-    M(i, 0), M(i * i, 1);
-    if (C()) {
-      for (int j = 1; j <= l1; j++) {
-        cout << a[j][0];
-      }
+    A n(i * i);
+    if (n.C()) {
+      A m(i);
+      m.Print();
       cout << " ";
-      for (int j = 1; j <= l2; j++) {
-        cout << a[j][1];
-      }
+      n.Print();
       cout << endl;
     }
   }
---
+++ USACO/1.2.5.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: dualpal
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("dualpal.in", "r", stdin);
+  // freopen("dualpal.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
