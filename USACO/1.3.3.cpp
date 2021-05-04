351d77312fd49fa359f5ae2032a2cf18501e4d1e
Deng Yufan <2695375004@qq.com>
2021-05-04 18:11:34 +0800

    Normal

--- USACO/1.3.3.cpp
+++ USACO/1.3.3.cpp
@@ -12,12 +12,24 @@ int n, ans, a[11];
 bool b[10];
 
 bool C(int x, int d) {
-  
+  int k = 0, c[6] = {};
+  while (x) {
+    c[++k] = x % 10, x /= 10;
+  }
+  if(k != d) {
+    return false;
+  }
+  for (int i = 1; i <= k; i++) {
+    if (!b[c[i]]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 int main() {
-  // freopen("crypt1.in", "r", stdin);
-  // freopen("crypt1.out", "w", stdout);
+  freopen("crypt1.in", "r", stdin);
+  freopen("crypt1.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> n;
@@ -28,10 +40,10 @@ int main() {
   for (int i1 = 1, tmp; i1 <= n; i1++) {
     for (int i2 = 1; i2 <= n; i2++) {
       for (int i3 = 1; i3 <= n; i3++) {
-        for (int j1 = 1; j1 = n; j1++) {
-          for (int j2 = 1; j2 = n; j2++) {
-            tmp = i1 * 100 + i2 * 10 + i3;
-            ans += C(tmp * j1, 3) && C(tmp * j2, 3) && C(tmp * (j1 * 10 + j2), 4);
+        for (int j1 = 1; j1 <= n; j1++) {
+          for (int j2 = 1; j2 <= n; j2++) {
+            tmp = a[i1] * 100 + a[i2] * 10 + a[i3];
+            ans += C(tmp * a[j1], 3) && C(tmp * a[j2], 3) && C(tmp * (a[j1] * 10 + a[j2]), 4);
           }
         }
       }
---
+++ USACO/1.3.4.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: combo
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("combo.in", "r", stdin);
+  // freopen("combo.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
