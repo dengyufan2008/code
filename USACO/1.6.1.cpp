84dda4e1d03e20dd4c556ff70cf43c9111d693bc
Deng Yufan <2695375004@qq.com>
2021-05-07 18:09:48 +0800

    Noraml

---
+++ USACO/1.6.1.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: numtri
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("numtri.in", "r", stdin);
+  // freopen("numtri.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
