eb90a02cdb9f71f27230616e6a0a68bfea051515
Deng Yufan <2695375004@qq.com>
2021-05-07 18:15:41 +0800

    Normal

--- USACO/1.6.1.cpp
+++ USACO/1.6.1.cpp
@@ -8,11 +8,24 @@ LANG: C++
 
 using namespace std;
 
+int r, a[1001][1001];
+
 int main() {
-  // freopen("numtri.in", "r", stdin);
-  // freopen("numtri.out", "w", stdout);
+  freopen("numtri.in", "r", stdin);
+  freopen("numtri.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> r;
+  for (int i = 1; i <= r; i++) {
+    for (int j = 1; j <= i; j++) {
+      cin >> a[i][j];
+    }
+  }
+  for (int i = r - 1; i >= 1; i--) {
+    for (int j = 1; j <= i; j++) {
+      a[i][j] += max(a[i + 1][j], a[i + 1][j + 1]);
+    }
+  }
+  cout << a[1][1] << endl;
   return 0;
 }
---
+++ USACO/1.6.2.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: pprime
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("pprime.in", "r", stdin);
+  // freopen("pprime.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
