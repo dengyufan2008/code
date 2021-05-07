64ad59b768971464150bf2b6da0cd24d435353f4
Deng Yufan <2695375004@qq.com>
2021-05-07 20:23:44 +0800

    Normal

---
+++ USACO/1.6.3.cpp
@@ -0,0 +1,18 @@
+/*
+ID: dengyuf1
+TASK: sprime
+LANG: C++
+*/
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("sprime.in", "r", stdin);
+  // freopen("sprime.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
