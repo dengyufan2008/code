5f9b6be97f782f95f49ad57516ceb9cc45648921
Deng Yufan <2695375004@qq.com>
2021-05-07 17:31:53 +0800

    Normal

--- USACO/1.5.2.cpp
+++ USACO/1.5.2.cpp
@@ -9,11 +9,11 @@ LANG: C++
 using namespace std;
 
 int a, b, c;
-bool ans[61];
+bool ans[21];
 
 void Dfs(int _a, int _b) {
   int _c = c - _a - _b;
-  if (_a < 0 || _b < 0 || _c < 0 || ans[_c]) {
+  if (ans[_c]) {
     return;
   }
   if (!_a) {
@@ -27,21 +27,6 @@ void Dfs(int _a, int _b) {
   Dfs(_a, _b - min(c - _c, _b));
 }
 
-void Print() {
-  int mx = 60;
-  for (; mx >= 0; mx--) {
-    if (ans[mx]) {
-      break;
-    }
-  }
-  for (int i = 0; i < mx; i++) {
-    if (ans[i]) {
-      cout << i << " ";
-    }
-  }
-  cout << mx << endl;
-}
-
 int main() {
   //	freopen("milk3.in", "r", stdin);
   //	freopen("milk3.out", "w", stdout);
@@ -49,6 +34,11 @@ int main() {
   ios::sync_with_stdio(false);
   cin >> a >> b >> c;
   Dfs(0, 0);
-  Print();
+  for (int i = 0; i < c; i++) {
+    if (ans[i]) {
+      cout << i << " ";
+    }
+  }
+  cout << c << endl;
   return 0;
 }
