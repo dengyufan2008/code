8f41dc801140350ad80926889f12cc122cbfa846
Deng Yufan <2695375004@qq.com>
2021-05-11 21:18:10 +0800

    Normal

--- CF/1520A.cpp
+++ CF/1520A.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+int t, n;
+char s[51];
+bool flag, b[26];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    flag = true;
+    fill(&b[0], &b[25] + 1, false);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+      cin >> s[i];
+    }
+    for (int i = 1; i <= n; i++) {
+      if (b[s[i] - 'A'] && s[i] != s[i - 1]) {
+        flag = false;
+      } else {
+        b[s[i] - 'A'] = true;
+      }
+    }
+    cout << (flag ? "YES" : "NO") << endl;
+  }
   return 0;
 }
