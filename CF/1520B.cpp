ac2337f9a4eca1b05a618c110f5d57a80a0f494e
Deng Yufan <2695375004@qq.com>
2021-05-11 21:39:31 +0800

    Normal

--- CF/1520B.cpp
+++ CF/1520B.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+const int kMaxM[10] = {1, 11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111, 1111111111};
+
+int t, n, l, ans;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    l = 0;
+    cin >> n;
+    for (int i = 10; i <= n; i *= 10) {
+      l++;
+    }
+    ans = l * 9;
+    for (int i = kMaxM[l]; i <= n; i += kMaxM[l]) {
+      ans++;
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
