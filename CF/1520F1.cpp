23ca98f498820a209a323009bc4d8ab94b1cb782
Deng Yufan <2695375004@qq.com>
2021-05-13 07:28:24 +0800

    Normal

--- CF/1520F1.cpp
+++ CF/1520F1.cpp
@@ -3,9 +3,38 @@
 
 using namespace std;
 
+int n, t, k, l, r, mid, v;
+
+int Q(int x, int y) {
+  int ans;
+  cout << "? " << x << " " << y << endl;
+  cout.flush();
+  cin >> ans;
+  return y - x + 1 - ans;
+}
+
+void A(int x) {
+  cout << "! " << x << endl;
+  cout.flush();
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> t;
+  while (t--) {
+    cin >> k;
+    l = 1, r = n;
+    while (l <= r) {
+      mid = (l + r) >> 1;
+      v = Q(l, mid);
+      if (v < k) {
+        l = mid + 1, k -= v;
+      } else {
+        r = mid - 1;
+      }
+    }
+    A(l);
+  }
   return 0;
 }
