168a75acbc9a20acaa0e6300b79fc95cbc9b79e2
Deng Yufan <2695375004@qq.com>
2021-05-12 20:03:55 +0800

    Normal

--- CF/1520C.cpp
+++ CF/1520C.cpp
@@ -3,9 +3,33 @@
 
 using namespace std;
 
+int t, n, k;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    if (n == 2) {
+      cout << -1 << endl;
+      continue;
+    }
+    k = 0;
+    for (int i = 1; i <= n * n; i += 2) {
+      cout << i << " ";
+      if (++k == n) {
+        cout << endl;
+        k = 0;
+      }
+    }
+    for (int i = 2; i <= n * n; i += 2) {
+      cout << i << " ";
+      if (++k == n) {
+        cout << endl;
+        k = 0;
+      }
+    }
+  }
   return 0;
 }
