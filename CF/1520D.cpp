fb5c0f602aa6a6344aab522be52136cdcc3a60c4
Deng Yufan <2695375004@qq.com>
2021-05-12 20:29:07 +0800

    Normal

--- CF/1520D.cpp
+++ CF/1520D.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+const int kMaxN = 200000;
+
+LL t, n, ans, a[2 * kMaxN + 1];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    ans = 0;
+    fill(&a[1], &a[2 * kMaxN], 0);
+    cin >> n;
+    for (LL i = 1, tmp; i <= n; i++) {
+      cin >> tmp;
+      a[tmp - i + kMaxN]++;
+    }
+    for (LL i = 0; i <= 2 * kMaxN; i++) {
+      ans += max(a[i] * (a[i] - 1) / 2, 0ll);
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
