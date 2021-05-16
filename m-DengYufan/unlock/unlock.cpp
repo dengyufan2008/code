b07d49beded246c5879fe7b77737922deb1497e4
Deng Yufan <2695375004@qq.com>
2021-05-16 08:54:30 +0800

    Normal

---
+++ m-DengYufan/unlock/unlock.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("unlock.in", "r", stdin);
+  // freopen("unlock.out", "w", stdout);
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  
+  return 0;
+}
