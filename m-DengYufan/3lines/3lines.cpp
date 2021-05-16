15783c732cf01b15d019923556a7f45ec9920739
Deng Yufan <2695375004@qq.com>
2021-05-16 09:39:40 +0800

    Normal

--- m-DengYufan/3lines/3lines.cpp
+++ m-DengYufan/3lines/3lines.cpp
@@ -3,11 +3,34 @@
 
 using namespace std;
 
+struct A {
+  int x, y, a, b;
+} v[51];
+int n, l[51];
+
 int main() {
   // freopen("3lines.in", "r", stdin);
   // freopen("3lines.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> v[i].x >> v[i].y;
+  }
+  sort(l + 1, l + n + 1, [](int i, int j) {
+    return v[i].x < v[j].x;
+  });
+  for (int i = 1; i <= n; i++) {
+    v[l[i]].a = v[l[i - 1]].a + (v[l[i]].x != v[l[i - 1]].x);
+  }
+  sort(l + 1, l + n + 1, [](int i, int j) {
+    return v[i].y < v[j].y;
+  });
+  for (int i = 1; i <= n; i++) {
+    v[l[i]].b = v[l[i - 1]].b + (v[l[i]].y != v[l[i - 1]].y);
+  }
+  for (int i = 1; i <= n; i++) {
+    cout << v[i].a << " " << v[i].b << endl;
+  }
   return 0;
 }
--- m-DengYufan/cowrow/cowrow.cpp
+++ m-DengYufan/cowrow/cowrow.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
+int n, a[1001];
+
 int main() {
   // freopen("cowrow.in", "r", stdin);
   // freopen("cowrow.out", "w", stdout);
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
   
   return 0;
 }
