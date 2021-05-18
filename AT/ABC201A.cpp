e25e4bf36b68107b1e4bee9438880bce308c0938
Deng Yufan <2695375004@qq.com>
2021-05-18 20:37:16 +0800

    Normal

--- AT/ABC201A.cpp
+++ AT/ABC201A.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
+int a1, a2, a3;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> a1 >> a2 >> a3;
+  cout << (a1 - a2 == a2 - a3 || a1 - a3 == a3 - a2 || a2 - a1 == a1 - a3 || a2 - a3 == a3 - a1 || a3 - a1 == a1 - a2 || a3 - a2 == a2 - a1 ? "Yes" : "No") << endl;
   return 0;
 }
