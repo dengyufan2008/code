8ae674323c705acb5f675fb6df7fa0fa1c2b4d04
Deng Yufan <2695375004@qq.com>
2021-05-18 20:40:05 +0800

    Normal

--- AT/ABC201B.cpp
+++ AT/ABC201B.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+struct A {
+  int h;
+  string s;
+} v[1001];
+
+int n;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> v[i].s >> v[i].h;
+  }
+  sort(v + 1, v + n + 1, [](A i, A j) {
+    return i.h > j.h;
+  });
+  cout << v[2].s << endl;
   return 0;
 }
