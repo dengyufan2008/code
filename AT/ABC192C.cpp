23bf7ef8aa3cb6824c122847325c9b6540910bd8
Deng Yufan <2695375004@qq.com>
2021-05-19 21:00:43 +0800

    Normal

--- AT/ABC192C.cpp
+++ AT/ABC192C.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+int n, k, l, s1, s2, a[10];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> k;
+  while (k--) {
+    l = s1 = s2 = 0;
+    while (n) {
+      a[++l] = n % 10, n /= 10;
+    }
+    sort(a + 1, a + l + 1);
+    for (int i = l; i >= 1; i--) {
+      s1 = s1 * 10 + a[i];
+    }
+    for (int i = 1; i <= l; i++) {
+      s2 = s2 * 10 + a[i];
+    }
+    n = s1 - s2;
+  }
+  cout << n << endl;
   return 0;
 }
