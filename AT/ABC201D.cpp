7d15f2694482aa05c3e7d76a2d70bf6f34adf4c9
Deng Yufan <2695375004@qq.com>
2021-05-19 19:50:34 +0800

    Normal

--- AT/ABC201D.cpp
+++ AT/ABC201D.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int h, w, a[2001][2001];
+int h, w, a[2002][2002], f[2002][2002];
 char ch;
 
 int main() {
@@ -16,6 +16,21 @@ int main() {
       a[i][j] = (ch == '+' ? 1 : -1);
     }
   }
-  
+  for (int i = h; i >= 1; i--) {
+    for (int j = w; j >= 1; j--) {
+      if ((i + j) & 1) {
+        f[i][j] = min(f[i + 1][j] - a[i + 1][j], f[i][j + 1] - a[i][j + 1]);
+      } else {
+        f[i][j] = max(f[i + 1][j] + a[i + 1][j], f[i][j + 1] + a[i][j + 1]);
+      }
+    }
+  }
+  if (f[1][1] > 0) {
+    cout << "Takahashi" << endl;
+  } else if (f[1][1] < 0) {
+    cout << "Aoki" << endl;
+  } else {
+    cout << "Draw" << endl;
+  }
   return 0;
 }
