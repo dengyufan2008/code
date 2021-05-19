d6eb2f4e064e816278ed17a411c71ea65e078901
Deng Yufan <2695375004@qq.com>
2021-05-19 20:51:00 +0800

    Normal

--- AT/ABC192B.cpp
+++ AT/ABC192B.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+const int kM[4] = {97, 122, 65, 90};
+
+string s;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> s;
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] < kM[(i & 1) * 2] || s[i] > kM[(i & 1) * 2 + 1]) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
   return 0;
 }
