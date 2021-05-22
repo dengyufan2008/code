8a829ee8d9edbb4db832c3ba7ca7b13d98ff7da7
Deng Yufan <2695375004@qq.com>
2021-05-22 20:55:26 +0800

    Normal

--- AT/ABC202D.cpp
+++ AT/ABC202D.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
+LL a, b, k, d[31][31];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> a >> b >> k;
+  for (int i = 0; i <= 30; i++) {
+    d[i][0] = d[0][i] = 1;
+  }
+  for (int i = 1; i <= 30; i++) {
+    for (int j = 1; j <= 30; j++) {
+      d[i][j] = d[i - 1][j] + d[i][j - 1];
+    }
+  }
+  while (k) {
+    if (d[a][b] / 2 <= k) {
+      cout << 'a';
+      a--;
+    } else {
+      cout << 'b';
+      b--, k -= d[a][b] / 2;
+    }
+  }
   return 0;
 }
