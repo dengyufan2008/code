736847b6ddbed6b9d71cd3a3fc45332638743a02
Deng Yufan <2695375004@qq.com>
2021-05-22 20:06:16 +0800

    Normal

--- AT/ABC202B.cpp
+++ AT/ABC202B.cpp
@@ -3,9 +3,18 @@
 
 using namespace std;
 
+const int kM[10] = {0, 1, 0, 0, 0, 0, 9, 0, 8, 6};
+
+int l;
+string s;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> s;
+  l = s.length();
+  for (int i = l - 1; i >= 0; i--) {
+    cout << kM[s[i] - '0'];
+  }
   return 0;
 }
