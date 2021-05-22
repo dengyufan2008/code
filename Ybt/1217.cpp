91e70f1b351cf18b8810ccd1dd81568665fa4bea
Deng Yufan <2695375004@qq.com>
2021-05-22 18:26:49 +0800

    Normal

--- Ybt/1217.cpp
+++ Ybt/1217.cpp
@@ -3,9 +3,42 @@
 
 using namespace std;
 
+int n, k, ans;
+bool a[9][9], b[9];
+char ch;
+
+void S(int d, int s) {
+  ans += s == k;
+  if (d > n || s >= k) {
+    return;
+  }
+  for (int i = 1; i <= n; i++) {
+    if (a[d][i] && !b[i]) {
+      b[i] = true;
+      S(d + 1, s + 1);
+      b[i] = false;
+    }
+  }
+  S(d + 1, s);
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  while (1) {
+    ans = 0;
+    cin >> n >> k;
+    if (n == -1 && k == -1) {
+      break;
+    }
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        cin >> ch;
+        a[i][j] = ch == '#';
+      }
+    }
+    S(1, 0);
+    cout << ans << endl;
+  }
   return 0;
 }
