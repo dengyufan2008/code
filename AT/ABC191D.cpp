a28cb6c07a7962ac213d29d903e6c57b19704301
Deng Yufan <2695375004@qq.com>
2021-05-24 20:32:01 +0800

    Normal

--- AT/ABC191D.cpp
+++ AT/ABC191D.cpp
@@ -3,9 +3,14 @@
 
 using namespace std;
 
+LL x, y, r;
+double _x, _y, _r;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> _x >> _y >> _r;
+  x = _x * 10000, y = _y * 10000, r = _r * 10000;
+  cout << x << " " << y << " " << r << " " << r * r << endl;
   return 0;
 }
