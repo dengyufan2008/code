ec23df7c202a79a740cfa828c2c6e006c78af660
Deng Yufan <2695375004@qq.com>
2021-05-24 21:00:22 +0800

    Normal

--- AT/ABC191E.cpp
+++ AT/ABC191E.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+struct A {
+  int p, d, x;
+} e[2001];
+struct B {
+  int et, d = -1;
+} v[2001];
+int n, m;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
+  cin >> n >> m;
+  for (int i = 1, a, b, c; i <= m; i++) {
+    cin >> a >> b >> c;
+    e[i] = {v[a].et, b, c}, v[a].et = i;
+  }
   
+  for (int i = 1; i <= n; i++) {
+    cout << v[i].d << endl;
+  }
   return 0;
 }
