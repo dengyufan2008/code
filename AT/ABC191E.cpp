3f19b1a6a5494702424b9c58d02ab67b41da139e
Deng Yufan <2695375004@qq.com>
2021-05-25 21:15:01 +0800

    Normal

--- AT/ABC191E.cpp
+++ AT/ABC191E.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 struct A {
-  int p, d, x;
+  LL p, d, x;
 } e[2001];
 struct B {
-  int et, d = 2e9 + 1;
+  LL et, d = 2e9 + 1;
 } v[2001];
-int n, m;
+LL n, m;
 bool b[2001];
 
-int S(int x, int y, int s) {
+LL S(LL x, LL y, LL s) {
   if (b[x]) {
     return 2e9 + 1;
   }
@@ -20,8 +20,8 @@ int S(int x, int y, int s) {
     return s;
   }
   b[x] = true;
-  int ans = 2e9 + 1;
-  for (int i = v[x].et; i; i = e[i].p) {
+  LL ans = 2e9 + 1;
+  for (LL i = v[x].et; i; i = e[i].p) {
     ans = min(ans, S(e[i].d, y, s + e[i].x));
   }
   return ans;
@@ -31,12 +31,15 @@ int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> n >> m;
-  for (int i = 1, a, b, c; i <= m; i++) {
+  for (LL i = 1, a, b, c; i <= m; i++) {
     cin >> a >> b >> c;
     e[i] = {v[a].et, b, c}, v[a].et = i;
+    if (a == b) {
+      v[a].d = c;
+    }
   }
-  for (int i = 1; i <= n; i++) {
-    for (int j = v[i].et; j; j = e[j].p) {
+  for (LL i = 1; i <= n; i++) {
+    for (LL j = v[i].et; j; j = e[j].p) {
       fill(&b[0], &b[2000] + 1, false);
       v[i].d = min(v[i].d, e[j].x + S(e[j].d, i, 0));
     }
