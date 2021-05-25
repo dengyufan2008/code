96073c8cc860fbd759669351b3f22c838c0f9d3a
Deng Yufan <2695375004@qq.com>
2021-05-25 21:20:23 +0800

    Normal

--- AT/ABC190B.cpp
+++ AT/ABC190B.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-int a, b, c;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> a >> b >> c;
-  cout << (!c && a > b || c && a >= b ? "Takahashi" : "Aoki") << endl;
+  
   return 0;
 }
