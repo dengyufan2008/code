0dc1007914b6b694db174b5b769e40cb21458e4d
Deng Yufan <2695375004@qq.com>
2021-05-26 07:24:20 +0800

    Normal

--- AT/ABC190B.cpp
+++ AT/ABC190B.cpp
@@ -3,9 +3,17 @@
 
 using namespace std;
 
+int n, s, d, x, y;
+bool flag;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> s >> d;
+  for (int i = 1; i <= n; i++) {
+    cin >> x >> y;
+    flag = flag || x < s && y > d;
+  }
+  cout << (flag ? "Yes" : "No") << endl;
   return 0;
 }
