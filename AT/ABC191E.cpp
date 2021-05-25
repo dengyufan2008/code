263ff27eaa0913be00e26834878eb6e2ac69f879
Deng Yufan <2695375004@qq.com>
2021-05-25 19:56:59 +0800

    Normal

--- AT/ABC191E.cpp
+++ AT/ABC191E.cpp
@@ -7,9 +7,20 @@ struct A {
   int p, d, x;
 } e[2001];
 struct B {
-  int et, d = -1;
+  int et, d = 2147483647, k;
 } v[2001];
 int n, m;
+
+int M(int x, int y, int s) {
+  if (x == y) {
+    return s ? s : v[x].k ? v[x].k : 2147483647;
+  }
+  int ans = 2147483647;
+  for (int i = v[x].et; i; i = e[i].p) {
+    ans = min(ans, M(e[i].d, y, s + e[i].x));
+  }
+  return ans;
+}
 
 int main() {
   cin.tie(0), cout.tie(0);
@@ -17,10 +28,12 @@ int main() {
   cin >> n >> m;
   for (int i = 1, a, b, c; i <= m; i++) {
     cin >> a >> b >> c;
-    e[i] = {v[a].et, b, c}, v[a].et = i;
+    e[i] = {v[a].et, b, c}, v[a].et = i, v[a].k = (v[a].k || a == b) * c;
   }
-  
   for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      v[i].d = min(v[i].d, M(i, j, 0) + M(j, i, 0));
+    }
     cout << v[i].d << endl;
   }
   return 0;
