89f4d8d90770b0c747369d070514a6e51d7ded10
Deng Yufan <2695375004@qq.com>
2021-05-26 20:51:38 +0800

    Normal

--- AT/ABC189A.cpp
+++ AT/ABC189A.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
+bool flag = true;
+char s[3];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> s;
+  for (int i = 0; i < 2; i++) {
+    flag = flag && s[i] == s[i + 1];
+  }
+  cout << (flag ? "Won" : "Lost") << endl;
   return 0;
 }
