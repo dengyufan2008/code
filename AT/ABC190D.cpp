fde15eab5a9a36d3ecfe05b9bfc5518cbb23478b
Deng Yufan <2695375004@qq.com>
2021-05-26 20:36:31 +0800

    Normal

--- AT/ABC190D.cpp
+++ AT/ABC190D.cpp
@@ -3,9 +3,15 @@
 
 using namespace std;
 
+LL n, ans;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (LL i = 1; i * (i + 1) <= n * 2; i++) {
+    ans += i & 1 && !(n % i) || !(i & 1) && n & 1 && i % 4 && !(n % (i / 2)) && (n * 2 / i) % 2 || !(n % 2) && !(i % 4) && !(n % (i / 2)) && (n * 2 / i) % 2;
+  }
+  cout << ans * 2 << endl;
   return 0;
 }
