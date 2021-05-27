f7424c65ad28f6aa2838748c1479b72e1468a1a0
Deng Yufan <2695375004@qq.com>
2021-05-27 20:11:31 +0800

    Normal

--- AT/ABC188B.cpp
+++ AT/ABC188B.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-int x, y;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> x >> y;
-  cout << (min(x, y) + 3 > max(x, y) ? "Yes" : "No") << endl;
+  
   return 0;
 }
