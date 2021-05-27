7584ac6f0c381473d758bb21d99f679fdabebf83
Deng Yufan <2695375004@qq.com>
2021-05-27 20:51:55 +0800

    Normal

--- AT/ABC188C.cpp
+++ AT/ABC188C.cpp
@@ -3,9 +3,21 @@
 
 using namespace std;
 
+int n;
+vector<pair<int, int>> a, b;
+
 int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1, x; i <= 1 << n; i++) {
+    cin >> x;
+    a.push_back({x, i});
+  }
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < a.size(); j += 2) {
+      b.push_back(a[j + (a[j].first <= a[j + 1].first)]);
+    }
+    a = b, b.clear();
+  }
+  cout << (a[0].first <= a[1].first ? a[0].second : a[1].second) << endl;
   return 0;
 }
