87170fa7b2233709b6d56eff08c2990eb9e543d3
Deng Yufan <2695375004@qq.com>
2021-05-27 19:22:47 +0800

    Normal

--- AT/ABC189D.cpp
+++ AT/ABC189D.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
+LL n, f[61][2];
+string s[61];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  fill(&f[0][0], &f[60][1] + 1, 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+    f[i][0] = f[i - 1][0] + (f[i - 1][0] + f[i - 1][1]) * (s[i] == "AND");
+    f[i][1] = f[i - 1][1] + (f[i - 1][0] + f[i - 1][1]) * (s[i] == "OR");
+  }
+  cout << f[n][1] << endl;
   return 0;
 }
