e6098434f3c4e438d855ca9b3f32e173324b7be9
Deng Yufan <2695375004@qq.com>
2021-05-27 21:20:21 +0800

    Normal

--- AT/ABC188D.cpp
+++ AT/ABC188D.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+LL n, c, s, p, ans;
+map<int, LL> q;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> c;
+  for (int i = 1, x, y, z; i <= n; i++) {
+    cin >> x >> y >> z;
+    q[x] += z;
+    q[y + 1] -= z;
+  }
+  for (auto it : q) {
+    ans += min(s, c) * (it.first - p);
+    s += it.second;
+    p = it.first;
+  }
+  cout << ans << endl;
   return 0;
 }
