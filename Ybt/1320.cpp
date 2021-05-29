48a8fe18972979a1a8d72cdf6858762ba02d4ebc
Deng Yufan <2695375004@qq.com>
2021-05-29 17:46:25 +0800

    Normal

--- Ybt/1320.cpp
+++ Ybt/1320.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+int n, tot, ans, a[101];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    tot += a[i];
+  }
+  tot /= n;
+  for (int i = 1; i < n; i++) {
+    while (a[i] == tot) {
+      i++;
+    }
+    a[i + 1] += a[i] - tot, ans += i < n;
+  }
+  cout << ans << endl;
   return 0;
 }
