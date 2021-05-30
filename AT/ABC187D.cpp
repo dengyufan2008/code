55c9996430efd2406bba44a82ec9417c60066bcc
Deng Yufan <2695375004@qq.com>
2021-05-30 19:56:23 +0800

    Normal

--- AT/ABC187D.cpp
+++ AT/ABC187D.cpp
@@ -3,9 +3,21 @@
 
 using namespace std;
 
+LL n, tot, tmp, ans, a[200001];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i] >> tmp;
+    tot += a[i], a[i] = a[i] * 2 + tmp;
+  }
+  sort(a + 1, a + n + 1);
+  for (int i = n; i >= 1 && tot >= 0; i--) {
+    tot -= a[i];
+    ans++;
+  }
+  cout << ans << endl;
   return 0;
 }
