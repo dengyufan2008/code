c956867b07f028dbde1a75980e1f94da82b2726e
Deng Yufan <2695375004@qq.com>
2021-05-30 16:08:28 +0800

    Normal

---
+++ LuoGu/P3865_1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define LL long long
+
+using namespace std;
+
+int n, m, ans, a[100001], d[100001][21];
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(false);
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    d[i][0] = a[i];
+  }
+  for (int i = 1; (1 << i) <= n; i++) {
+    for (int j = 1; j <= n - (1 << i) + 1; j++) {
+      d[j][i] = max(d[j][i - 1], d[j + (1 << (i - 1))][i - 1]);
+    }
+  }
+  for (int i = 1, l, r; i <= m; i++) {
+    cin >> l >> r;
+    ans = 0;
+    for (int j = 20; j >= 0; j--) {
+      if (l + (1 << j) - 1 <= r) {
+        ans = max(ans, d[l][j]);
+        l += 1 << j;
+      }
+    }
+    cout << ans << "\n";
+  }
+  return 0;
+}
