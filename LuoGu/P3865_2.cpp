2b40dcf8966b80ddf731847e1681293f2c11c9b6
Deng Yufan <2695375004@qq.com>
2021-05-30 17:55:34 +0800

    Normal

--- LuoGu/P3865_2.cpp
+++ LuoGu/P3865_2.cpp
@@ -10,18 +10,18 @@ int main() {
   ios::sync_with_stdio(0);
   cin >> n >> m;
   for (int i = 1, j = 0; i <= n; i++) {
-    cin >> f[i][0];          // 读入单位区间的最大值
-    j += i == 1 << (j + 1);  // 最高1位变化
-    v[i] = j;                // 记录最高1位的位权
+    cin >> f[i][0];
+    j += i == 1 << (j + 1);
+    v[i] = j;
   }
   for (int j = 1; j < 20; j++) {                              // 枚举幂
     for (int i = 1; i + (1 << j) <= n + 1; i++) {                // 枚举左界
       f[i][j] = max(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);  // 计算相应区间的最值
     }
   }
-  while (m--) {  // 处理查询
+  while (m--) {
     cin >> l >> r;
-    d = v[r - l + 1];  // 求出覆盖区间的幂
+    d = v[r - l + 1];
     cout << max(f[l][d], f[r - (1 << d) + 1][d]) << '\n';
   }
   return 0;
