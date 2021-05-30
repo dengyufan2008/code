b6ef5505d598a96b31aede077724bf643dc4471f
Deng Yufan <2695375004@qq.com>
2021-05-30 18:03:23 +0800

    Normal

--- LuoGu/T180185.cpp
+++ LuoGu/T180185.cpp
@@ -13,7 +13,7 @@ int main() {
     cin >> d[i][0];
   }
   for (int i = 1; (1 << i) <= n; i++) {
-    for (int j = 1; j <= n - (1 << i) + 1; j++) {
+    for (int j = 1; j <= n; j++) {
       d[j][i] = d[d[j][i - 1]][i - 1];
     }
   }
