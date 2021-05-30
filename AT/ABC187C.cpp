e59e430ed35a98c18e7468366b88bb576546d8a7
Deng Yufan <2695375004@qq.com>
2021-05-30 19:15:36 +0800

    Normal

--- AT/ABC187C.cpp
+++ AT/ABC187C.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+int n;
+string tmp;
+vector<string> v;
+set<string> s;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> tmp;
+    if (tmp[0] == '!') {
+      s.insert(tmp.substr(1));
+    } else {
+      v.push_back(tmp);
+    }
+  }
+  for (int i = 0; i < v.size(); i++) {
+    if (s.find(v[i]) != s.end()) {
+      cout << v[i] << endl;
+      return 0;
+    }
+  }
+  cout << "satisfiable\n";
   return 0;
 }
