d80399860336b0ab3b3212031b887f3df41fef26
Deng Yufan <2695375004@qq.com>
2021-05-30 20:04:48 +0800

    Normal

--- AT/ABC203B.cpp
+++ AT/ABC203B.cpp
@@ -3,9 +3,17 @@
 
 using namespace std;
 
+int n, k, ans;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= k; j++) {
+      ans += i * 100 + j;
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
