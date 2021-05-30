30d2d79264b622cae4a42d22758ea8759606dfc5
Deng Yufan <2695375004@qq.com>
2021-05-30 18:07:32 +0800

    Normal

--- LuoGu/T180269.cpp
+++ LuoGu/T180269.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+int n, m, x, k, num, ans, d[100001][21];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    cin >> d[i][0];
+  }
+  for (int i = 1; (1 << i) <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      d[j][i] = d[d[j][i - 1]][i - 1];
+    }
+  }
+  for (int i = 1; i <= m; i++) {
+    cin >> x >> k;
+    ans = x, num = 0;
+    for (int j = 20; j >= 0; j--) {
+      if (num + (1 << j) <= k) {
+        ans = d[ans][j], num += 1 << j;
+      }
+    }
+    cout << ans << "\n";
+  }
   return 0;
 }
