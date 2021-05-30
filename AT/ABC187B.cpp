b559bc7b77763d3a02c2607fa9d0fb5ef1ee8e9b
Deng Yufan <2695375004@qq.com>
2021-05-30 18:24:21 +0800

    Normal

--- AT/ABC187B.cpp
+++ AT/ABC187B.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+struct V {
+  int x, y;
+} v[1001];
+int n, ans;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> v[i].x >> v[i].y;
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      ans += abs(v[i].x - v[j].x) >= abs(v[i].y - v[j].y);
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
