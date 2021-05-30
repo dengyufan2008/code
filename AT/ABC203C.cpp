34dcd38e5f300102961473cd37dc2f19d478a0cd
Deng Yufan <2695375004@qq.com>
2021-05-30 20:15:56 +0800

    Normal

--- AT/ABC203C.cpp
+++ AT/ABC203C.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+struct A {
+  LL a, b;
+} v[200001];
+LL n, k;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> k;
+  for (LL i = 1; i <= n; i++) {
+    cin >> v[i].a >> v[i].b;
+  }
+  v[n + 1] = {(LL)1e20 + 1, 0};
+  sort(v + 1, v + n + 1, [](A i, A j) {
+    return i.a < j.a;
+  });
+  for (LL i = 1, p = 0; i <= n + 1; i++) {
+    if (k < v[i].a - p) {
+      cout << p + k << endl;
+      return 0;
+    }
+    k += p - v[i].a + v[i].b;
+    p = v[i].a;
+  }
   return 0;
 }
