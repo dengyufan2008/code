1e17409c719c412e51af159e5c587da0a49f79e8
Deng Yufan <2695375004@qq.com>
2021-05-30 20:03:13 +0800

    Normal

--- AT/ABC203A.cpp
+++ AT/ABC203A.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+int a, b, c;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> a >> b >> c;
+  if (a == b) {
+    cout << c << endl;
+  } else if (a == c) {
+    cout << b << endl;
+  } else if (b == c) {
+    cout << a << endl;
+  } else {
+    cout << 0 << endl;
+  }
   return 0;
 }
