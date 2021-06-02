20ab79cdad34fbd52c9038388b2299646a468df2
Deng Yufan <2695375004@qq.com>
2021-06-02 20:48:12 +0800

    Normal

--- Other/dui_sort.cpp
+++ Other/dui_sort.cpp
@@ -4,24 +4,27 @@
 using namespace std;
 
 struct V {
-  int d;
-  string s;
+  int d, x;
 } v[100001];
 struct H {
   int n, a[100001];
 
+  bool Cmp(int x, int y) {
+    return v[x].d < v[y].d || v[x].d == v[y].d && v[x].x < v[y].x;
+  }
+
   int Son(int x) {
-    return x + (x < n && v[a[x]].d > v[a[x + 1]].d);
+    return x + (x < n && Cmp(a[x + 1], a[x]));
   }
 
   void Up() {
-    for (int i = n, j = i >> 1; j && v[a[i]].d < v[a[j]].d; i = j, j = i >> 1) {
+    for (int i = n, j = i >> 1; j && Cmp(a[i], a[j]); i = j, j = i >> 1) {
       swap(a[i], a[j]);
     }
   }
 
   void Down() {
-    for (int i = 1, j = Son(i << 1); j <= n && v[a[i]].d > v[a[j]].d; i = j, j = Son(i << 1)) {
+    for (int i = 1, j = Son(i << 1); j <= n && Cmp(a[j], a[i]); i = j, j = Son(i << 1)) {
       swap(a[i], a[j]);
     }
   }
@@ -49,11 +52,11 @@ int main() {
   ios::sync_with_stdio(false);
   cin >> n;
   for (int i = 1; i <= n; i++) {
-    cin >> v[i].d >> v[i].s;
+    cin >> v[i].d >> v[i].x;
     h.Push();
   }
   for (int i = 1; i <= n; i++) {
-    cout << v[h.Top()].d << " " << v[h.Top()].s << endl;
+    cout << v[h.Top()].d << " " << v[h.Top()].x << endl;
     h.Pop();
   }
   return 0;
