3bc56b28637f05f26c4cc731adea19739e8007b0
Deng Yufan <2695375004@qq.com>
2021-06-03 20:51:35 +0800

    Normal

--- AT/ABC185B.cpp
+++ AT/ABC185B.cpp
@@ -3,15 +3,9 @@
 
 using namespace std;
 
-int mn = 101;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  for (int i = 1, tmp; i <= 4; i++) {
-    cin >> tmp;
-    mn = min(mn, tmp);
-  }
-  cout << mn << endl;
+  
   return 0;
 }
