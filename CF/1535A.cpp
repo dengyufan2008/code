e40e31f2788b8a4eb872ee45c313dd494a528137
Deng Yufan <2695375004@qq.com>
2021-06-04 22:45:50 +0800

    Normal

--- CF/1535A.cpp
+++ CF/1535A.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+int t, a, b, s[5];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> t;
+  while (t--) {
+    a = b = 0;
+    for (int i = 1; i <= 4; i++) {
+      cin >> s[i];
+    }
+    for (int i = 1; i <= 4; i++) {
+      if (s[i] > s[a]) {
+        b = a, a = i;
+      } else if (s[i] > s[b]) {
+        b = i;
+      }
+    }
+    cout << (a + b == 3 || a == 3 && b == 4 || a == 4 && b == 3 ? "NO" : "YES") << endl;
+  }
   return 0;
 }
