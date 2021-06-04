da8aaddc1c88849ebc89808eee0a901f91416075
Deng Yufan <2695375004@qq.com>
2021-06-04 13:57:22 +0800

    Normal

--- AT/ABC185D.cpp
+++ AT/ABC185D.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+int n, m, k, s, a[200002];
+
 int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(false);
-  
+  cin >> n >> m;
+  for (int i = 1; i <= m; i++) {
+    cin >> a[i];
+  }
+  sort(a + 1, a + 1 + m);
+  a[++m] = n + 1;
+  k = n;
+  for (int i = 1; i <= m; i++) {
+    if (a[i] > a[i - 1] + 1) {
+      k = min(k, a[i] - a[i - 1] - 1);
+    }
+  }
+  for (int i = 1; i <= m; i++) {
+    if (a[i] > a[i - 1] + 1) {
+      s += 1 + (a[i] - a[i - 1] - 2) / k;
+    }
+  }
+  cout << s << endl;
   return 0;
 }
