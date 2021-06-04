214384a29c3c6ec66332e40c61884a792e470f8b
Deng Yufan <2695375004@qq.com>
2021-06-04 17:35:09 +0800

    Normal

--- LuoGu/P3865.cpp
+++ LuoGu/P3865.cpp
@@ -3,14 +3,15 @@
 
 using namespace std;
 
-int n, m, ans, d[100001][21];
+int n, m, lg[100001], d[100001][21];
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> n >> m;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1, p = 0; i <= n; i++) {
     cin >> d[i][0];
+    lg[i] = p += i == 1 << p + 1;
   }
   for (int i = 1; (1 << i) <= n; i++) {
     for (int j = 1; j <= n - (1 << i) + 1; j++) {
@@ -19,14 +20,7 @@ int main() {
   }
   for (int i = 1, l, r; i <= m; i++) {
     cin >> l >> r;
-    ans = 0;
-    for (int j = 20; j >= 0; j--) {
-      if (l + (1 << j) - 1 <= r) {
-        ans = max(ans, d[l][j]);
-        l += 1 << j;
-      }
-    }
-    cout << ans << "\n";
+    cout << max(d[l][lg[r - l + 1]], d[r - (1 << lg[r - l + 1]) + 1][lg[r - l + 1]]) << "\n";
   }
   return 0;
 }
