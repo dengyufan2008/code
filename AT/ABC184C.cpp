3a6309155ee8aa2008a957212d6789d0cb410d68
Deng Yufan <2695375004@qq.com>
2021-06-04 17:49:28 +0800

    Normal

--- AT/ABC184C.cpp
+++ AT/ABC184C.cpp
@@ -3,16 +3,9 @@
 
 using namespace std;
 
-int n, x;
-string s;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> n >> x >> s;
-  for (int i = 0; i < n; i++) {
-    x = max(x + (s[i] == 'o' ? 1 : -1), 0);
-  }
-  cout << x << endl;
+  
   return 0;
 }
