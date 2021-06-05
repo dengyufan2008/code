1f5005f5fd30059a9f47adc2cc7c6bc398b8de04
Deng Yufan <2695375004@qq.com>
2021-06-05 18:10:48 +0800

    Noraml

--- Ybt/1236.cpp
+++ Ybt/1236.cpp
@@ -3,16 +3,9 @@
 
 using namespace std;
 
-int k;
-string s;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> k;
-  while (k--) {
-    cin >> s;
-    
-  }
+  
   return 0;
 }
