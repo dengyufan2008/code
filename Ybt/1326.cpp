771ba55f9e0217c54ac47ff3c161d1915fc03b6b
Deng Yufan <2695375004@qq.com>
2021-06-05 17:37:07 +0800

    Normal

--- Ybt/1326.cpp
+++ Ybt/1326.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
+LL b, p, k, tmp, ans = 1;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> b >> p >> k;
+  tmp = b % k;
+  for (LL i = 1; i <= p; i <<= 1) {
+    if (p & i) {
+      ans = ans * tmp % k;
+    }
+    tmp = tmp * tmp % k;
+  }
+  cout << b << "^" << p << " mod " << k << "=" << ans << endl;
   return 0;
 }
