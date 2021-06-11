23de6ad1c4552b4b4c79beb96b51c92b9f1a149b
Deng Yufan <2695375004@qq.com>
2021-06-11 17:45:59 +0800

    Normal

--- CF/1256B.cpp
+++ CF/1256B.cpp
@@ -3,16 +3,9 @@
 
 using namespace std;
 
-int t, a, b, n, s;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> t;
-  while (t--) {
-    cin >> a >> b >> n >> s;
-    s -= min(a, s / n) * n;
-    cout << (s <= b ? "YES" : "NO") << endl;
-  }
+  
   return 0;
 }
