3f44d6ed1f12707444e31d60e3438e69d1c70b5c
Deng Yufan <2695375004@qq.com>
2021-06-12 19:01:07 +0800

    Normal

--- CF/1256C.cpp
+++ CF/1256C.cpp
@@ -3,30 +3,40 @@
 
 using namespace std;
 
-int n, m, d, p, c[1001], ans[2001];
+int n, m, d, sum, s, t = 1, w[2001], ans[2001];
+bool flag;
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> n >> m >> d;
   for (int i = 1; i <= m; i++) {
-    cin >> c[i];
-    if (c[i] > d) {
-      cout << "NO" << endl;
+    cin >> w[i];
+    sum += w[i];
+  }
+  while (s <= n) {
+    s = s + d + sum - 1 <= n ? s + d : n - sum + 1;
+    if (s > n) {
+      break;
+    }
+    if (t <= m) {
+      for (int i = s; i < s + w[t]; i++) {
+        ans[i] = t;
+      }
+      s += w[t] - 1;
+      sum -= w[t++];
+    } else {
+      flag = true;
+      break;
     }
   }
-  for (int i = 1; i <= m && p <= n; i++) {
-    p += d;
-    fill(&ans[p], &ans[p + c[i] - 1] + 1, i);
-    p += c[i] - 1;
-  }
-  if (p > n) {
+  if (flag) {
+    cout << "NO" << endl;
+  } else {
     cout << "YES" << endl;
     for (int i = 1; i <= n; i++) {
       cout << ans[i] << " ";
     }
-  } else {
-    cout << "NO" << endl;
   }
   return 0;
 }
