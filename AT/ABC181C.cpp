542f75b2aa660a6c1fc1225e1aff9429fa441e2c
Deng Yufan <2695375004@qq.com>
2021-06-14 14:05:13 +0800

    Normal

--- AT/ABC181C.cpp
+++ AT/ABC181C.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
+int n, x[101], y[101];
+
+bool C() {
+  for (int i = 1; i <= n; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      for (int k = j + 1; k <= n; k++) {
+        if ((x[i] - x[k]) * (y[j] - y[k]) == (x[j] - x[k]) * (y[i] - y[k])) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> x[i] >> y[i];
+  }
+  cout << (C() ? "Yes" : "No") << endl;
   return 0;
 }
