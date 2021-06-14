3bf68dbd5e7d3f8ac44a901adcdbae4842993c7d
Deng Yufan <2695375004@qq.com>
2021-06-14 13:31:05 +0800

    Normal

--- AT/ABC181B.cpp
+++ AT/ABC181B.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-int n;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> n;
-  cout << (n & 1 ? "Black" : "White") << endl;
+  
   return 0;
 }
