441159c7b9fe525ace803415288721a0d2e78e1b
Deng Yufan <2695375004@qq.com>
2021-06-14 11:31:26 +0800

    Normal

--- AT/ABC183D.cpp
+++ AT/ABC183D.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+LL n, w, a[200002];
+
+bool C() {
+  for (LL i = 1; i <= 200001; i++) {
+    a[i] += a[i - 1];
+    if (a[i] > w) {
+      return false;
+    }
+  }
+  return true;
+}
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> w;
+  for (LL i = 1, s, t, p; i <= n; i++) {
+    cin >> s >> t >> p;
+    a[s + 1] += p, a[t + 1] -= p;
+  }
+  cout << (C() ? "Yes" : "No") << endl;
   return 0;
 }
