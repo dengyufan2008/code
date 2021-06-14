927f76edffa0cc74f57d47caffba50247818219a
Deng Yufan <2695375004@qq.com>
2021-06-14 12:22:38 +0800

    Normal

--- AT/ABC182B.cpp
+++ AT/ABC182B.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-int a, b;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> a >> b;
-  cout << a * 2 + 100 - b << endl;
+  
   return 0;
 }
