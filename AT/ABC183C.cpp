eeccb2c65046dd68812d6d0d9be6b5ff4edf5f63
Deng Yufan <2695375004@qq.com>
2021-06-14 10:59:43 +0800

    Normal

--- AT/ABC183C.cpp
+++ AT/ABC183C.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-long double sx, sy, gx, gy;
-
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  cin >> sx >> sy >> gx >> gy;
-  cout << fixed << setprecision(10) << (sx * gy + gx * sy) / (sy + gy) << endl;
+  
   return 0;
 }
