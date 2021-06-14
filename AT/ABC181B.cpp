37ea412423fe17cb0057a19b706d1bc811887829
Deng Yufan <2695375004@qq.com>
2021-06-14 13:34:28 +0800

    Normal

--- AT/ABC181B.cpp
+++ AT/ABC181B.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
+LL n, ans, a[100001], b[100001];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i] >> b[i];
+    ans += (a[i] + b[i]) * (b[i] - a[i] + 1) / 2;
+  }
+  cout << ans << endl;
   return 0;
 }
