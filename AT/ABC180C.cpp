914108e57089ad45a417f89b301b5bfda0a3d697
Deng Yufan <2695375004@qq.com>
2021-06-15 21:13:49 +0800

    Normal

--- AT/ABC180C.cpp
+++ AT/ABC180C.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+LL n, k, a[3000001];
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (LL i = 1; i * i <= n; i++) {
+    if (!(n % i)) {
+      a[++k] = i;
+      if (i * i != n) {
+        a[++k] = n / i;
+      }
+    }
+  }
+  sort(a + 1, a + k + 1);
+  for (LL i = 1; i <= k; i++) {
+    cout << a[i] << endl;
+  }
   return 0;
 }
