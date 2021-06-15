5390789b8d119e1e5ee30d4e1e2bbe6937f68dfd
Deng Yufan <2695375004@qq.com>
2021-06-15 20:24:30 +0800

    Normal

--- AT/ABC180B.cpp
+++ AT/ABC180B.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
+LL n, tmp, m, c;
+double e;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> tmp;
+    m += abs(tmp), e += abs(tmp) * abs(tmp), c = max(abs(tmp), c);
+  }
+  cout << m << endl;
+  printf("%.15lf\n", sqrt(e));
+  cout << c << endl;
   return 0;
 }
