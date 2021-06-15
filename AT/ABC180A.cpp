0be56786a030723141460e35cfee29e6b79df341
Deng Yufan <2695375004@qq.com>
2021-06-15 14:27:45 +0800

    NOrmal

--- AT/ABC180A.cpp
+++ AT/ABC180A.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
+int n, a, b;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  cin >> n >> a >> b;
+  cout << n - a + b << endl;
   return 0;
 }
