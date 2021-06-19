c783a9f4ca1c6cf64af6dc71be925cf9565773a2
Deng Yufan <2695375004@qq.com>
2021-06-19 17:31:23 +0800

    Normal

--- Other/bing_cha_ji.cpp
+++ Other/bing_cha_ji.cpp
@@ -3,9 +3,46 @@
 
 using namespace std;
 
+struct H {
+  int f[100001];
+
+  void Spawn() {
+    for (int i = 1; i <= 100000; i++) {
+      f[i] = i;
+    }
+  }
+
+  int Father(int x) {
+    if (f[x] != x) {
+      f[x] = Father(f[x]);
+    }
+    return f[x];
+  }
+
+  void Push(int x, int y) {
+    f[y] = x;
+  }
+
+  bool Same(int x, int y) {
+    return Father(x) == Father(y);
+  }
+} h;
+
+int n, q;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
-  
+  h.Spawn();
+  cin >> n;
+  for (int i = 1, a, b; i <= n; i++) {
+    cin >> a >> b;
+    h.Push(a, b);
+  }
+  cin >> q;
+  for (int i = 1, a, b; i <= q; i++) {
+    cin >> a >> b;
+    cout << (h.Same(a, b) ? "Yes" : "No") << endl;
+  }
   return 0;
 }
