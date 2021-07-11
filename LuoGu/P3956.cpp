01bf7fb9fb8dfe97f06cbd69912c62ce570fb590
Deng Yufan <2695375004@qq.com>
2021-07-11 12:48:54 +0800

    Normal

--- LuoGu/P3956.cpp
+++ LuoGu/P3956.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-const int kMove[12][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {2, 0}, {0, 2}, {-2, 0}, {0, -2}};
+const LL kMove[12][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {2, 0}, {0, 2}, {-2, 0}, {0, -2}};
 
-int m, n, a[1001][1001], d[1001][1001];
+LL m, n, a[1001][1001], d[1001][1001];
 
-void S(int x, int y, int c) {
+void S(LL x, LL y, LL c) {
   if (x < 1 || y < 1 || x > m || y > m || !a[x][y] || c >= d[x][y]) {
     return;
   }
   d[x][y] = c;
-  for (int i = 0; i <= 11; i++) {
+  for (LL i = 0; i <= 11; i++) {
     S(x + kMove[i][0], y + kMove[i][1], c + (a[x][y] != a[x + kMove[i][0]][y + kMove[i][1]]) + (i >= 4) * 2);
   }
 }
@@ -21,12 +21,16 @@ int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(false);
   cin >> m >> n;
-  for (int i = 1, x, y, c; i <= n; i++) {
+  for (LL i = 1, x, y, c; i <= n; i++) {
     cin >> x >> y >> c;
     a[x][y] = c + 1;
   }
   fill(&d[0][0], &d[1000][1000] + 1, 114514);
   S(1, 1, 0);
+  if (!a[m][m]) {
+    d[m][m] = min(d[m][m], min(d[m][m - 1], d[m - 1][m]) + 2);
+  }
   cout << (d[m][m] == 114514 ? -1 : d[m][m]) << endl;
   return 0;
 }
+//今天记得找胖教练填一份那个防止溺水的资料，要签名，然后还要辛苦帮教练拍个照片给妈妈，妈妈要发给童老师。
