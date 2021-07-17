9e025aa687cf063f4e223651816f39162d433602
Deng Yufan <2695375004@qq.com>
2021-07-17 21:03:17 +0800

    Normal

--- LuoGu/P2937.cpp
+++ LuoGu/P2937.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
+#include <queue>
 #define LL long long
 
 using namespace std;
 
 const int kMove[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+struct A {
+  int x, y, d, s;
+};
 int w, h, m, n, ans = 114514;
 bool b[101][101];
 char ch[101][101];
-
-void S(int x, int y, int d, int s) {
-  if (x < 1 || y < 1 || x > w || y > h || b[x][y] || ch[x][y] == '*') {
-    return;
-  }
-  if (ch[x][y] == 'C' && (x != m || y != n)) {
-    ans = min(ans, s);
-    return;
-  }
-  b[x][y] = 1;
-  for (int i = 0; i <= 3; i++) {
-    S(x + kMove[i][0], y + kMove[i][1], i, s + (i != d));
-  }
-  b[x][y] = 0;
-}
+queue<A> q;
 
 int main() {
   cin >> w >> h;
@@ -33,7 +23,22 @@ int main() {
       }
     }
   }
-  S(m, n, 4, 0);
+  q.push({m, n, 4, 0});
+  while (!q.empty()) {
+    A c = q.front();
+    q.pop();
+    if (c.x < 1 || c.y < 1 || c.x > w || c.y > h || b[c.x][c.y] || ch[c.x][c.y] == '*') {
+      continue;
+    }
+    b[c.x][c.y] = 1;
+    if (ch[c.x][c.y] == 'C' && (c.x != m || c.y != n)) {
+      ans = min(ans, c.s);
+      continue;
+    }
+    for (int i = 0; i <= 3; i++) {
+      q.push({c.x + kMove[i][0], c.y + kMove[i][1], i, c.s + (i != c.d)});
+    }
+  }
   cout << ans - 1 << endl;
   return 0;
 }
