e1c4f416068fe290f31d3831320536c28ebac26f
Deng Yufan <2695375004@qq.com>
2021-07-18 18:16:24 +0800

    Normal

--- CF/1070A.cpp
+++ CF/1070A.cpp
@@ -4,32 +4,44 @@
 
 using namespace std;
 
-struct A{
+struct A {
   int d, s;
-  string ans;
 };
-int d, s;
+struct V {
+  int x, y;
+  char ch;
+} v[501][5001];
+int d, s, m = -1;
 bool b[501][5001];
 queue<A> q;
 
+void Print(int x, int y) {
+  if (y) {
+    Print(v[x][y].x, v[x][y].y);
+    cout << v[x][y].ch;
+  }
+}
+
 int main() {
   cin >> d >> s;
-  q.push({0, 0, ""});
+  q.push({0, 0});
   while (!q.empty()) {
     A c = q.front();
     q.pop();
-    if (c.s > s || b[c.d][c.s]) {
-      continue;
-    }
-    if (c.s == s && !c.d) {
-      cout << c.ans << endl;
-      return 0;
-    }
-    b[c.d][c.s] = 1;
     for (int i = 0; i <= 9; i++) {
-      q.push({(c.d * 10 + i) % d, c.s + i, c.ans + (char)(i + '0')});
+      if (c.s + i <= s && !b[(c.d * 10 + i) % d][c.s + i]) {
+        b[(c.d * 10 + i) % d][c.s + i] = 1;
+        q.push({(c.d * 10 + i) % d, c.s + i});
+        v[(c.d * 10 + i) % d][c.s + i] = {c.d, c.s, (char)(i + '0')};
+      }
     }
   }
-  cout << -1 << endl;
+  b[0][0] = 1;
+  if (b[0][s]) {
+    Print(0, s);
+    cout << endl;
+  } else {
+    cout << -1 << endl;
+  }
   return 0;
 }
