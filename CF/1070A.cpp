c0ae6dc14755f9ef99a34360208ba37a2f2c19ec
Deng Yufan <2695375004@qq.com>
2021-07-18 13:39:11 +0800

    Normal

--- CF/1070A.cpp
+++ CF/1070A.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <queue>
 #define LL long long
 
 using namespace std;
 
+struct A{
+  int d, s;
+  string ans;
+};
+int d, s;
+bool b[501][5001];
+queue<A> q;
+
 int main() {
-  
+  cin >> d >> s;
+  q.push({0, 0, ""});
+  while (!q.empty()) {
+    A c = q.front();
+    q.pop();
+    if (c.s > s || b[c.d][c.s]) {
+      continue;
+    }
+    if (c.s == s && !c.d) {
+      cout << c.ans << endl;
+      return 0;
+    }
+    b[c.d][c.s] = 1;
+    for (int i = 0; i <= 9; i++) {
+      q.push({(c.d * 10 + i) % d, c.s + i, c.ans + (char)(i + '0')});
+    }
+  }
+  cout << -1 << endl;
   return 0;
 }
