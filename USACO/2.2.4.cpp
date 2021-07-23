c177ac042fc052e38b28eb4c37abc0834f2d40c0
Deng Yufan <2695375004@qq.com>
2021-07-23 20:49:02 +0800

    Normal

--- USACO/2.2.4.cpp
+++ USACO/2.2.4.cpp
@@ -1,9 +1,32 @@
+/*
+ID: dengyuf1
+TASK: subset
+LANG: C++
+*/
 #include <iostream>
 #define LL long long
 
 using namespace std;
 
+int n, ans;
+
+void S(int x, int t) {
+  if (x < 0) {
+    return;
+  } else if (!x) {
+    ans++;
+    return;
+  }
+  for (int i = t + 1; i <= n; i++) {
+    S(x - i, i);
+  }
+}
+
 int main() {
-  
+  // freopen("subset.in", "r", stdin);
+  // freopen("subset.out", "w", stdout);
+  cin >> n;
+  S(n - 1, 1);
+  cout << ans << endl;
   return 0;
 }
