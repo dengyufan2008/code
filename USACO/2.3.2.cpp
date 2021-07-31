b911ed42d7764da7550fab3c3bd152d06d9e60af
Deng Yufan <2695375004@qq.com>
2021-07-31 20:34:26 +0800

    Normal

--- USACO/2.3.2.cpp
+++ USACO/2.3.2.cpp
@@ -8,9 +8,23 @@ LANG: C++
 
 using namespace std;
 
+int n, k, f[201][101];
+
 int main() {
   // freopen("nocows.in", "r", stdin);
   // freopen("nocows.out", "w", stdout);
-  
+  cin >> n >> k;
+  f[0][0] = f[1][1] = f[3][2] = 1;
+  for (int i = 4; i <= n; i++) {
+    for (int j = 3; j <= k; j++) {
+      for (int _i = 1; _i < i; _i++) {
+        for (int _j = 1; _j < k - 1; _j++) {
+          f[i][j] = (f[i][j] + f[_i][j - 1] * f[i - _i - 1][_j] * 2) % 9901;
+        }
+        f[i][j] = (f[i][j] + f[_i][k - 1] * f[i - _i - 1][k - 1]) % 9901;
+      }
+    }
+  }
+  cout << f[n][k] << endl;
   return 0;
 }
