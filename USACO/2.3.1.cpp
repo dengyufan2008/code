8782d0d89ade5b1ee76425bae7f60211ff897181
Deng Yufan <2695375004@qq.com>
2021-07-31 19:47:22 +0800

    Normal

--- USACO/2.3.1.cpp
+++ USACO/2.3.1.cpp
@@ -8,9 +8,32 @@ LANG: C++
 
 using namespace std;
 
+int n;
+bool f[200001] = {1};
+string tot = " ", tmp, s[251];
+
 int main() {
-  // freopen("prefix.in", "r", stdin);
-  // freopen("prefix.out", "w", stdout);
-  
+  freopen("prefix.in","r",stdin);
+  freopen("prefix.out","w",stdout);
+  while (1) {
+    cin >> s[++n];
+    if (s[n] == ".") {
+      break;
+    }
+  }
+  while (cin >> tmp) {
+    tot += tmp;
+  }
+  for (int i = 1; i <= tot.size() - 1; i++) {
+    for (int j = 1; j < n; j++) {
+      f[i] |= (i - s[j].size() >= 0 && f[i - s[j].size()] && tot.substr(i + 1 - s[j].size(), s[j].size()) == s[j]);
+    }
+  }
+  for (int i = tot.size() - 1; i >= 0; i--) {
+    if (f[i]) {
+      cout << i << endl;
+      break;
+    }
+  }
   return 0;
 }
---
+++ USACO/2.3.2.cpp
@@ -0,0 +1,16 @@
+/*
+ID: dengyuf1
+TASK: nocows
+LANG: C++
+*/
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("nocows.in", "r", stdin);
+  // freopen("nocows.out", "w", stdout);
+  
+  return 0;
+}
