08dd7189fb8b5ef8064a7f11d1eb2dd00b2fe2fe
Deng Yufan <2695375004@qq.com>
2021-08-08 14:19:09 +0800

    Normal

--- LuoGu/P1616.cpp
+++ LuoGu/P1616.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+struct E {
+  LL t, v;
+} e[10001];
+LL t, m, f[10000001];
+
 int main() {
-  
+  cin >> t >> m;
+  for (LL i = 1; i <= m; i++) {
+    cin >> e[i].t >> e[i].v;
+  }
+  for (LL i = 1; i <= m; i++) {
+    for (LL j = e[i].t; j <= t; j++) {
+      f[j] = max(f[j], f[j - e[i].t] + e[i].v);
+    }
+  }
+  cout << f[t] << endl;
   return 0;
 }
