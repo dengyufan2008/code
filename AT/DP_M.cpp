d271fdb545c9fea9da2fbb50d9cab18f7f392ee1
Deng Yufan <2695375004@qq.com>
2021-08-08 12:04:53 +0800

    Normal

---
+++ AT/DP_M.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+const LL kMod = 1000000007;
+LL n, k, a[102], f[102][100002] = {1};
+
+int main() {
+  cin >> n >> k;
+  for (LL i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (LL i = 1; i <= n; i++) {
+    for (LL j = 0; j <= k; j++) {
+      f[i][j] = (f[i][j] + f[i - 1][j]) % kMod;
+      f[i][j + a[i] + 1] = (f[i][j + a[i] + 1] - f[i - 1][j] + kMod) % kMod;
+    }
+    for (LL j = 1; j <= k; j++) {
+      f[i][j] = (f[i][j] + f[i][j - 1]) % kMod;
+    }
+  }
+  cout << f[n][k] << endl;
+  return 0;
+}
--- LuoGu/P1115.cpp
+++ LuoGu/P1115.cpp
@@ -3,7 +3,17 @@
 
 using namespace std;
 
+LL n, ans = -1145141919810, a[200001], f[201];
+
 int main() {
-  
+  cin >> n;
+  for (LL i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (LL i = 1; i <= n; i++) {
+    f[i] = max(f[i - 1], 0LL) + a[i];
+    ans = max(ans, f[i]);
+  }
+  cout << ans << endl;
   return 0;
 }
