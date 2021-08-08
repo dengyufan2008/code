cccf6dd3cefa3b89b5bc951faefc6a076358f577
Deng Yufan <2695375004@qq.com>
2021-08-08 20:09:08 +0800

    Normal

---
+++ AT/ABC213A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int a, b;
+
+int main() {
+  cin >> a >> b;
+  cout << (a ^ b) << endl;
+  return 0;
+}
--- LuoGu/P4933.cpp
+++ LuoGu/P4933.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
+const int kMod = 998244353;
+int n, ans, h[1001], f[1001][40001];
+
 int main() {
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> h[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j < i; j++) {
+      int d = h[i] - h[j];
+      f[i][d + 20000] = (f[i][d + 20000] + f[j][d + 20000] + 1) % kMod;
+    }
+  }
+  for (int i = 0; i <= 40000; i++) {
+    ans = (ans + f[n][i]) % kMod;
+  }
+  cout << (ans + n) % kMod << endl;
   return 0;
 }
