8b23b12b2c5d14ad2af77496d74514d2c23f66c2
Deng Yufan <2695375004@qq.com>
2021-08-08 20:30:27 +0800

    AllFine

--- AT/ABC213D.cpp
+++ AT/ABC213D.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <queue>
 #define LL long long
 
 using namespace std;
 
+struct V {
+  priority_queue<int> e;
+} v[200001];
+int n;
+
+void S(int f, int x) {
+  cout << x << " ";
+  while (!v[x].e.empty()) {
+    if (-v[x].e.top() != f) {
+      S(x, -v[x].e.top());
+      cout << x << " ";
+    }
+    v[x].e.pop();
+  }
+}
+
 int main() {
-  
+  cin >> n;
+  for (int i = 1, x, y; i < n; i++) {
+    cin >> x >> y;
+    v[x].e.push(-y), v[y].e.push(-x);
+  }
+  S(0, 1);
   return 0;
 }
