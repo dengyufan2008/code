e4b408724e9b7ef43cc74b7e48bce3e7545ce592
Deng Yufan <2695375004@qq.com>
2021-08-08 20:13:49 +0800

    Normal

--- AT/ABC213B.cpp
+++ AT/ABC213B.cpp
@@ -3,7 +3,18 @@
 
 using namespace std;
 
+int n, mx1, mx2, a[200001];
+
 int main() {
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    if (a[i] > a[mx1]) {
+      mx2 = mx1, mx1 = i;
+    } else if (a[i] > a[mx2]) {
+      mx2 = i;
+    }
+  }
+  cout << mx2 << endl;
   return 0;
 }
