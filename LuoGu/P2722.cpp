c72189e0ca00f6fbb9c98647ddae765387a1a385
Deng Yufan <2695375004@qq.com>
2021-08-08 14:38:09 +0800

    Normal

--- LuoGu/P2722.cpp
+++ LuoGu/P2722.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
+struct E {
+  int p, t;
+} e[10001];
+int m, n, f[100000001];
+
 int main() {
-  
+  cin >> m >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> e[i].p >> e[i].t;
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      if (j >= e[i].t) {
+        f[j] = max(f[j], f[j - e[i].t] + e[i].p);
+      }
+    }
+  }
+  cout << f[m] << endl;
   return 0;
 }
