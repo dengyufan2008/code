8d6abb265bc5c507c89856e49fae46444703eed1
Deng Yufan <2695375004@qq.com>
2021-08-08 09:05:08 +0800

    Normal

--- AT/DP_A.cpp
+++ AT/DP_A.cpp
@@ -3,7 +3,18 @@
 
 using namespace std;
 
+LL n, h[100002], f[100002];
+
 int main() {
-  
+  cin >> n;
+  for (LL i = 1; i <= n; i++) {
+    cin >> h[i];
+  }
+  fill(&f[2], &f[100000] + 1, 1145141919810);
+  for (LL i = 1; i < n; i++) {
+    f[i + 1] = min(f[i + 1], f[i] + abs(h[i] - h[i + 1]));
+    f[i + 2] = min(f[i + 2], f[i] + abs(h[i] - h[i + 2]));
+  }
+  cout << f[n] << endl;
   return 0;
 }
