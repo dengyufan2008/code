bf03935c29d7af4fa3b2a801e02f44f6f8dd8ddb
Deng Yufan <2695375004@qq.com>
2021-08-09 16:21:35 +0800

    AllFine

--- AT/DP_L.cpp
+++ AT/DP_L.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
+LL n, a[3002], d[3002], f[3002][3002];
+
 int main() {
-  
+  cin >> n;
+  for (LL i = 1; i <= n; i++) {
+    cin >> a[i];
+    d[i] = d[i - 1] + a[i];
+  }
+  for (LL i = 1; i <= n; i++) {
+    for (LL j = 1; j <= n; j++) {
+      f[j][j + i - 1] = d[j + i - 1] - d[j - 1] - min(f[j + 1][j + i - 1], f[j][j + i - 2]);
+    }
+  }
+  cout << f[1][n] * 2 - d[n] << endl;
   return 0;
 }
