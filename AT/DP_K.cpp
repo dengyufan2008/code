b04f466173e022e5c8b01bb5319a4ba78163e01f
Deng Yufan <2695375004@qq.com>
2021-08-09 15:41:28 +0800

    AllFine

--- AT/DP_K.cpp
+++ AT/DP_K.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
+int n, k, a[101];
+bool f[100001];
+
 int main() {
-  
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= k; i++) {
+    for (int j = 1; j <= n; j++) {
+      f[i] |= (i >= a[j] && !f[i - a[j]]);
+    }
+  }
+  cout << (f[k] ? "First" : "Second") << endl;
   return 0;
 }
