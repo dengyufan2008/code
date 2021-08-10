1b8ecfba42a7ed825fd7be1f8dabb760f432ad7f
Deng Yufan <2695375004@qq.com>
2021-08-10 13:52:41 +0800

    AllFine

--- LuoGu/P2782.cpp
+++ LuoGu/P2782.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct E {
   int a, b;
 } e[200001];
-int n;
+int n, ans, f[200001];
 
 int main() {
   cin >> n;
@@ -17,6 +17,22 @@ int main() {
   sort(e + 1, e + n + 1, [](E i, E j) {
     return i.a < j.a;
   });
-  
+  for (int i = 1; i <= n; i++) {
+    if (e[i].b > f[ans]) {
+      f[++ans] = e[i].b;
+    } else {
+      int l = 0, r = ans, mid;
+      while (l <= r) {
+        mid = (l + r) >> 1;
+        if (f[mid] <= e[i].b) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+      f[l] = min(f[l], e[i].b);
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
