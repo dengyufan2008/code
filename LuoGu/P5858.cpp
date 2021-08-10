b4d7ff93f2a67c845ce23f982adbc3004455930b
Deng Yufan <2695375004@qq.com>
2021-08-10 16:53:12 +0800

    AllFine

--- LuoGu/P5858.cpp
+++ LuoGu/P5858.cpp
@@ -3,7 +3,24 @@
 
 using namespace std;
 
+LL n, w, s, ans = -1145141919810, a[5001], f[5001][5001];  //前i个原料,锅里还剩j个
+
 int main() {
-  
+  cin >> n >> w >> s;
+  for (LL i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  fill(&f[1][0], &f[5000][5000] + 1, -1145141919810);
+  for (LL i = 1; i <= n; i++) {
+    for (LL j = 1; j <= min(i, w); j++) {
+      for (LL k = 0; k <= min(i - j, s); k++) {
+        f[i][j] = max(f[i][j], f[i - 1][j + k - 1] + a[i] * j);
+      }
+    }
+  }
+  for (LL i = 0; i <= w; i++) {
+    ans = max(ans, f[n][i]);
+  }
+  cout << ans << endl;
   return 0;
 }
