da0d20827870b590e6112f2b607bfdcf2fa550c8
Deng Yufan <2695375004@qq.com>
2021-08-10 13:05:08 +0800

    AllFine

--- LuoGu/P2782.cpp
+++ LuoGu/P2782.cpp
@@ -1,9 +1,22 @@
+#include <algorithm>
 #include <iostream>
 #define LL long long
 
 using namespace std;
 
+struct E {
+  int a, b;
+} e[200001];
+int n;
+
 int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> e[i].a >> e[i].b;
+  }
+  sort(e + 1, e + n + 1, [](E i, E j) {
+    return i.a < j.a;
+  });
   
   return 0;
 }
