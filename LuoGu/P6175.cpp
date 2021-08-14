cf42b135e0a66b0a231aaccb9ff31d3cf5e89645
Deng Yufan <2695375004@qq.com>
2021-08-14 14:40:32 +0800

    AllFine

--- LuoGu/P6175.cpp
+++ LuoGu/P6175.cpp
@@ -7,24 +7,24 @@ const LL kInf = 0x7fffffffffffff;
 LL n, m, ans = kInf, a[101][101], f[101][101];
 
 int main() {
+  cin >> n >> m;
   for (int i = 1; i <= 100; i++) {
     for (int j = 1; j <= 100; j++) {
       f[i][j] = kInf * (i != j);
     }
   }
-  cin >> n >> m;
   for (LL i = 1, x, y, z; i <= m; i++) {
     cin >> x >> y >> z;
     f[x][y] = f[y][x] = a[x][y] = a[y][x] = z;
   }
-  for (LL i = 1; i <= n; i++) {
-    for (LL j = 1; j < i; j++) {
-      for (LL k = j + 1; k < i; k++) {
-        ans = min(ans, f[i][j] + a[i][k] + a[j][k]);
+  for (LL k = 1; k <= n; k++) {
+    for (LL i = 1; i < k; i++) {
+      for (LL j = i + 1; j < k; j++) {
+        ans = min(ans, f[i][j] + a[j][k] + a[k][i]);
       }
     }
-    for (LL j = 1; j <= n; j++) {
-      for (LL k = 1; k <= n; k++) {
+    for (LL i = 1; i <= n; i++) {
+      for (LL j = 1; j <= n; j++) {
         f[i][j] = min(f[i][j], f[i][k] + f[k][j]);
       }
     }
