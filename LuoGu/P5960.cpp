9204f64c34d249d982c9b82ef05f270030552dd6
Deng Yufan <2695375004@qq.com>
2021-08-14 13:01:10 +0800

    AllFine

--- LuoGu/P5960.cpp
+++ LuoGu/P5960.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
+#include <queue>
 #define LL long long
 
 using namespace std;
 
+struct V {
+  int d = 1, t;
+  bool b;
+  vector<pair<int, int>> e;
+} v[5001];
+int n, m;
+bool flag;
+queue<int> q;
+
+bool Record(int x, int d) {
+  if (v[x].d > d) {
+    v[x].d = d;
+    if (!v[x].b) {
+      v[x].t += v[x].b = 1;
+      q.push(x);
+    }
+  }
+  return v[x].t < n;
+}
+
+bool Spfa() {
+  for (int i = 1; i <= n; i++) {
+    Record(i, 0);
+  }
+  for (; !q.empty(); q.pop()) {
+    int c = q.front();
+    v[c].b = 0;
+    for (pair<int, int> j : v[c].e) {
+      if (!Record(j.first, v[c].d + j.second)) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
 int main() {
-  
+  cin >> n >> m;
+  for (int i = 1, x, y, z; i <= m; i++) {
+    cin >> x >> y >> z;
+    v[y].e.push_back(make_pair(x, z));
+  }
+  if (Spfa()) {
+    for (int i = 1; i <= n; i++) {
+      cout << v[i].d << " ";
+    }
+  } else {
+    cout << "NO" << endl;
+  }
   return 0;
 }
--- LuoGu/P6175.cpp
+++ LuoGu/P6175.cpp
@@ -3,7 +3,36 @@
 
 using namespace std;
 
+const LL kInf = 0x7fffffffffffff;
+LL n, m, ans = kInf, a[101][101], f[101][101];
+
 int main() {
-  
+  for (int i = 1; i <= 100; i++) {
+    for (int j = 1; j <= 100; j++) {
+      f[i][j] = kInf * (i != j);
+    }
+  }
+  cin >> n >> m;
+  for (LL i = 1, x, y, z; i <= m; i++) {
+    cin >> x >> y >> z;
+    f[x][y] = f[y][x] = a[x][y] = a[y][x] = z;
+  }
+  for (LL i = 1; i <= n; i++) {
+    for (LL j = 1; j < i; j++) {
+      for (LL k = j + 1; k < i; k++) {
+        ans = min(ans, f[i][j] + a[i][k] + a[j][k]);
+      }
+    }
+    for (LL j = 1; j <= n; j++) {
+      for (LL k = 1; k <= n; k++) {
+        f[i][j] = min(f[i][j], f[i][k] + f[k][j]);
+      }
+    }
+  }
+  if (ans == kInf) {
+    cout << "No solution." << endl;
+  } else {
+    cout << ans << endl;
+  }
   return 0;
 }
