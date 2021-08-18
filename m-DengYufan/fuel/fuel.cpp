d828e1b955f2992bed8a931565af1d19e236ac25
Deng Yufan <2695375004@qq.com>
2021-08-18 09:13:30 +0800

    AllFine

--- m-DengYufan/fuel/fuel.cpp
+++ m-DengYufan/fuel/fuel.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct E {
   LL x, v;
 } e[50002];
-LL n, g, b, d, f[502][1002];
+LL n, g, b, d, f[2][1000001];
 
 int main() {
   // freopen("fuel.in", "r", stdin);
@@ -16,7 +16,7 @@ int main() {
   for (LL i = 1; i <= n; i++) {
     cin >> e[i].x >> e[i].v;
   }
-  fill(&f[0][0], &f[501][1001] + 1, 1145141919810);
+  fill(&f[0][0], &f[1][1000001] + 1, 1145141919810);
   fill(&f[0][0], &f[0][b] + 1, 0);
   sort(e + 1, e + n + 1, [](E i, E j) {
     return i.x < j.x;
@@ -27,11 +27,13 @@ int main() {
     for (LL j = 0; j <= g; j++) {
       if (j + e[i].x - e[i - 1].x <= g) {
         for (LL k = 0; k <= j + e[i].x - e[i - 1].x; k++) {
-          f[i][j] = min(f[i][j], f[i - 1][k] + (j + e[i].x - e[i - 1].x - k) * e[i - 1].v);
+          f[1][j] = min(f[1][j], f[0][k] + (j + e[i].x - e[i - 1].x - k) * e[i - 1].v);
         }
       }
     }
+    copy(&f[1][0], &f[1][1000000] + 1, &f[0][0]);
+    fill(&f[1][0], &f[1][1000000] + 1, 1145141919810);
   }
-  cout << f[n][0] << endl;
+  cout << f[0][0] << endl;
   return 0;
 }
