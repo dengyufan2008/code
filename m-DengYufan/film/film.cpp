947b7d6eeab6ff084a768eb787a187362d920896
Deng Yufan <2695375004@qq.com>
2021-08-19 08:33:02 +0800

    AllFine

---
+++ m-DengYufan/film/film.cpp
@@ -0,0 +1,13 @@
+// #include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("film.in", "r", stdin);
+  // freopen("film.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
---
+++ m-DengYufan/zone/zone.cpp
@@ -0,0 +1,13 @@
+// #include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("zone.in", "r", stdin);
+  // freopen("zone.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
