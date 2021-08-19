0d4345890a2fbe9b864e85218a500850b528db4f
Deng Yufan <2695375004@qq.com>
2021-08-19 10:27:05 +0800

    AllFine

--- m-DengYufan/fuel/fuel.cpp
+++ m-DengYufan/fuel/fuel.cpp
@@ -1,13 +1,27 @@
-// #include <ctime>
+#include <ctime>
 #include <iostream>
 #define LL long long
 
 using namespace std;
 
+int n, ans, a[500001], f[2][500001];
+
 int main() {
-  // freopen("fuel.in", "r", stdin);
-  // freopen("fuel.out", "w", stdout);
-  
+  freopen("fuel.in", "r", stdin);
+  freopen("fuel.out", "w", stdout);
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      f[i & 1][j] = max(f[!(i & 1)][j - 1] + (a[i] == j), f[!(i & 1)][j]);
+    }
+  }
+  for (int i = 1; i <= n; i++) {
+    ans = max(ans, f[n & 1][i]);
+  }
+  cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
