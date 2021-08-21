b95a96baa96c40ff296908a4daa84a39e75182af
Deng Yufan <2695375004@qq.com>
2021-08-21 18:53:10 +0800

    AllFine

--- LuoGu/P1063.cpp
+++ LuoGu/P1063.cpp
@@ -4,23 +4,26 @@
 
 using namespace std;
 
-int n, a[201], f[101][101];
+LL n, ans, a[201], f[201][201];
 
 int main() {
   cin >> n;
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     cin >> a[i];
-    f[i][i] = a[i + n] = a[i];
+    a[i + n] = a[i];
   }
   n *= 2;
-  for (int i = 2; i <= n; i++) {
-    for (int j = 1, k; (k = j + i - 1) <= n; j++) {
-      for (int l = j; l < k; l++) {
-        f[i][j] = max(f[i][j], f[i][l] + f[l + 1][j] + a[i] * a[l] * a[k + 1 > n ? 1 : k + 1]);
+  for (LL i = 2; i <= n; i++) {
+    for (LL j = 1, k; (k = j + i - 1) <= n; j++) {
+      for (LL l = j; l < k; l++) {
+        f[j][k] = max(f[j][k], f[j][l] + f[l + 1][k] + a[j] * a[l + 1] * a[k == n ? 1 : k + 1]);
       }
     }
   }
-  cout << f[1][n] << endl;
+  for (LL i = 1; i <= n; i++) {
+    ans = max(ans, f[i][i + n / 2 - 1]);
+  }
+  cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
