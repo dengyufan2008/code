e92bf4d1aa8091c5d6cd94b50f23f4023d24fca1
Deng Yufan <2695375004@qq.com>
2021-08-21 20:20:17 +0800

    AllFine

--- LuoGu/P1005.cpp
+++ LuoGu/P1005.cpp
@@ -7,11 +7,14 @@ using namespace std;
 __int128_t n, m, ans, a[81], f[81][81];
 
 void Input(__int128_t &x) {
-  string s;
+  char c = ' ';
   x = 0;
-  cin >> s;
-  for (__int128_t i = 0; i < s.length(); i++) {
-    x = (x * 10) + s[i] - '0';
+  while (c < '0' || c > '9') {
+    c = getchar();
+  }
+  while (c >= '0' && c <= '9') {
+    x = x * 10 + c - '0';
+    c = getchar();
   }
 }
 
@@ -19,7 +22,7 @@ void Output(__int128_t x) {
   if (x / 10) {
     Output(x / 10);
   }
-  cout << (char)(x % 10 + '0');
+  putchar(x % 10 + '0');
 }
 
 int main() {
@@ -28,11 +31,11 @@ int main() {
     fill(&f[0][0], &f[80][80] + 1, 0);
     for (__int128_t i = 1; i <= m; i++) {
       Input(a[i]);
-      f[i][i] = a[i] << m;
+      f[i][i] = a[i] * 2;
     }
     for (__int128_t i = 2; i <= m; i++) {
       for (__int128_t j = 1, k; (k = j + i - 1) <= m; j++) {
-        f[j][k] = max(f[j + 1][k] + a[j] * (1 << m - i + 1), f[j][k - 1] + a[k] * (1 << m - i + 1));
+        f[j][k] = max(f[j + 1][k] * 2 + a[j] * 2, f[j][k - 1] * 2 + a[k] * 2);
       }
     }
     ans += f[1][m];
