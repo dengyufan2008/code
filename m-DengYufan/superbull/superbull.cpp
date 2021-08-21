cc5b0c721ae398b6f061a61fb3ae073b9a5984ab
Deng Yufan <2695375004@qq.com>
2021-08-21 09:55:02 +0800

    AllFine

--- m-DengYufan/superbull/superbull.cpp
+++ m-DengYufan/superbull/superbull.cpp
@@ -4,9 +4,15 @@
 
 using namespace std;
 
+int n, a[2001];
+
 int main() {
   // freopen("superbull.in", "r", stdin);
   // freopen("superbull.out", "w", stdout);
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
   
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
