757148170df4e80cc6260a0cb2461175db63255c
Deng Yufan <2695375004@qq.com>
2021-08-22 08:34:23 +0800

    AllFine

--- LuoGu/P1040.cpp
+++ LuoGu/P1040.cpp
@@ -4,8 +4,22 @@
 
 using namespace std;
 
+int n, a[31], f[31][31];
+
 int main() {
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  fill(&f[0][0], &f[30][30] + 1, 1);
+  for (int i = 2; i <= n; i++) {
+    for (int j = 1, k; (k = j + i - 1) <= n; j++) {
+      for (int l = j; l < k; l++) {
+        f[j][k] = max(f[j][k], f[j][l] * f[l + 1][k] + a[l]);
+      }
+    }
+  }
+  cout << f[1][n] << endl;
   // cout << endl << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
