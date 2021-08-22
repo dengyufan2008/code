6d30c56e128d8fc2e87ef107eb2f932b0f760566
Deng Yufan <2695375004@qq.com>
2021-08-22 09:56:31 +0800

    AllFine

--- test.cpp
+++ test.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 int main() {
   
-  // cout << endl << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
