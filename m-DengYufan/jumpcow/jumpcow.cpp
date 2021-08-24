7941e3803f94653e511f3c67d76f6a00d441f187
Deng Yufan <2695375004@qq.com>
2021-08-24 08:49:16 +0800

    AllFine

--- m-DengYufan/jumpcow/jumpcow.cpp
+++ m-DengYufan/jumpcow/jumpcow.cpp
@@ -4,10 +4,20 @@
 
 using namespace std;
 
+int p, a[150001], f[150001][2];
+
 int main() {
-  // freopen("jumpcow.in", "r", stdin);
-  // freopen("jumpcow.out", "w", stdout);
-  
+  freopen("jumpcow.in", "r", stdin);
+  freopen("jumpcow.out", "w", stdout);
+  cin >> p;
+  for (int i = 1; i <= p; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= p; i++) {
+    f[i][0] = max(f[i - 1][1] - a[i], f[i - 1][0]);
+    f[i][1] = max(f[i - 1][0] + a[i], f[i - 1][1]);
+  }
+  cout << max(f[p][0], f[p][1]) << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
