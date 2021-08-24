a3e7b223d8ce579a909bc292d15fb76e27683971
Deng Yufan <2695375004@qq.com>
2021-08-24 08:45:35 +0800

    AllFIne

---
+++ m-DengYufan/cheappal/cheappal.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("cheappal.in", "r", stdin);
+  // freopen("cheappal.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
---
+++ m-DengYufan/telewire/telewire.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("telewire.in", "r", stdin);
+  // freopen("telewire.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
