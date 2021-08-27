32e4fe274d86a46f29bd84011e81aec844e503a8
Deng Yufan <2695375004@qq.com>
2021-08-27 16:41:20 +0800

    AllFine

--- Other/diamond2.cpp
+++ Other/diamond2.cpp
@@ -5,31 +5,16 @@
 using namespace std;
 
 struct V {
-  LL l, d[256];
+  LL l;
+  bool d[256];
 } kMul, kMask, x, z, _i, _j, s[6], _s[3];
 string str;
 
-void M(V &v) {
-  v.l = str.length();
-  fill(&v.d[0], &v.d[255] + 1, 0);
-  for (LL i = 0; i < v.l; i++) {
-    v.d[v.l - i - 1] = str[i] - '0';
-  }
-}
-
-string _M(V v) {
-  string s = "";
-  for (LL i = 0; i < v.l; i++) {
-    s += (v.d[v.l - i - 1] + '0');
-  }
-  return s;
-}
-
 V C(V a) {
   for (LL i = 0; i < a.l; i++) {
-    a.d[i + 1] += a.d[i] / 10, a.d[i] %= 10, a.l += a.d[a.l] > 0;
+    a.d[i + 1] += a.d[i] >> 1, a.d[i] &= 1, a.l += a.d[a.l];
   }
-  while (a.d[a.l - 1] == 0) {
+  while (!a.d[a.l - 1]) {
     a.l--;
   }
   return a;
@@ -89,9 +74,25 @@ V Sh(V a, LL d) {  //positive:LSH negative:RSH
   return C(c);
 }
 
+void M(V &v) {
+  v.l = str.length();
+  fill(&v.d[0], &v.d[255] + 1, 0);
+  for (LL i = 0; i < v.l; i++) {
+    v.d[v.l - i - 1] = str[i] - '0';
+  }
+}
+
+string _M(V v) {
+  string s = "";
+  for (LL i = 0; i < v.l; i++) {
+    s += (v.d[v.l - i - 1] + '0');
+  }
+  return s;
+}
+
 int main() {
-  kMul = {11, {7, 1, 9, 3, 0, 9, 4, 1, 2, 5, 2}};
-  kMask = {15, {5, 5, 6, 0, 1, 7, 6, 7, 9, 4, 7, 4, 1, 8, 2}};
+  kMul = {35, {1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1}};
+  kMask = {47, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
   cout << "Please input the seed of the map." << endl;
   cin >> str, M(s[0]);
   cout << "Please input x and z of the block." << endl;
@@ -99,18 +100,18 @@ int main() {
   cin >> str, M(z);
   s[1] = And(Xor(s[0], kMul), kMask);
   for (LL i = 2; i <= 5; i++) {
-    s[i] = And(Add(Mul(s[i - 1], kMul), {2, {1, 1}}), kMask);
+    s[i] = And(Add(Mul(s[i - 1], kMul), {4, {1, 1, 0, 1}}), kMask);
   }
   for (LL i = 2; i <= 5; i++) {
     s[i] = i & 1 ? Sh(Sh(s[i], 16), -32) : Sh(Sh(s[i], -16), 32);
   }
   _i = Add(s[2], s[3]), _j = Add(s[4], s[5]);
   _i.d[_i.l - 1] += !(_i.d[_i.l - 1] & 1), _j.d[_j.l - 1] += !(_j.d[_j.l - 1] & 1);
-  _s[0] = And(Xor(Add(Xor(Add(Mul(Mul({2, {6, 1}}, x), _i), Mul(Mul({2, {6, 1}}, z), _j)), s[0]), {5, {9, 0, 0, 0, 6}}), kMul), kMask);
+  _s[0] = And(Xor(Add(Xor(Add(Mul(Mul({5, {0, 0, 0, 0, 1}}, x), _i), Mul(Mul({5, {0, 0, 0, 0, 1}}, z), _j)), s[0]), {16, {1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1}}), kMul), kMask);
   for (LL i = 1; i <= 2; i++) {
     _s[i] = And(Add(Mul(_s[i - 1], kMul), {2, {1, 1}}), kMask);
   }
-  cout << "The diamond is at x:" << _M(Add(Sh(_s[1], -44), Mul({2, {6, 1}}, x))) << " z:" << _M(Add(Sh(_s[2], -44), Mul({2, {6, 1}}, z))) << endl;
+  cout << "The diamond is at x:" << _M(Add(Sh(_s[1], -44), Mul({5, {0, 0, 0, 0, 1}}, x))) << " z:" << _M(Add(Sh(_s[2], -44), Mul({5, {0, 0, 0, 0, 1}}, z))) << endl;
   system("pause");
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
