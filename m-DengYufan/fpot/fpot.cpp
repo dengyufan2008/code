5e1e7ac2cac62bcbd9621417d6db0c62235379e4
Deng Yufan <2695375004@qq.com>
2021-09-05 08:44:59 +0800

    AllFine

---
+++ m-DengYufan/fpot/fpot.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("fpot.in", "r", stdin);
+  // freopen("fpot.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
---
+++ m-DengYufan/rblock/rblock.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("rblock.in", "r", stdin);
+  // freopen("rblock.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
