e057fe8abfb18d281e24e8396b9221455377cfb9
Deng Yufan <2695375004@qq.com>
2021-09-12 08:45:34 +0800

    AllFine

--- LuoGu/P2202.cpp
+++ LuoGu/P2202.cpp
@@ -1,11 +1,48 @@
+#include <algorithm>
 #include <ctime>
 #include <iostream>
+#include <set>
 #define LL long long
+#define Pii pair<int, int>
 
 using namespace std;
 
+struct V {
+  bool b;
+  int x, y;
+} v[101];
+int n, k, ans;
+set<pair<int, int>> s;
+
 int main() {
-  
+  cin >> n >> k;
+  k /= 2;
+  for (int i = 1, x, y; i <= n; i++) {
+    cin >> x >> y;
+    v[i] = {0, x, y}, v[i + n] = {1, x, y};
+  }
+  n *= 2;
+  sort(v + 1, v + n + 1, [](V i, V j) { return i.x < j.x; });
+  for (int i = 1; i <= n; i++) {
+    if (v[i].b) {
+      int c = 0;
+      pair<int, int> p;
+      for (auto j : s) {
+        if (abs(v[i].x - j.first) < k && abs(v[i].y - j.second) < k) {
+          c++, p = j;
+        }
+      }
+      if (c == 1) {
+        ans = (ans == 0 ? (k - abs(v[i].x - p.first)) * (k - abs(v[i].y - p.second)) : -1);
+      } else {
+        ans = -1;
+      }
+      s.erase({v[i].x, v[i].y});
+    } else {
+      s.insert({v[i].x, v[i].y});
+    }
+  }
+  cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
---
+++ m-DengYufam/poker/poker.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("poker.in", "r", stdin);
+  // freopen("poker.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
