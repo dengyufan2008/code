dd860764dc0824d54973e722235d2c769e231971
Deng Yufan <2695375004@qq.com>
2021-09-21 16:48:57 +0800

    AllFine

--- LuoGu/P1879.cpp
+++ LuoGu/P1879.cpp
@@ -4,33 +4,37 @@
 
 using namespace std;
 
-int m, n, ans, a[12][12], d[2][1 << 12];
-
-int M(int x, int y) {
-  return ((x - 1) * n + y) & 1;
-}
+const int kMod = 100000000;
+int n, m, ans, a[12][12], d[2][4096];
+bool b;
 
 int main() {
-  cin >> m >> n;
-  for (int i = 1; i <= m; i++) {
-    for (int j = 1; j <= n; j++) {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
       cin >> a[i][j];
     }
   }
-  d[1][0] = 1;
-  for (int i = 1; i <= m; i++) {
-    for (int j = 1; j <= n; j++) {
-      fill(&d[!M(i, j)][0], &d[!M(i, j)][1 << 12], 0);
-      for (int k = 0; k < (1 << 12); k++) {
-        d[!M(i, j)][k >> 1] += d[M(i, j)][k];
-        if (a[i][j] && !(k & 1) && (j == 1 || !(k & (1 << n - 1)))) {
-          d[!M(i, j)][(k >> 1) + (1 << n - 1)] += d[M(i, j)][k];
+  d[0][0] = 1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      b ^= 1;
+      fill(&d[b][0], &d[b][4096], 0);
+      for (int k = 0; k < (1 << m); k++) {
+        bool l = (j > 0) * ((1 << (j - 1)) & k), u = (1 << j) & k;
+        if ((i || !u) && (j || !l)) {
+          if (!u) {
+            d[b][k] = (d[b][k] + d[b ^ 1][k]) % kMod;
+          }
+          if (u || !l && a[i][j]) {
+            d[b][k ^ (1 << j)] = (d[b][k ^ (1 << j)] + d[b ^ 1][k]) % kMod;
+          }
         }
       }
     }
   }
-  for (int i = 0; i < (1 << 12); i++) {
-    ans += d[M(m, n)][i];
+  for (int i = 0; i < (1 << m); i++) {
+    ans = (ans + d[b][i]) % kMod;
   }
   cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
