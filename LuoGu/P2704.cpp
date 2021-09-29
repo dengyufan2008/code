57b591e6ad73f324c192e3f359dec6c73951c81e
Deng Yufan <2695375004@qq.com>
2021-09-29 17:25:25 +0800

    AllFine

--- LuoGu/P2704.cpp
+++ LuoGu/P2704.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int n, m, ans, d[2][4][1024] = {1};
+int n, m, ans, d[2][1048576] = {1};
 bool b, a[101][11];
 char ch;
 
@@ -19,19 +19,15 @@ int main() {
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= m; j++) {
       b ^= 1;
-      fill(&d[b][0][0], &d[b][3][1024], 0);
-      for (int k = 0; k < 4; k++) {
-        for (int l = 0; l < 1024; l++) {
-          int _k = ((k & (1 << m - 1)) > 0) * 2 + ((k & (1 << m - 2)) > 0);
-          
-        }
+      fill(&d[b][0], &d[b][1048576], 0);
+      for (int k = 0; k < 1048576; k++) {
+        bool u1 = (1 << m + j) & k, u2 = (1 << j) & k, l1 = (j > 0) * (1 << m + j - 1) & k, l2 = (j > 1) * (1 << m + j - 2) & k;
+        d[b][k] = d[b][k] + d[b ^ 1][k];
       }
     }
   }
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 1024; j++) {
-      ans += d[b][i][j];
-    }
+  for (int i = 0; i < 1048576; i++) {
+    ans += d[b][i];
   }
   cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
