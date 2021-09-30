75816184733779979bd85c37af47b547ad946832
Deng Yufan <2695375004@qq.com>
2021-09-30 08:33:52 +0800

    AllFine

--- LuoGu/P1879.cpp
+++ LuoGu/P1879.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const int kMod = 100000000;
 int n, m, ans, a[12][12], d[2][4096] = {1};
-bool b;
+bool b = 1;
 
 int main() {
   cin >> n >> m;
@@ -17,19 +17,19 @@ int main() {
   }
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      b ^= 1;
       fill(&d[b][0], &d[b][4096], 0);
+      b ^= 1;
       for (int k = 0; k < (1 << m); k++) {
         bool l = (j > 0) * ((1 << m - 1) & k), u = 1 & k;
-        d[b][k >> 1] = (d[b][k >> 1] + d[b ^ 1][k]) % kMod;
+        d[b ^ 1][k >> 1] = (d[b ^ 1][k >> 1] + d[b][k]) % kMod;
         if (!u && !l && a[i][j]) {
-          d[b][(k >> 1) + (1 << m - 1)] = (d[b][(k >> 1) + (1 << m - 1)] + d[b ^ 1][k]) % kMod;
+          d[b ^ 1][(k >> 1) + (1 << m - 1)] = (d[b ^ 1][(k >> 1) + (1 << m - 1)] + d[b][k]) % kMod;
         }
       }
     }
   }
   for (int i = 0; i < (1 << m); i++) {
-    ans = (ans + d[b][i]) % kMod;
+    ans = (ans + d[b ^ 1][i]) % kMod;
   }
   cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
