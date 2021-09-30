bf6f6ae6ec741d87ecd4b3babddddb81b98cb8b2
Deng Yufan <2695375004@qq.com>
2021-09-30 08:35:15 +0800

    AllFine

--- LuoGu/P2704.cpp
+++ LuoGu/P2704.cpp
@@ -18,19 +18,19 @@ int main() {
   }
   for (LL i = 0; i < n; i++) {
     for (LL j = 0; j < m; j++) {
-      b ^= 1;
       fill(&f[b][0], &f[b][1048576], 0);
+      b ^= 1;
       for (LL k = 0; k < (1 << 2 * m); k++) {
         bool u1 = (1 << m) & k, u2 = 1 & k, l1 = (1 << 2 * m - 2) & k, l2 = (1 << 2 * m - 3) & k;
-        f[b][k >> 1] = max(f[b][k >> 1], f[b ^ 1][k]);
+        f[b ^ 1][k >> 1] = max(f[b ^ 1][k >> 1], f[b][k]);
         if (!u1 && !u2 && !l1 && !l2 && a[i][j]) {
-          f[b][(k >> 1) + (1 << m * 2 - 1)] = max(f[b][(k >> 1) + (1 << m * 2 - 1)], f[b ^ 1][k] + 1);
+          f[b ^ 1][(k >> 1) + (1 << m * 2 - 1)] = max(f[b ^ 1][(k >> 1) + (1 << m * 2 - 1)], f[b][k] + 1);
         }
       }
     }
   }
   for (LL i = 0; i < (1 << m * 2); i++) {
-    ans = max(ans, f[b][i]);
+    ans = max(ans, f[b ^ 1][i]);
   }
   cout << ans << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
