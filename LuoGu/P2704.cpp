5ca48cfbc948c13e84866883aa16673fe4810407
Deng Yufan <2695375004@qq.com>
2021-09-30 18:03:32 +0800

    AllFine

--- LuoGu/P2704.cpp
+++ LuoGu/P2704.cpp
@@ -5,25 +5,24 @@
 using namespace std;
 
 LL n, m, ans, f[2][1048576];
-bool b, a[101][11];
-char ch;
+bool b;
+char a[101][11];
 
 int main() {
   cin >> n >> m;
   for (LL i = 0; i < n; i++) {
     for (LL j = 0; j < m; j++) {
-      cin >> ch;
-      a[i][j] = ch == 'P';
+      cin >> a[i][j];
     }
   }
   for (LL i = 0; i < n; i++) {
     for (LL j = 0; j < m; j++) {
-      fill(&f[b][0], &f[b][1048576], 0);
+      fill(&f[b][0], &f[b][1 << 2 * m], 0);
       b ^= 1;
       for (LL k = 0; k < (1 << 2 * m); k++) {
-        bool u1 = (1 << m) & k, u2 = 1 & k, l1 = (1 << 2 * m - 2) & k, l2 = (1 << 2 * m - 3) & k;
+        bool u1 = (i > 0) * ((1 << m) & k), u2 = (i > 1) * (1 & k), l1 = (j > 0) * ((1 << 2 * m - 2) & k), l2 = (j > 1) * ((1 << 2 * m - 3) & k);
         f[b ^ 1][k >> 1] = max(f[b ^ 1][k >> 1], f[b][k]);
-        if (!u1 && !u2 && !l1 && !l2 && a[i][j]) {
+        if (!u1 && !u2 && !l1 && !l2 && a[i][j] == 'P') {
           f[b ^ 1][(k >> 1) + (1 << m * 2 - 1)] = max(f[b ^ 1][(k >> 1) + (1 << m * 2 - 1)], f[b][k] + 1);
         }
       }
