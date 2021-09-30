70888103011c8a790988f13918ccf15922a70802
Deng Yufan <2695375004@qq.com>
2021-09-30 18:55:45 +0800

    .

--- LuoGu/P2704_2.cpp
+++ LuoGu/P2704_2.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-LL n, m, ans, f[2][1048576];
+LL n, m, ans, three[10] = {1}, f[2][59049];
 bool b;
-char a[101][11];
+char a[100][10];
 
 int main() {
   cin >> n >> m;
@@ -15,20 +15,23 @@ int main() {
       cin >> a[i][j];
     }
   }
+  for (int i = 1; i <= 9; i++) {
+    three[i] = three[i - 1] * 3;
+  }
   for (LL i = 0; i < n; i++) {
     for (LL j = 0; j < m; j++) {
-      fill(&f[b][0], &f[b][(1 << 2 * m)], 0);
+      fill(&f[b][0], &f[b][three[m]], 0);
       b ^= 1;
-      for (LL k = 0; k < (1 << 2 * m); k++) {
-        bool u1 = (i > 0) * ((1 << m) & k), u2 = (i > 1) * (1 & k), l1 = (j > 0) * ((1 << 2 * m - 1) & k), l2 = (j > 1) * ((1 << 2 * m - 2) & k);
-        f[b ^ 1][k >> 1] = max(f[b ^ 1][k >> 1], f[b][k]);
-        if (!u1 && !u2 && !l1 && !l2 && a[i][j] == 'P') {
-          f[b ^ 1][(k >> 1) + (1 << m * 2 - 1)] = max(f[b ^ 1][(k >> 1) + (1 << m * 2 - 1)], f[b][k] + 1);
+      for (LL k = 0; k < three[m]; k++) {
+        bool u = k / three[j] % 3, l1 = (j > 0) * k / three[j - 1] % 3, l2 = (j > 1) * k / three[j - 2];
+        f[b ^ 1][k + ((u + 1) % 3 - u) * three[j]] = max(f[b ^ 1][k + ((u + 1) % 3 - u) * three[j]], f[b][k]);
+        if (!u && l1 != 1 && l2 != 1) {
+          f[b ^ 1][k + three[j]] = max(f[b ^ 1][k + three[j]], f[b][k] + 1);
         }
       }
     }
   }
-  for (LL i = 0; i < (1 << m * 2); i++) {
+  for (LL i = 0; i < three[m]; i++) {
     ans = max(ans, f[b ^ 1][i]);
   }
   cout << ans << endl;
