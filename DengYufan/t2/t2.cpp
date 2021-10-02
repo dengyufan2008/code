b2b589746c4aa4033b4b5939c0c2dfd29e0647fa
Deng Yufan <2695375004@qq.com>
2021-10-02 08:54:38 +0800

    .

--- DengYufan/t2/t2.cpp
+++ DengYufan/t2/t2.cpp
@@ -4,10 +4,33 @@
 
 using namespace std;
 
+const int kMove[4][2] = {{1, 1}, {1, 0}, {1, -1}, {0, 1}};
+int n, b = -1, a[16][16];  // 1:Alice -1:Bob
+
 int main() {
-  // freopen("t2.in", "r", stdin);
-  // freopen("t2.out", "w", stdout);
-  
+  freopen("t2.in", "r", stdin);
+  freopen("t2.out", "w", stdout);
+  cin >> n;
+  for (int i = 1, x, y; i <= n; i++) {
+    cin >> x >> y;
+    b = -b, a[x][y] = b;
+    for (int j = 0; j <= 3; j++) {    // Dir
+      for (int k = 0; k <= 4; k++) {  // Turn
+        int _x = x - kMove[j][0] * k, _y = y - kMove[j][1] * k, flag = 1;
+        for (int l = 1; l <= 5; l++, _x += kMove[j][0], _y += kMove[j][1]) {  // Place
+          if (a[_x][_y] != b) {
+            flag = 0;
+            break;
+          }
+        }
+        if (flag) {
+          cout << (b == 1 ? "A " : "B ") << i << endl;
+          return 0;
+        }
+      }
+    }
+  }
+  cout << "Tie" << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
