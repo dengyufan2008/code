f714d6b54f905124de6d01886b459c2eba976751
Deng Yufan <2695375004@qq.com>
2021-10-02 11:41:57 +0800

    .

--- DengYufan/t3/t3.cpp
+++ DengYufan/t3/t3.cpp
@@ -31,15 +31,16 @@ void _Input() {
 }
 
 int main() {
-  // freopen("t3.in", "r", stdin);
-  // freopen("t3.out", "w", stdout);
+  freopen("t3.in", "r", stdin);
+  freopen("t3.out", "w", stdout);
   k['T' - 'A'] = 1, k['B' - 'A'] = 2;
   Input(v[1], 1), Input(v[0], -1), _Input();
-  while ((v[0].n[0] || v[0].n[1] || v[0].n[2]) && (v[1].n[0] || v[1].n[1] || v[1].n[2])) {
-    b ^= 1;
-    if (b) {
-      
-    }
+  if (v[1].q[1].empty()) {
+    cout << "C\n2\n";
+  } else if (v[1].q[1].top().first == 1) {
+    cout << "L\n6\n";
+  } else {
+    cout << "C\n" << (v[1].n[0] + v[1].n[1] + v[1].n[2]) * 2 << endl;
   }
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
