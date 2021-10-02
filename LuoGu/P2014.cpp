632a0166d94cbe8d4dfaca3c4726dd842849aa75
Deng Yufan <2695375004@qq.com>
2021-10-02 15:43:43 +0800

    .

--- LuoGu/P2014.cpp
+++ LuoGu/P2014.cpp
@@ -6,18 +6,21 @@
 using namespace std;
 
 struct V {
-  int d, f[301];
-  bool b;
+  int f[301];
   vector<int> e;
 } v[301];
-int n, m, ans[301];
+int n, m;
 
 void T(int f, int x) {
   for (int i : v[x].e) {
     if (f != i) {
       T(x, i);
-      for (int j = 0; j <= 300; j++) {
-        for (int k = 0; k <= 300; k++) {
+    }
+  }
+  for (int i : v[x].e) {
+    if (f != i) {
+      for (int j = m; j >= 1; j--) {
+        for (int k = 0; k < j; k++) {
           v[x].f[j] = max(v[x].f[j], v[x].f[j - k] + v[i].f[k]);
         }
       }
@@ -28,14 +31,12 @@ void T(int f, int x) {
 int main() {
   cin >> n >> m;
   for (int i = 1, k; i <= n; i++) {
-    cin >> k >> v[i].d;
-    v[i].b = k, v[i].e.push_back(k), v[k].e.push_back(i);
+    cin >> k >> v[i].f[1];
+    v[i].e.push_back(k), v[k].e.push_back(i);
   }
-  for (int i = 1; i <= n; i++) {
-    if (!v[i].b) {
-      T(114514, i);
-    }
-  }
+  m++;
+  T(114514, 0);
+  cout << v[0].f[m] << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
