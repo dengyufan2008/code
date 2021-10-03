9e8f99453c0faa919df74b7692eb9f07914e5b0d
Deng Yufan <2695375004@qq.com>
2021-10-03 11:54:39 +0800

    .

--- m-DengYufan/bbreeds/bbreeds.cpp
+++ m-DengYufan/bbreeds/bbreeds.cpp
@@ -1,45 +1,36 @@
 #include <ctime>
 #include <iostream>
-#include <stack>
 #define LL long long
 
 using namespace std;
 
-int ans, d[2];
+int d, f[1001][501][501];
 string s;
-
-inline void S(register int x) {
-  if (x == s.length()) {
-    ans = (ans + (!d[0] && !d[1])) % 2012;
-    return;
-  }
-  for (register int i(0); i <= 1; ++i) {
-    if (s[x] == '(') {
-      d[i]++;
-      S(x + 1);
-      d[i]--;
-    } else if (d[i]) {
-      d[i]--;
-      S(x + 1);
-      d[i]++;
-    } else {
-      continue;
-    }
-  }
-}
 
 int main() {
   freopen("bbreeds.in", "r", stdin);
-  freopen("bbreeds.out", "w", stdout);
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(0);
+  // freopen("bbreeds.out", "w", stdout);
   cin >> s;
-  if (s.length() == 1000) {
-    cout << 1604 << endl;
+  for (int i = 0; i < s.length(); i++) {
+    d += s[i] == '(' ? 1 : -1;
+  }
+  if (d != 0) {
+    cout << 0 << endl;
     return 0;
   }
-  S(0);
-  cout << ans << endl;
+  f[0][1][0] = f[0][0][1] = s[0] == '(';
+  for (int i = 1; i < s.length(); i++) {
+    for (int j = 0; j < s.length() / 2; j++) {
+      for (int k = 0; k < s.length() / 2; k++) {
+        if (s[i] == '(') {
+          f[i][j][k] = (f[i][j][k] + f[i - 1][j - 1][k] + f[i - 1][j][k - 1]) % 2012;
+        } else {
+          f[i][j][k] = (f[i][j][k] + f[i - 1][j + 1][k] + f[i - 1][j][k + 1]) % 2012;
+        }
+      }
+    }
+  }
+  cout << f[s.length() - 1][0][0] << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
