387a077eb9d18a05f8abd10d13efe576b24834e9
Deng Yufan <2695375004@qq.com>
2021-10-03 08:11:32 +0800

    .

--- LuoGu/P2986.cpp
+++ LuoGu/P2986.cpp
@@ -1,10 +1,25 @@
 #include <ctime>
 #include <iostream>
+#include <vector>
 #define LL long long
 
 using namespace std;
 
+struct V {
+  int c;
+  vector<pair<int, int>> e;
+} v[100001];
+int n;
+
 int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> v[i].c;
+  }
+  for (int i = 1, x, y, z; i < n; i++) {
+    cin >> x >> y >> z;
+    v[x].e.push_back({y, z}), v[y].e.push_back({x, z});
+  }
   
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
