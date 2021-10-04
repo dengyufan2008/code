f6d2c37770ee4a8414ef2b0f90c446716189750c
Deng Yufan <2695375004@qq.com>
2021-10-04 08:17:10 +0800

    .

--- LuoGu/P3146.cpp
+++ LuoGu/P3146.cpp
@@ -4,8 +4,21 @@
 
 using namespace std;
 
+int n, f[251][251][2];
+
 int main() {
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> f[i][i][0];
+    f[i][i][1] = f[i][i][0];
+  }
+  for (int i = 2; i <= n; i++) {
+    for (int j = 1, k; (k = j + i - 1) <= n; j++) {
+      f[j][k][0] = max(f[j][k - 1][0] + (f[max(j - i, 1)][j - 1][1] == f[j][k - 1][0]), f[j + 1][min(j + i, n)][0] + (f[j + 1][min(j + 1, n)][0] == f[max(j - i, 1)][j - 1][1]));
+      f[j][k][1] = f[j + 1][k][1] + (f[k + 1][min(k + i, n)][0] == f[j + 1][k][1]);
+    }
+  }
+  cout << max(f[1][n][0], f[1][n][1]) << endl;
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
---
+++ m-DengYufan/tractor/tractor.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("tractor.in", "r", stdin);
+  // freopen("tractor.out", "w", stdout);
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
