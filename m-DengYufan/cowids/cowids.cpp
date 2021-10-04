0a4116d58d17d16c5eb743d05bdd5330411bff34
Deng Yufan <2695375004@qq.com>
2021-10-04 10:40:00 +0800

    .

--- m-DengYufan/cowids/cowids.cpp
+++ m-DengYufan/cowids/cowids.cpp
@@ -4,9 +4,12 @@
 
 using namespace std;
 
+int n, k;
+
 int main() {
   // freopen("cowids.in", "r", stdin);
   // freopen("cowids.out", "w", stdout);
+  cin >> n >> k;
   
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
