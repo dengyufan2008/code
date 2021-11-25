25b6f9caac905f961e1bab222356bef6d79598eb
DengYufan <2695375004@qq.com>
2021-11-25 18:24:47 +0800

    .

---
+++ LuoGu/P3258.cpp
@@ -0,0 +1,12 @@
+#include <ctime>
+#include <iostream>
+#define LL long long
+#define UNICODE
+
+using namespace std;
+
+int main() {
+  
+  // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
+  return 0;
+}
