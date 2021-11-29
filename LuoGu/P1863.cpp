2557bac8a0705ff691f37f65f113016227242439
DengYufan <2695375004@qq.com>
2021-11-29 17:15:19 +0800

    .

--- LuoGu/P1863.cpp
+++ LuoGu/P1863.cpp
@@ -5,27 +5,27 @@
 
 using namespace std;
 
-int n;
-bool b[1001] = {1};
-pair<int, int> a[1001] = {{0, 0}};
+LL n;
+bool b[1001];
+pair<LL, LL> a[1001] = {{0, 0}};
 
-int ChaCheng(int o, int x, int y) {
+LL ChaCheng(LL o, LL x, LL y) {
   return (a[x].first - a[o].first) * (a[y].second - a[o].second) - (a[y].first - a[o].first) * (a[x].second - a[o].second);
 }
 
-double Dis(int x, int y) {
+double Dis(LL x, LL y) {
   return sqrt((a[x].first - a[y].first) * (a[x].first - a[y].first) + (a[x].second - a[y].second) * (a[x].second - a[y].second));
 }
 
 int main() {
   cin >> n;
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     cin >> a[i].first >> a[i].second;
   }
   cout << n << ' ';
-  for (int i = 1, p = 0; i <= n; i++) {
-    int mx = 0;
-    for (int j = 1; j <= n; j++) {
+  for (LL i = 1, p = 0; i <= n; i++) {
+    LL mx = 0;
+    for (LL j = 1; j <= n; j++) {
       if (!b[j] && (!mx || ChaCheng(p, mx, j) < 0 || ChaCheng(p, mx, j) == 0 && Dis(p, j) < Dis(p, mx))) {
         mx = j;
       }
