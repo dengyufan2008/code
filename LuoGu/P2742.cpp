63f3964bb3a2b00c9e3b9883a14a7bd70bad7e06
DengYufan <2695375004@qq.com>
2021-12-05 09:39:05 +0800

    .

--- LuoGu/P2742.cpp
+++ LuoGu/P2742.cpp
@@ -4,10 +4,14 @@
 
 using namespace std;
 
-
+int n;
+pair<int, int> a[100001];
 
 int main() {
-  
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i].first >> a[i].second;
+  }
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
 }
