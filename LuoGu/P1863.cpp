b7f5f09866b9e78b901a7cd352e4c72f9a0f5844
DengYufan <2695375004@qq.com>
2021-12-05 10:49:30 +0800

    .

--- LuoGu/P1863.cpp
+++ LuoGu/P1863.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-LL n;
+LL n, p;
 bool b[1001];
-pair<LL, LL> a[1001] = {{0, 0}};
+pair<LL, LL> a[1001];
 
 LL Cross(LL o, LL x, LL y) {
   return (a[x].first - a[o].first) * (a[y].second - a[o].second) - (a[y].first - a[o].first) * (a[x].second - a[o].second);
@@ -21,9 +21,13 @@ int main() {
   cin >> n;
   for (LL i = 1; i <= n; i++) {
     cin >> a[i].first >> a[i].second;
+    if (!p || a[p].second > a[i].second || a[p].second == a[i].second && a[p].first < a[i].first) {
+      p = i;
+    }
   }
-  cout << n << ' ';
-  for (LL i = 1, p = 0; i <= n; i++) {
+  b[p] = 1;
+  cout << n << ' ' << p << ' ';
+  for (LL i = 1; i < n; i++) {
     LL mx = 0;
     for (LL j = 1; j <= n; j++) {
       if (!b[j] && (!mx || Cross(p, mx, j) < 0 || Cross(p, mx, j) == 0 && Dis(p, j) < Dis(p, mx))) {
