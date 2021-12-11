5bacbf0afef3c65349703b6f3bd1d0c161082969
DengYufan <2695375004@qq.com>
2021-12-11 21:27:13 +0800

    .

--- LuoGu/P1168.cpp
+++ LuoGu/P1168.cpp
@@ -4,7 +4,13 @@
 
 using namespace std;
 
+int n, a[100001];
+
 int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
   
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
