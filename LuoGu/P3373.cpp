3720b5be1cfc88ded90d0b615e183e9b933feabc
DengYufan <2695375004@qq.com>
2021-12-12 19:55:15 +0800

    .

--- LuoGu/P3373.cpp
+++ LuoGu/P3373.cpp
@@ -4,13 +4,7 @@
 
 using namespace std;
 
-int n, a[100001];
-
 int main() {
-  cin >> n;
-  for (int i = 1; i <= n; i++) {
-    cin >> a[i];
-  }
   
   // cout << "Runtime:" << (double)clock() / 1000.0 << "s" << endl;
   return 0;
