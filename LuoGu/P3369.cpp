67f4c4222cbdfbe21cd117a10d41b142bcfd2f9f
DengYufan <2695375004@qq.com>
2022-01-19 11:18:43 +0800

    .

--- LuoGu/P3369.cpp
+++ LuoGu/P3369.cpp
@@ -5,35 +5,35 @@ using namespace std;
 
 const int kInf = 1000000000;
 struct V {
-  int d, c, s, l, r;
+  int d, s, l, r;
 } v[100001];
 int n, m, s = 1;
 
+void Update(int &p) {
+  v[p].s = v[v[p].l].s + v[v[p].r].s + (p != 0);
+}
+
 void Insert(int &p, int x) {
-  if (!p) {
-    v[p = ++m] = {x, 1, 1};
-  } else if (v[p].d == x) {
-    v[p].c++, v[p].s++;
+  if (!p || !m) {
+    v[p = ++m].d = x;
   } else {
     Insert(v[p].d < x ? v[p].r : v[p].l, x);
-    v[p].s = v[v[p].l].s + v[v[p].r].s;
   }
+  Update(p);
 }
 
 void Delete(int &p, int x) {
   if (v[p].d == x) {
-    v[p].c--, v[p].s--;
-    if (!v[p].c) {
-      int i = v[p].l;
-      for (; v[v[i].r].r; i = v[i].r) {
-        v[i].s--;
-      }
-      v[p].d = v[v[i].r].d, v[p].c = v[v[i].r].c, v[i].r = v[v[i].r].r;
+    v[p].s--;
+    int i = v[p].l;
+    for (; v[v[i].r].r; i = v[i].r) {
+      v[i].s--;
     }
+    v[p].d = v[v[i].r].d, v[i].r = v[v[i].r].r;
   } else {
     Delete(v[p].d < x ? v[p].r : v[p].l, x);
-    v[p].s = v[v[p].l].s + v[v[p].r].s;
   }
+  Update(p);
 }
 
 int FindRank(int &p, int x) {
