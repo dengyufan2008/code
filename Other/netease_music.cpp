a0fcfb44fdd4e3b2a62430e055a02c19ed39a858
DengYufan <2695375004@qq.com>
2022-01-21 00:22:36 +0800

    .

--- Other/netease_music.cpp
+++ Other/netease_music.cpp
@@ -1,14 +1,33 @@
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
-string s, _s;
+string num, local;
+
+void Powershell() {
+  ofstream file;
+  file.open("test.ps1");
+  string newArg = "-auto";
+  string powershell;
+  powershell += "$WebClient = New-Object System.Net.WebClient\n$WebClient.DownloadFile(\"http://music.163.com/song/media/outer/url?id=";
+  powershell += num;
+  powershell += ".mp3\",\"";
+  powershell += local;
+  powershell += "\")\n";
+  file << powershell;
+  file.close();
+  system("powershell -ExecutionPolicy Bypass -F test.ps1");
+  remove("test.ps1");
+}
 
 int main() {
   while (1) {
-    cin >> s;
-    _s = "start http://music.163.com/song/media/outer/url?id=" + s + ".mp3";
-    system(_s.c_str());
+    cout << "Music number (For example:1):";
+    cin >> num;
+    cout << "Save to (For example:D:\\1.mp3):";
+    cin >> local;
+    Powershell();
   }
   return 0;
 }
