441f2f70a5743a45ea8efc76963e8c45f5d9a4c3
DengYufan <2695375004@qq.com>
2022-01-20 17:38:25 +0800

    .

--- LuoGu/P1471.cpp
+++ LuoGu/P1471.cpp
@@ -3,7 +3,25 @@
 
 using namespace std;
 
+int n, m;
+pair<int, int> d[400001];
+
 int main() {
-  
+  cin >> n >> m;
+  for (int i = 1, x; i <= n; i++) {
+    cin >> x;
+    Add(1, 1, n, i, i, x);
+  }
+  for (int i = 1, o, x, y, k; i <= m; i++) {
+    cin >> o >> x >> y;
+    if (o == 1) {
+      cin >> k;
+      Add(1, 1, n, x, y, k);
+    } else if (o == 2) {
+      ;
+    } else {
+      ;
+    }
+  }
   return 0;
 }
