f9eeb867d14f40640ff5b780b9cb622e300b7b9a
DengYufan <2695375004@qq.com>
2022-01-22 15:59:59 +0800

    .

--- LuoGu/P3391.cpp
+++ LuoGu/P3391.cpp
@@ -1,90 +1,90 @@
 #include <iostream>
+#include <vector>
 #define LL long long
 
 using namespace std;
 
 struct V {
-  int l, r, t, v;
+  bool b;
+  int v, c, s[2];
 } v[100001];
-int n, q, l, r, s = 1;
+int n, q, l, r, s = 1, _s;
+vector<bool> h;
 
-void Left(int &p) {
-  int q = v[p].r;
-  v[p].r = v[q].l, v[q].l = p;
-  p = q;
+void Update(int p) {
+  v[p].c = v[v[p].s[0]].c + v[v[p].s[1]].c + (p > 0);
 }
 
-void Right(int &p) {
-  int q = v[p].l;
-  v[p].l = v[q].r, v[q].r = p;
-  p = q;
-}
-
-bool Update(int &p, int q) {
-  if (v[v[p].l].v == q) {
-    Right(p);
-    return 1;
-  } else if (v[v[p].r].v == q) {
-    Left(p);
-    return 1;
+void Down(int p) {
+  if (v[p].b) {
+    swap(v[p].s[0], v[p].s[1]);
+    v[v[p].s[0]].b = v[v[p].s[1]].b = 1;
+    v[p].b = 0;
   }
-  return 0;
 }
 
-void TurnUp(int &p, int q, bool b) {
+void Turn(int &p, bool b) {  // 0:L 1:R
+  int q = v[p].s[!b];
+  v[p].s[!b] = v[q].s[b], v[q].s[b] = p;
+  Update(p), Update(q);
+  p = q;
+}
+
+void Rebalance(int &p) {
+  if (p != _s && h.size() != 2) {
+    return;
+  }
+  if (h.back() != h.front()) {
+    Turn(v[p].s[h.back()], h.back());
+  } else if (h.size() == 2) {
+    Turn(p, !h.back());
+  }
+  Turn(p, !h.back());
+  h.clear();
+}
+
+void Up(int &p, int q) {
   if (!p) {
     return;
   }
-  if (v[p].t & 1) {
-    swap(v[p].l, v[p].r);
-    v[v[p].l].t++, v[v[p].r].t++, v[p].t = 0;
+  Down(p);
+  int c = v[v[p].s[0]].c;
+  if (c + 1 != q) {
+    Up(v[p].s[c + 1 < q], q - (c + 1 < q) * (c + 1));
+    h.push_back(c + 1 < q);
   }
-  if (v[p].v > q) {
-    TurnUp(v[p].l, q, b);
-  } else if (v[p].v < q) {
-    TurnUp(v[p].r, q, b);
-  }
-  if (p != s || b) {
-    if (v[v[v[p].l].l].v == q) {
-      Right(p), Right(p);
-    } else if (v[v[v[p].r].r].v == q) {
-      Left(p), Left(p);
-    } else if (v[v[v[p].l].r].v == q) {
-      Left(p), Right(p);
-    } else if (v[v[v[p].r].l].v == q) {
-      Right(p), Left(p);
-    }
-  }
+  Update(p), Rebalance(p);
+}
+
+void Swap(int l, int r) {
+  _s = s;
+  Up(s, r + 2);
+  _s = v[s].s[0];
+  Up(v[s].s[0], l);
+  v[v[v[s].s[0]].s[1]].b = 1;
 }
 
 void Print(int p) {
   if (!p) {
     return;
   }
-  if (v[p].t & 1) {
-    swap(v[p].l, v[p].r);
-    v[v[p].l].t++, v[v[p].r].t++, v[p].t = 0;
-  }
-  Print(v[p].l);
+  Down(p);
+  Print(v[p].s[0]);
   if (v[p].v >= 1 && v[p].v <= n) {
     cout << v[p].v << ' ';
   }
-  Print(v[p].r);
+  Print(v[p].s[1]);
 }
 
 int main() {
   cin >> n >> q;
-  v[0].v = -1;
   for (int i = 1; i <= n + 2; i++) {
-    v[i].r = (i + 1) % (n + 3), v[i].v = i - 1;
+    v[i] = {0, i - 1, n + 3 - i, 0, i + 1};
   }
+  v[n + 2].s[1] = 0;
   while (q--) {
     cin >> l >> r;
-    TurnUp(s, r + 1, 1);
-    Update(s, r + 1);
-    TurnUp(s, l - 1, 0);
-    Update(v[s].l, l - 1) || Update(v[s].r, l - 1);
-    v[v[v[s].l].r].t++;
+    Swap(l, r);
   }
   Print(s);
   return 0;
