60a44268a5e57d6fdb119fad2ca82a4929c2d1ab
DengYufan <2695375004@qq.com>
2022-01-23 18:29:06 +0800

    .

--- CF/320B.cpp
+++ CF/320B.cpp
@@ -3,8 +3,10 @@
 
 using namespace std;
 
-int n, m, f[101];
-pair<int, int> a[101];
+struct A {
+  int b, x, y;
+} a[101];
+int n, f[101];
 
 int GetRoot(int x) {
   return f[x] == x ? x : f[x] = GetRoot(f[x]);
@@ -12,18 +14,17 @@ int GetRoot(int x) {
 
 int main() {
   cin >> n;
-  for (int i = 1, o, x, y; i <= n; i++) {
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i].b >> a[i].x >> a[i].y;
     f[i] = i;
-    cin >> o >> x >> y;
-    if (o == 1) {
-      a[++m] = {x, y};
-      for (int j = 1; j < m; j++) {
-        if (a[j].first < x && a[j].second > x || a[j].first < y && a[j].second > y) {
+    if (a[i].b == 1) {
+      for (int j = 1; j < i; j++) {
+        if (a[i].b == 1 && a[i].x < a[j].x && a[j].x < a[i].y || a[i].x < a[j].y && a[j].y < a[i].y) {
           f[GetRoot(i)] = GetRoot(j);
         }
       }
     } else {
-      cout << (GetRoot(x) == GetRoot(y) ? "YES\n" : "NO\n");
+      cout << (GetRoot(a[i].x) == GetRoot(a[i].y) ? "YES\n" : "NO\n");
     }
   }
   return 0;
