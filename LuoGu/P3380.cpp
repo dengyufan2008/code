9910b99065f1d3dad17417a2db118d63c3ce0293
dengyufan2008 <2695375004@qq.com>
2022-01-25 16:04:08 +0800

    .

--- LuoGu/P3380.cpp
+++ LuoGu/P3380.cpp
@@ -6,24 +6,24 @@
 using namespace std;
 
 struct V1 {
-  int v, c, k, s[2];
+  LL v, k, c, s[2];
 };
 struct B {  // Balance Tree
-  int s;
+  LL s;
   vector<V1> v = {{}};
 
-  void Update(int p) {
+  void Update(LL p) {
     v[p].c = v[v[p].s[0]].c + v[v[p].s[1]].c + (p > 0);
   }
 
-  void Turn(int &p, bool b) {
-    int q = v[p].s[!b];
+  void Turn(LL &p, bool b) {
+    LL q = v[p].s[!b];
     v[p].s[!b] = v[q].s[b], v[q].s[b] = p;
     Update(p), Update(q);
     p = q;
   }
 
-  void Rebalance(int &p) {
+  void Rebalance(LL &p) {
     if (v[p].k < v[v[p].s[0]].k) {
       Turn(p, 1);
     } else if (v[p].k < v[v[p].s[1]].k) {
@@ -31,17 +31,17 @@ struct B {  // Balance Tree
     }
   }
 
-  void Insert(int &p, int x) {
+  void Insert(LL &p, LL x) {
     if (!p) {
-      p = v.size(), v.push_back({x, 1, rand()});
+      p = v.size(), v.push_back({x, rand()});
     } else {
       Insert(v[p].s[v[p].v < x], x);
     }
     Update(p);
   }
 
-  int Replace(int &p) {
-    int t = v[p].v;
+  LL Replace(LL &p) {
+    LL t = v[p].v;
     if (!v[p].s[1]) {
       p = v[p].s[0];
     } else {
@@ -51,7 +51,7 @@ struct B {  // Balance Tree
     return t;
   }
 
-  void Delete(int &p, int x) {
+  void Delete(LL &p, LL x) {
     if (v[p].v == x) {
       if (!v[p].s[0] || !v[p].s[1]) {
         p = v[p].s[0] + v[p].s[1];
@@ -64,7 +64,7 @@ struct B {  // Balance Tree
     Update(p);
   }
 
-  int FindRank(int p, int x) {
+  LL FindRank(LL p, LL x) {
     if (!p) {
       return 0;
     } else if (v[p].v < x) {
@@ -74,8 +74,8 @@ struct B {  // Balance Tree
     }
   }
 
-  int FindVal(int p, int x) {
-    int t = v[v[p].s[0]].c + 1;
+  LL FindVal(LL p, LL x) {
+    LL t = v[v[p].s[0]].c + 1;
     if (t == x) {
       return v[p].v;
     } else if (t > x) {
@@ -85,7 +85,7 @@ struct B {  // Balance Tree
     }
   }
 
-  int FindPast(int p, int x) {
+  LL FindPast(LL p, LL x) {
     if (!p) {
       return -2147483647;
     } else if (v[p].v < x) {
@@ -95,7 +95,7 @@ struct B {  // Balance Tree
     }
   }
 
-  int FindNext(int p, int x) {
+  LL FindNext(LL p, LL x) {
     if (!p) {
       return 2147483647;
     } else if (v[p].v > x) {
@@ -106,23 +106,33 @@ struct B {  // Balance Tree
   }
 };
 struct S {  // Seg Tree
-  vector<B> v = {{}};
+  B v[50001];
 
-  void Modify(int p, int l, int r, int x, int _k, int k) {
-    int mid = (l + r) / 2;
+  void Init(LL n) {
+    for (LL i = 1; i <= n * 4; i++) {
+      v[i].Insert(v[i].s, 0);
+    }
+  }
+
+  void Modify(LL p, LL l, LL r, LL x, LL _k, LL k) {
+    if (l == r) {
+      v[p].Delete(v[p].s, _k), v[p].Insert(v[p].s, k);
+      return;
+    }
+    LL mid = (l + r) / 2;
     if (mid >= x) {
       Modify(p * 2, l, mid, x, _k, k);
     } else {
       Modify(p * 2 + 1, mid + 1, r, x, _k, k);
     }
-    v[p].Delete(v[p].s, _k);
+    v[p].Delete(v[p].s, _k), v[p].Insert(v[p].s, k);
   }
 
-  int FindRank(int p, int l, int r, int _l, int _r, int k) {
+  LL FindRank(LL p, LL l, LL r, LL _l, LL _r, LL k) {
     if (l >= _l && r <= _r) {
       return v[p].FindRank(v[p].s, k);
     }
-    int mid = (l + r) / 2, ans = 0;
+    LL mid = (l + r) / 2, ans = 0;
     if (mid >= _l) {
       ans += FindRank(p * 2, l, mid, _l, _r, k);
     }
@@ -132,11 +142,11 @@ struct S {  // Seg Tree
     return ans;
   }
 
-  int FindPast(int p, int l, int r, int _l, int _r, int k) {
+  LL FindPast(LL p, LL l, LL r, LL _l, LL _r, LL k) {
     if (l >= _l && r <= _r) {
       return v[p].FindPast(v[p].s, k);
     }
-    int mid = (l + r) / 2, ans = -2147483647;
+    LL mid = (l + r) / 2, ans = -2147483647;
     if (mid >= _l) {
       ans = max(ans, FindPast(p * 2, l, mid, _l, _r, k));
     }
@@ -146,11 +156,11 @@ struct S {  // Seg Tree
     return ans;
   }
 
-  int FindNext(int p, int l, int r, int _l, int _r, int k) {
+  LL FindNext(LL p, LL l, LL r, LL _l, LL _r, LL k) {
     if (l >= _l && r <= _r) {
       return v[p].FindNext(v[p].s, k);
     }
-    int mid = (l + r) / 2, ans = 2147483647;
+    LL mid = (l + r) / 2, ans = 2147483647;
     if (mid >= _l) {
       ans = min(ans, FindNext(p * 2, l, mid, _l, _r, k));
     }
@@ -160,25 +170,26 @@ struct S {  // Seg Tree
     return ans;
   }
 } s;
-int n, m, a[50001];
+LL n, m, a[50001];
 
 int main() {
   srand(unsigned(time(0)));
   cin >> n >> m;
-  for (int i = 1; i <= n; i++) {
+  s.Init(n);
+  for (LL i = 1; i <= n; i++) {
     cin >> a[i];
     s.Modify(1, 1, n, i, 0, a[i]);
   }
-  for (int i = 1, o, x, y, k; i <= m; i++) {
+  for (LL i = 1, o, x, y, k; i <= m; i++) {
     cin >> o >> x >> y;
     if (o == 1) {
       cin >> k;
       cout << s.FindRank(1, 1, n, x, y, k) + 1 << '\n';
     } else if (o == 2) {
       cin >> k;
-      int l = 0, r = 1e8;
+      LL l = 0, r = 1e8;
       while (l <= r) {
-        int mid = (l + r) / 2, c = s.FindRank(1, 1, n, x, y, mid) + 1;
+        LL mid = (l + r) / 2, c = s.FindRank(1, 1, n, x, y, mid) + 1;
         if (c >= k) {
           r = mid - 1;
         } else {
