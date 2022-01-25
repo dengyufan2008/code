9ca4c355b5e97ecd3912d1c25b84cad536907b09
dengyufan2008 <2695375004@qq.com>
2022-01-25 09:06:14 +0800

    .

---
+++ LuoGu/P1235.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#define LL long long
+
+using namespace std;
+
+struct V {
+  int d;
+  vector<int> e;
+} v[301];
+int n, k;
+
+int main() {
+  
+  return 0;
+}
