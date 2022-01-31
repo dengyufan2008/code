ec6a1d2946650bbc5798521e2f93f4a84086b441
dengyufan2008 <2695375004@qq.com>
2022-01-31 13:18:38 +0800

    .

--- LuoGu/P1285.cpp
+++ LuoGu/P1285.cpp
@@ -8,11 +8,9 @@ bool b[101][101];
 
 int main() {
   cin >> n;
-  for (int i = 1, x, y; i <= n; i++) {
-    cin >> x;
-    while (x--) {
-      cin >> y;
-      b[i][y] = 1;
+  for (int i = 1, j; i <= n; i++) {
+    for (cin >> j; j; cin >> j) {
+      b[i][j] = 1;
     }
   }
   
