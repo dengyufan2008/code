feb654852d6997ebc64133f286332d3ed100224f
dengyufan2008 <2695375004@qq.com>
2022-02-04 16:39:08 +0800

    .

--- Ybt/1414.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-using namespace std;
-int main() {
-  int zy, xc, qm, ans;
-  scanf("%d%d%d", &zy, &xc, &qm);
-  ans = zy * 2 / 10 + xc * 3 / 10 + qm * 5 / 10;
-  printf("%d", ans);
-  return 0;
-}
