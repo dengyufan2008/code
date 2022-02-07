226d95958d448fe6d33813061f267b2afa0abdf9
dengyufan2008 <2695375004@qq.com>
2022-02-07 12:07:06 +0800

    .

--- LuoGu/P5030.cpp
+++ LuoGu/P5030.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int kMove[4][2] = {{1, 3}, {3, 1}, {1, -3}, {3, -1}};
+const int kMove[8][2] = {{1, 3}, {3, 1}, {1, -3}, {3, -1}, {-1, 3}, {-3, 1}, {-1, -3}, {-3, -1}};
 int n, m, k, ans, b[40000], v[40000];
 bool a[200][200];
 vector<int> e[40000];
@@ -24,22 +24,26 @@ bool T(int x, int k) {
 
 int main() {
   cin >> n >> m >> k;
-  for (int i = 1, x, y; i <= k; i++) {
+  for (int i = k, x, y; i--;) {
     cin >> x >> y;
-    a[x - 1][y - 1] = 1;
+    k -= a[x - 1][y - 1], a[x - 1][y - 1] = 1;
   }
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      for (int k = 0; k < 4; k++) {
+      for (int k = 0; k < 8; k++) {
         int _i = i + kMove[k][0], _j = j + kMove[k][1];
         if (_i < n && _j < n && _j >= 0 && !a[i][j] && !a[_i][_j]) {
-          e[i * 200 + j].push_back(_i * 200 + _j), e[_i * 200 + _j].push_back(i * 200 + j);
+          if (i & 1) {
+            e[i * 200 + j].push_back(_i * 200 + _j);
+          } else {
+            e[_i * 200 + _j].push_back(i * 200 + j);
+          }
         }
       }
     }
   }
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
+    for (int j = 0; j < m; j++) {
       if (!a[i][j]) {
         ans += T(i * 200 + j, i * 200 + j);
       }
