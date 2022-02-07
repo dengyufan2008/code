45e6d00038b82cedb5fd534e160c70e15feb2b45
dengyufan2008 <2695375004@qq.com>
2022-02-07 13:43:24 +0800

    .

--- LuoGu/P5030.cpp
+++ LuoGu/P5030.cpp
@@ -25,13 +25,13 @@ int main() {
   cin >> n >> m >> k;
   for (int i = k, x, y; i--;) {
     cin >> x >> y;
-    k -= b[x * 200 + y - 201] == kInf, b[x * 200 + y - 201] = kInf;
+    k -= !b[x * 200 + y - 201], b[x * 200 + y - 201] = kInf;
   }
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       for (int k = 0; k < 4; k++) {
         int _i = i + kMove[k][0], _j = j + kMove[k][1];
-        if (_i < n && _j < m && _j >= 0 && b[i * 200 + j] != kInf && b[_i * 200 + _j] != kInf) {
+        if (_i < n && _j < m && _j >= 0 && !b[i * 200 + j] && !b[_i * 200 + _j]) {
           if (i & 1) {
             e[i * 200 + j].push_back(_i * 200 + _j);
           } else {
