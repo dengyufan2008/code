538c6aca4c8a4a827c21bd69c956ee7e2aae2942
dengyufan2008 <2695375004@qq.com>
2022-02-07 11:45:20 +0800

    .

---
+++ LuoGu/P5030.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#define LL long long
+
+using namespace std;
+
+const int kMove[4][2] = {{1, 3}, {3, 1}, {1, -3}, {3, -1}};
+int n, m, k, ans, b[40000], v[40000];
+bool a[200][200];
+vector<int> e[40000];
+
+bool T(int x, int k) {
+  if (b[x] != k) {
+    b[x] = k;
+    for (int i : e[x]) {
+      if (!v[i] || T(v[i], k)) {
+        v[i] = x;
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+int main() {
+  cin >> n >> m >> k;
+  for (int i = 1, x, y; i <= k; i++) {
+    cin >> x >> y;
+    a[x - 1][y - 1] = 1;
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      for (int k = 0; k < 4; k++) {
+        int _i = i + kMove[k][0], _j = j + kMove[k][1];
+        if (_i < n && _j < n && _j >= 0 && !a[i][j] && !a[_i][_j]) {
+          e[i * 200 + j].push_back(_i * 200 + _j), e[_i * 200 + _j].push_back(i * 200 + j);
+        }
+      }
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (!a[i][j]) {
+        ans += T(i * 200 + j, i * 200 + j);
+      }
+    }
+  }
+  cout << n * m - k - ans;
+  return 0;
+}
