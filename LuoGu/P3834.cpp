3a8fcbbfcd9639fbada44eeddaaad3bd8efb9ded
dengyufan2008 <2695375004@qq.com>
2022-02-09 09:37:35 +0800

    .

---
+++ LuoGu/P3834.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+struct V {
+  int v, l, r;
+} v[4000001];
+int n, m, s[1000001] = {1};
+
+int main() {
+  cin >> n >> m;
+  for (int i = 1, x; i <= n; i++) {
+    cin >> x;
+    Insert(s[0])
+  }
+  return 0;
+}
