89b38a96dad4b641378b33506b7c4f58f7e16a3b
dengyufan2008 <2695375004@qq.com>
2022-02-12 12:37:41 +0800

    .

---
+++ DengYufan/position/position.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define LL long long
+
+using namespace std;
+
+int main() {
+  // freopen("position.in", "r", stdin);
+  // freopen("position.out", "w", stdout);
+  
+  return 0;
+}
