69dd2d19e35d0c8822d9715c53b918b78b30b064
dengyufan2008 <2695375004@qq.com>
2022-02-19 00:02:04 +0800

    .

--- LuoGu/P2071.cpp
+++ LuoGu/P2071.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <vector>
 #define LL long long
 
 using namespace std;
 
 int n, k, ans, b[4001], v[4001];
-bool l[4001][4001];
+vector<int> l[4001];
 
 bool T(int x) {
   if (b[x] != k) {
     b[x] = k;
-    for (int i = 1; i <= n * 2; i++) {
-      if (l[x][i] && (!v[i] || T(v[i]))) {
+    for (int i : l[x]) {
+      if (!v[i] || T(v[i])) {
         v[i] = x;
         return 1;
       }
@@ -23,7 +24,8 @@ int main() {
   cin >> n;
   for (int i = 1, x, y; i <= n * 2; i++) {
     cin >> x >> y;
-    l[i][x] = l[i][y] = l[i][x + n] = l[i][y + n] = 1;
+    l[i].push_back(x), l[i].push_back(y);
+    l[i].push_back(x + n), l[i].push_back(y + n);
   }
   for (int i = 1; i <= n * 2; i++) {
     k++, ans += T(i);
