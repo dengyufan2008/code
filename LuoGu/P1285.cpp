3555f9f9c3be14ca3c321fb0c5a77ccd20b7f735
dengyufan2008 <2695375004@qq.com>
2022-02-20 09:01:52 +0800

    .

--- LuoGu/P1285.cpp
+++ LuoGu/P1285.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
 #define LL long long
 
 using namespace std;
 
+struct V {
+  vector<int> e;
+} v[101];
+int n;
+bool b[101];
+
 int main() {
-  
+  cin >> n;
+  for (int i = 1, x; i <= n; i++) {
+    fill(&b[0], &b[100] + 1, 0);
+    
+  }
   return 0;
 }
