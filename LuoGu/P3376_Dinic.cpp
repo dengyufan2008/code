2825e65d01f7ecd325f260fba99fe53464df4ddf
dengyufan2008 <2695375004@qq.com>
2022-03-03 13:54:16 +0800

    .

--- LuoGu/P3376_Dinic.cpp
+++ LuoGu/P3376_Dinic.cpp
@@ -4,35 +4,55 @@
 
 using namespace std;
 
+const LL kInf = 1e18;
 struct E {
-  int x, v, p;
-} e[402];
+  LL x, v, p;
+} e[10001];
 struct V {
-  int et, b, d;
+  LL et, n, d;
 } v[201];
-int n, m, s, t, k = 1;
+LL n, m, s, t, ans;
 
-void Bfs() {
-  queue<pair<int, int>> q;
-  for (q.push({s, 0}); !q.empty(); q.pop()) {
-    auto c = q.front();
-    if (v[c.first].b == k) {
-      continue;
-    }
-    v[c.first].b = k, v[c.first].d = c.second;
-    for (int i = v[c.first].et; i; i = e[i].p) {
-      q.push({e[i].x, c.second + 1});
+bool Bfs() {
+  queue<LL> q;
+  for (int i = 1; i <= n; i++) {
+    v[i].d = i == s, v[i].n = v[i].et;
+  }
+  for (q.push(s); !q.empty(); q.pop()) {
+    int c = q.front();
+    for (LL i = v[c].et; i; i = e[i].p) {
+      if (e[i].v && !v[e[i].x].d) {
+        v[e[i].x].d = v[c].d + 1, q.push({e[i].x});
+      }
     }
   }
+  return v[t].d;
+}
+
+LL Dfs(LL x, LL in) {
+  if (x == t) {
+    return in;
+  }
+  LL out = 0;
+  for (LL &i = v[x].n; i && in; in && (i = e[i].p)) {
+    if (e[i].v && v[e[i].x].d == v[x].d + 1) {
+      LL t = Dfs(e[i].x, min(in, e[i].v));
+      e[i].v -= min(in, t), e[i ^ 1].v += min(in, t);
+      in -= t, out += t;
+    }
+  }
+  return out;
 }
 
 int main() {
   cin >> n >> m >> s >> t;
-  for (int i = 1, x, y, z; i <= m; i++) {
-    cin >> x >> y >> z;
-    e[i * 2] = {y, z, v[x].et}, v[x].et = i * 2;
-    e[i * 2 + 1] = {x, z, v[y].et}, v[y].et = i * 2 + 1;
+  for (LL i = 1, x, in, z; i <= m; i++) {
+    cin >> x >> in >> z;
+    e[i * 2] = {in, z, v[x].et}, v[x].et = i * 2;
+    e[i * 2 + 1] = {x, 0, v[in].et}, v[in].et = i * 2 + 1;
   }
-  
+  for (; Bfs(); ans += Dfs(s, kInf)) {
+  }
+  cout << ans;
   return 0;
 }
