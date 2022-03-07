2af3f07f536f720d3aa0a41ea823f0a811e957eb
dengyufan2008 <2695375004@qq.com>
2022-03-07 22:37:34 +0800

    .

--- LuoGu/P2762.cpp
+++ LuoGu/P2762.cpp
@@ -13,17 +13,20 @@ struct V {
 } v[103];
 LL n, m, c, tot, ans;
 
+void AddE(LL x, LL y, LL z) {
+  e[++c] = {y, z, v[x].et}, v[x].et = c;
+  e[++c] = {x, 0, v[y].et}, v[y].et = c;
+}
+
 bool Bfs() {
   queue<LL> q;
-  for (int i = 1; i <= n + m + 2; i++) {
-    v[i].d = i == n + m + 1;
-    v[i].nt = v[i].et;
+  for (LL i = 1; i <= n + m + 2; i++) {
+    v[i].d = i == n + m + 1, v[i].nt = v[i].et;
   }
   for (q.push(n + m + 1); !q.empty(); q.pop()) {
-    int c = q.front();
-    for (LL i = v[c].et; i; i = e[i].p) {
+    for (LL i = v[q.front()].et; i; i = e[i].p) {
       if (e[i].v && !v[e[i].x].d) {
-        v[e[i].x].d = v[c].d + 1, q.push({e[i].x});
+        v[e[i].x].d = v[q.front()].d + 1, q.push(e[i].x);
       }
     }
   }
@@ -38,7 +41,7 @@ LL Dfs(LL x, LL in) {
   for (LL &i = v[x].nt; i && in; in && (i = e[i].p)) {
     if (e[i].v && v[e[i].x].d == v[x].d + 1) {
       LL t = Dfs(e[i].x, min(in, e[i].v));
-      e[i].v -= min(in, t), e[i ^ 1].v += min(in, t);
+      e[i].v -= t, e[i ^ 1].v += t;
       in -= t, out += t;
     }
   }
@@ -47,35 +50,33 @@ LL Dfs(LL x, LL in) {
 
 int main() {
   cin >> n >> m;
-  for (int i = 1, x, y; i <= n; i++) {
+  for (LL i = 1, x, y; i <= n; i++) {
     cin >> x;
     tot += x;
-    e[++c] = {i, x, v[n + m + 1].et}, v[n + m + 1].et = c;
-    e[++c] = {n + m + 1, 0, v[i].et}, v[i].et = c;
-    while (getchar() == ' ') {
+    AddE(n + m + 1, i, x);
+    while (getchar() == 32) {
       cin >> y;
-      e[++c] = {n + y, kInf, v[i].et}, v[i].et = c;
-      e[++c] = {i, 0, v[n + y].et}, v[n + y].et = c;
+      AddE(i, n + y, kInf);
     }
   }
-  for (int i = 1, x; i <= m; i++) {
+  for (LL i = 1, x; i <= m; i++) {
     cin >> x;
-    e[++c] = {n + m + 2, x, v[n + i].et}, v[n + i].et = c;
-    e[++c] = {n + i, 0, v[n + m + 2].et}, v[n + m + 2].et = c;
+    AddE(n + i, n + m + 2, x);
   }
   for (; Bfs(); ans += Dfs(n + m + 1, kInf)) {
   }
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     if (v[i].d) {
       cout << i << ' ';
     }
   }
   cout << '\n';
-  for (int i = n + 1; i <= n + m; i++) {
-    if (v[i].d) {
-      cout << i - n << ' ';
+  for (LL i = 1; i <= m; i++) {
+    if (v[n + i].d) {
+      cout << i << ' ';
     }
   }
-  cout << '\n' << tot - ans << '\n';
+  cout << '\n'
+       << tot - ans;
   return 0;
 }
