20ebc0ea19f19c5212e1228e699cd53bc6942a37
dengyufan2008 <2695375004@qq.com>
2022-06-11 17:13:23 +0800

    .

--- LuoGu/P3919.cpp
+++ LuoGu/P3919.cpp
@@ -5,8 +5,24 @@ using namespace std;
 
 struct V {
   int l, r, v;
-} v[8000001];
+} v[32000001];
 int n, m, k1, k2, a[1000001], h[1000001];
+
+int Read() {
+  int x = 0, f = 1;
+  char c = getchar();
+  while (c < '0' || c > '9') {
+    if (c == '-') {
+      f = -1;
+    }
+    c = getchar();
+  }
+  while (c > '/' && c < ':') {
+    x = (x << 3) + (x << 1) + (c - '0');
+    c = getchar();
+  }
+  return f * x;
+}
 
 void Copy(int &p) {
   v[++k1] = v[p];
@@ -27,25 +43,38 @@ void Change(int &p, int l, int r, int x, int y) {
   Copy(p);
   if (l == r) {
     v[p].v = y;
+    return;
   }
   int mid = l + r >> 1;
-
+  if (x <= mid) {
+    Change(v[p].l, l, mid, x, y);
+  } else {
+    Change(v[p].r, mid + 1, r, x, y);
+  }
 }
 
 int Ask(int &p, int l, int r, int x) {
-
+  if (l == r) {
+    return v[p].v;
+  }
+  int mid = l + r >> 1;
+  if (x <= mid) {
+    return Ask(v[p].l, l, mid, x);
+  } else {
+    return Ask(v[p].r, mid + 1, r, x);
+  }
 }
 
 int main() {
-  cin >> n >> m;
+  n = Read(), m = Read();
   for (int i = 1; i <= n; i++) {
-    cin >> a[i];
+    a[i] = Read();
   }
   Build(h[0], 1, n);
   for (int i = 1, o, x, y, z; i <= m; i++) {
-    cin >> x >> o >> y;
+    x = Read(), o = Read(), y = Read();
     if (o == 1) {
-      cin >> z;
+      z = Read();
       Change(h[++k2] = h[x], 1, n, y, z);
     } else {
       cout << Ask(h[++k2] = h[x], 1, n, y) << '\n';
