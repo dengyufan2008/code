7898330025ad1968d13f32766478be4393c08934
dengyufan2008 <2695375004@qq.com>
2022-06-11 14:07:24 +0800

    .

--- LuoGu/P3224.cpp
+++ LuoGu/P3224.cpp
@@ -3,7 +3,80 @@
 
 using namespace std;
 
+struct V {
+  int l, r, d;
+} v[2100001];
+int n, m, q, k, a[100001], b[100001], s[100001], f[100001];
+char ch;
+
+int GetRoot(int x) {
+  return f[x] == x ? x : f[x] = GetRoot(f[x]);
+}
+
+void Insert(int &p, int l, int r, int x) {
+  if (!p) {
+    p = ++k;
+  }
+  v[p].d++;
+  if (l == r) {
+    return;
+  }
+  int mid = l + r >> 1;
+  if (x <= mid) {
+    Insert(v[p].l, l, mid, x);
+  } else {
+    Insert(v[p].r, mid + 1, r, x);
+  }
+}
+
+void Merge(int &p, int &q, int l, int r) {
+  if (!p || !q) {
+    p = p ? p : q;
+    return;
+  } else if (l == r) {
+    v[p].d += v[q].d;
+    return;
+  }
+  int mid = l + r >> 1;
+  Merge(v[p].l, v[q].l, l, mid);
+  Merge(v[p].r, v[q].r, mid + 1, r);
+  v[p].d = v[v[p].l].d + v[v[p].r].d;
+}
+
+int Ask(int p, int l, int r, int x) {
+  if (l == r) {
+    return b[l];
+  }
+  int mid = l + r >> 1;
+  if (x <= v[v[p].l].d) {
+    return Ask(v[p].l, l, mid, x);
+  } else {
+    return Ask(v[p].r, mid + 1, r, x - v[v[p].l].d);
+  }
+}
+
 int main() {
-  
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    b[a[i]] = i, f[i] = i, Insert(s[i] = ++k, 1, n, a[i]);
+  }
+  for (int i = 1, x, y; i <= m; i++) {
+    cin >> x >> y;
+    if (GetRoot(x) != GetRoot(y)) {
+      Merge(s[GetRoot(x)], s[GetRoot(y)], 1, n);
+      f[GetRoot(y)] = GetRoot(x);
+    }
+  }
+  cin >> q;
+  for (int i = 1, x, y; i <= q; i++) {
+    cin >> ch >> x >> y;
+    if (ch == 'Q') {
+      cout << (v[GetRoot(x)].d < y ? -1 : Ask(s[GetRoot(x)], 1, n, y)) << '\n';
+    } else if (GetRoot(x) != GetRoot(y)) {
+      Merge(s[GetRoot(x)], s[GetRoot(y)], 1, n);
+      f[GetRoot(y)] = GetRoot(x);
+    }
+  }
   return 0;
 }
