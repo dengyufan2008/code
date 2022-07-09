fd3606bee8f4b29bede973a3449878bb6cabe1f1
dengyufan2008 <2695375004@qq.com>
2022-07-09 20:28:55 +0800

    .

--- AT/ABC259B.cpp
+++ AT/ABC259B.cpp
@@ -1,9 +1,15 @@
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #define LL long long
 
 using namespace std;
 
+const double kPi = acos(-1);
+double a, b, d;
+
 int main() {
-  
+  cin >> a >> b >> d;
+  cout << fixed << setprecision(8) << a * cos(d / 180.0 * kPi) - b * sin(d / 180.0 * kPi) << ' ' << a * sin(d / 180.0 * kPi) + b * cos(d / 180.0 * kPi);
   return 0;
 }
