bd10c6cfc2b143092ac371ad1a0ae9cf71d0b441
dengyufan2008 <2695375004@qq.com>
2022-07-09 13:36:09 +0800

    .

--- LuoGu/P3812.cpp
+++ LuoGu/P3812.cpp
@@ -3,7 +3,36 @@
 
 using namespace std;
 
+LL n, m, d[51];
+
+void Insert(LL x) {
+  for (LL i = 50; i >= 0; i--) {
+    if (x & (1LL << i)) {
+      if (!d[i]) {
+        d[i] = x;
+        return;
+      }
+      x ^= d[i];
+    }
+  }
+}
+
+LL Max() {
+  LL ans = 0;
+  for (LL i = 50; i >= 0; i--) {
+    if ((ans ^ d[i]) > ans) {
+      ans ^= d[i];
+    }
+  }
+  return ans;
+}
+
 int main() {
-  
+  cin >> n;
+  for (LL i = 1, x; i <= n; i++) {
+    cin >> x;
+    Insert(x);
+  }
+  cout << Max();
   return 0;
 }
