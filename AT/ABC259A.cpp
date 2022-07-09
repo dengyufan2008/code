cc332889d2bc93d62b0d0d7311eafd15ad7963a4
dengyufan2008 <2695375004@qq.com>
2022-07-09 20:19:13 +0800

    .

--- AT/ABC259A.cpp
+++ AT/ABC259A.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 
+int n, m, x, t, d;
+
 int main() {
-  
+  cin >> n >> m >> x >> t >> d;
+  if (m >= x) {
+    cout << t;
+  } else {
+    cout << t - (x - m) * d;
+  }
   return 0;
 }
