e6947e0210ed33d1fd157c676af00fe3e29f4d02
dengyufan2008 <2695375004@qq.com>
2022-07-24 17:05:26 +0800

    .

--- LuoGu/P2664.cpp
+++ LuoGu/P2664.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 struct V {
-  int c, s, w, t;
+  LL c, s, w, t;
   bool b;
-  vector<int> e;
+  vector<LL> e;
 } v[100001];
-int n, s, r, t, b[100001], c[100001];
+LL n, s, r, t, b[100001], c[100001];
 
-void GetRoot(int f, int x) {
-  v[x].s = 1, v[x].w = 0, c[v[x].c] = 0;
-  for (int i : v[x].e) {
+void GetRoot(LL f, LL x) {
+  v[x].s = 1, v[x].w = b[v[x].c] = c[v[x].c] = 0;
+  for (LL i : v[x].e) {
     if (i != f && !v[i].b) {
       GetRoot(x, i);
       v[x].s += v[i].s, v[x].w = max(v[x].w, v[i].s);
@@ -24,12 +24,12 @@ void GetRoot(int f, int x) {
   }
 }
 
-void CalcCol(int f, int x, int p) {
+void CalcCol(LL f, LL x, LL p) {
   bool o = 0;
   if (!b[v[x].c]) {
     o = b[v[x].c] = 1, c[v[x].c] += p * v[x].s, t += p * v[x].s;
   }
-  for (int i : v[x].e) {
+  for (LL i : v[x].e) {
     if (i != f && !v[i].b) {
       CalcCol(x, i, p);
     }
@@ -39,13 +39,13 @@ void CalcCol(int f, int x, int p) {
   }
 }
 
-void CalcAns(int f, int x, int s) {
+void CalcAns(LL f, LL x, LL s) {
   bool o = 0;
   if (!b[v[x].c]) {
     o = b[v[x].c] = 1, t -= c[v[x].c] - s;
   }
   v[x].t += t;
-  for (int i : v[x].e) {
+  for (LL i : v[x].e) {
     if (i != f && !v[i].b) {
       CalcAns(x, i, s);
     }
@@ -59,7 +59,7 @@ void Divide() {
   v[r].b = 1, t = 0;
   CalcCol(0, r, 1);
   v[r].t += t, b[v[r].c] = 1, t -= c[v[r].c];
-  for (int i : v[r].e) {
+  for (LL i : v[r].e) {
     if (!v[i].b) {
       t += s - v[i].s;
       CalcCol(r, i, -1);
@@ -68,7 +68,7 @@ void Divide() {
       t -= s - v[i].s;
     }
   }
-  for (int i : v[r].e) {
+  for (LL i : v[r].e) {
     if (!v[i].b) {
       s = v[i].s, GetRoot(r, i), GetRoot(0, r), Divide();
     }
@@ -77,15 +77,15 @@ void Divide() {
 
 int main() {
   cin >> n;
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     cin >> v[i].c;
   }
-  for (int i = 1, x, y; i < n; i++) {
+  for (LL i = 1, x, y; i < n; i++) {
     cin >> x >> y;
     v[x].e.push_back(y), v[y].e.push_back(x);
   }
   s = n, GetRoot(0, 1), GetRoot(0, r), Divide();
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     cout << v[i].t << '\n';
   }
   return 0;
