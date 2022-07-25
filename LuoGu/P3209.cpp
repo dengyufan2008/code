c75d9b0d5e851595d728b697e494c42911f86e8d
dengyufan2008 <2695375004@qq.com>
2022-07-25 22:26:22 +0800

    .

--- LuoGu/P3209.cpp
+++ LuoGu/P3209.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -11,18 +10,19 @@ struct V {
   int d, l, r;
   vector<int> e;
 } v[1214];
-int t, n, m, k, l, d[2][201];
-bool ans = 1;
+int t, n, m, _m, k, l, d[2][201];
+bool ans;
 PII a[607];
+vector<PII> c;
 stack<int> s;
 
-void S(int x) {
+void Tarjan(int x) {
   if (v[x].d) {
     return;
   }
   v[x].d = v[x].l = ++k, s.push(x);
   for (int i : v[x].e) {
-    S(i);
+    Tarjan(i);
     if (!v[i].r) {
       v[x].l = min(v[x].l, v[i].l);
     }
@@ -41,6 +41,8 @@ void S(int x) {
 int main() {
   cin >> t;
   while (t--) {
+    k = l = _m = 0, ans = 1, c.clear();
+    fill(&v[1], &v[1214], v[0]);
     cin >> n >> m;
     if (m > n * 3 + 6) {
       cout << "NO\n";
@@ -52,24 +54,30 @@ int main() {
     for (int i = 1; i <= n; i++) {
       cin >> d[0][i];
       d[1][d[0][i]] = i;
-      v[i].d = v[i].l = v[i].r = 0;
-      v[i].e.clear();
     }
     for (int i = 1; i <= m; i++) {
       a[i].first = d[1][a[i].first], a[i].second = d[1][a[i].second];
-    }
-    sort(a + 1, a + m + 1);
-    for (int i = 1; i <= m; i++) {
-      for (int j = i + 1; j < i + m; j++) {
-        int k = (j - 1) % m + 1;
-        if (a[i].first < a[k].first && a[i].second < a[k].second || a[i].first > a[k].first && a[i].second > a[k].second) {
-          v[i].e.push_back(k + m), v[i + m].e.push_back(k);
-          v[k].e.push_back(i + m), v[k + m].e.push_back(i);
-        }
+      if (a[i].first > a[i].second) {
+        swap(a[i].first, a[i].second);
       }
     }
     for (int i = 1; i <= m; i++) {
-      S(i), S(i + m);
+      for (int j = 1; j <= m; j++) {
+        if (i != j && a[i].second - a[i].first != 1 && a[j].second - a[j].first != 1 &&
+            a[i].second - a[i].first != n - 1 && a[j].second - a[j].first != n - 1 &&
+            (a[i].first < a[j].first && a[i].second < a[j].second ||
+             a[i].first > a[j].first && a[i].second > a[j].second)) {
+          _m += 2, c.push_back({i, j});
+        }
+      }
+    }
+    m = _m;
+    for (auto i : c) {
+      v[i.first].e.push_back(i.second + m);
+      v[i.first + m].e.push_back(i.second);
+    }
+    for (int i = 1; i <= m; i++) {
+      Tarjan(i), Tarjan(i + m);
     }
     for (int i = 1; i <= m; i++) {
       ans &= v[i].r != v[i + m].r;
