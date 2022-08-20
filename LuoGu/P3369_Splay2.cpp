a35173602813c1420e522d08f46e1d5a250d8cc9
dengyufan2008 <2695375004@qq.com>
2022-08-20 13:04:21 +0800

    .

--- LuoGu/P3369_Splay2.cpp
+++ LuoGu/P3369_Splay2.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 const int kInf = 1e9;
 struct V {
-  int v, c, s[2];
+  int v, d, c, s[2];
 } v[100001];
 int n, m, s;
 vector<bool> l;
 
 void Update(int p) {
-  v[p].c = v[v[p].s[0]].c + v[v[p].s[1]].c + (p > 0);
+  v[p].c = v[v[p].s[0]].c + v[v[p].s[1]].c + (p != 0) * v[p].d;
 }
 
 void Turn(int &p, bool b) {
@@ -31,18 +31,29 @@ void Rebalance(int &p, int x) {
   }
 }
 
-void Insert(int &p, int x) {
+void Insert(int &p, int x) {  // CHICK
   if (!p) {
     v[p = ++m] = {x, 1};
     Rebalance(s, v[p].v);
+  } else if (v[p].v == x) {
+    v[p].c++;
+    Rebalance(s, v[p].v);
   } else {
     Insert(v[p].s[x >= v[p].v], x);
-    Update(p);
   }
+  Update(p);
+  // if (!p || v[p].v == x) {
+  //   !p && (p = ++m);
+  //   v[p].v = x, v[p].d++;
+  //   Rebalance(s, v[p].v);
+  // } else {
+  //   Insert(v[p].s[x >= v[p].v], x);
+  // }
+  // Update(p);
 }
 
 int Replace(int &p) {
-  int t = v[p].v;
+  int t = p;
   if (!v[p].s[1]) {
     p = v[p].s[0];
   } else {
@@ -54,10 +65,15 @@ int Replace(int &p) {
 
 void Delete(int &p, int x) {
   if (v[p].v == x) {
-    if (!v[p].s[0] || !v[p].s[1]) {
-      p = v[p].s[0] + v[p].s[1];
+    if (!--v[p].d) {
+      if (!v[p].s[0] || !v[p].s[1]) {
+        p = v[p].s[0] + v[p].s[1];
+      } else {
+        int t = Replace(v[p].s[0]);
+        v[p].v = v[t].v, v[p].d = v[t].d;
+      }
     } else {
-      v[p].v = Replace(v[p].s[0]);
+      Rebalance(s, v[p].v);
     }
   } else {
     Delete(v[p].s[x > v[p].v], x);
@@ -66,18 +82,18 @@ void Delete(int &p, int x) {
 }
 
 int FindRank(int p, int x) {
-  if (!p) {
-    return 0;
+  if (v[p].v == x) {
+    return v[v[p].s[0]].c + 1;
   }
-  return FindRank(v[p].s[x > v[p].v], x) + (x > v[p].v) * (v[v[p].s[0]].c + 1);
+  return FindRank(v[p].s[x > v[p].v], x) + (x > v[p].v) * (v[v[p].s[0]].c + v[p].d);
 }
 
 int FindVal(int p, int x) {
-  if (v[v[p].s[0]].c + 1 == x) {
+  if (v[v[p].s[0]].c < x && v[v[p].s[0]].c + v[p].d >= x) {
     Rebalance(s, v[p].v);
     return v[p].v;
   }
-  return FindVal(v[p].s[v[v[p].s[0]].c < x], x - (v[v[p].s[0]].c < x) * (v[v[p].s[0]].c + 1));
+  return FindVal(v[p].s[v[v[p].s[0]].c + v[p].d < x], x - (v[v[p].s[0]].c + v[p].d < x) * (v[v[p].s[0]].c + 1));
 }
 
 int FindPast(int p, int x) {
@@ -95,6 +111,8 @@ int FindNext(int p, int x) {
 }
 
 int main() {
+  // freopen("P3369_6.in", "r", stdin);
+  // freopen("P3369_6.out", "w", stdout);
   cin >> n;
   for (int i = 1, o, x; i <= n; i++) {
     cin >> o >> x;
@@ -104,7 +122,7 @@ int main() {
       Delete(s, x);
     } else if (o == 3) {
       Rebalance(s, x);
-      cout << FindRank(s, x) + 1 << '\n';
+      cout << FindRank(s, x) << '\n';
     } else if (o == 4) {
       cout << FindVal(s, x) << '\n';
     } else if (o == 5) {
