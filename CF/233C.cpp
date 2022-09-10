055b256ae05b84ed7f636ad3247a54a75f883d78
dengyufan2008 <2695375004@qq.com>
2022-09-10 10:33:05 +0800

    .

--- CF/233C.cpp
+++ CF/233C.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
+#define LL long long
 
 using namespace std;
 
-int n, k, a[2001];
+const LL kMod = 1e9 + 7;
+LL n, k, a[2001], d[2001];
+
+LL Pow(LL x, LL y) {
+  LL ans = 1;
+  for (LL i = 1; i <= y; i <<= 1) {
+    if (i & y) {
+      ans = ans * x % kMod;
+    }
+    x = x * x % kMod;
+  }
+  return ans;
+}
+
+LL Feb(LL x, LL y) {
+  LL ans = 1;
+  for (LL i = 1; i < y; i++) {
+    ans = ans * (x + y - i - 1) % kMod * Pow(i, kMod - 2) % kMod;
+  }
+  return ans;
+}
 
 int main() {
   cin >> n >> k;
-  for (int i = 1; i <= n; i++) {
+  for (LL i = 1; i <= n; i++) {
     cin >> a[i];
+    d[i] = Feb(k, i);
   }
-  
+  for (LL i = 1; i <= n; i++) {
+    LL ans = 0;
+    for (LL j = i; j; j--) {
+      ans = (ans + d[j] * a[i - j + 1] % kMod) % kMod;
+    }
+    cout << ans << ' ';
+  }
   return 0;
 }
