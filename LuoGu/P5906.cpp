72fd2e72df8b4d063f2577b537c120d1f1fb4928
dengyufan2008 <2695375004@qq.com>
2022-10-04 22:56:44 +0800

    .

--- LuoGu/P5906.cpp
+++ LuoGu/P5906.cpp
@@ -5,53 +5,58 @@
 
 using namespace std;
 
-const int kInf = 1e9;
-struct E {
-  int mx = -kInf, mn = kInf, _mx = -kInf, _mn = kInf;
-} e[200001];
-struct V {
-  int l, r, d;
-} v[200001];
-int n, m, k, a[200001], b[200001], l[200001], ans[200001];
+const LL kMaxN = 2e5, kInf = 1e9;
+struct Q {
+  LL l, r, d;
+} q[kMaxN];
+LL n, m, p, a[kMaxN], b[kMaxN], l[kMaxN], mn[kMaxN], _mn[kMaxN], mx[kMaxN], ans[kMaxN];
+
+LL Read() {
+  LL x = 0;
+  char ch = getchar();
+  while (ch < '0' || ch > '9') {
+    ch = getchar();
+  }
+  while (ch >= '0' && ch <= '9') {
+    x = x * 10 + ch - '0';
+    ch = getchar();
+  }
+  return x;
+}
 
 int main() {
-  cin >> n;
-  for (int i = 1; i <= n; i++) {
-    cin >> b[i];
-    l[i] = i;
+  n = Read();
+  for (LL i = 0; i < n; i++) {
+    b[i] = Read(), l[i] = i;
   }
-  cin >> m;
-  for (int i = 1; i <= m; i++) {
-    cin >> v[i].l >> v[i].r;
-    v[i].d = i;
+  m = Read();
+  for (LL i = 0; i < m; i++) {
+    q[i].l = Read() - 1, q[i].r = Read() - 1, q[i].d = i;
   }
-  k = round(pow(n, 0.5));
-  sort(v + 1, v + m + 1, [](V i, V j) { return (i.l - 1) / k == (j.l - 1) / k ? i.r < j.r : i.l < j.l; });
-  sort(l + 1, l + n + 1, [](int i, int j) { return b[i] < b[j]; });
-  for (int i = 1; i <= n; i++) {
+  p = sqrt(n);
+  sort(q, q + m, [](Q i, Q j) { return i.l / p == j.l / p ? i.r < j.r : i.l < j.l; });
+  sort(l, l + n, [](LL i, LL j) { return b[i] < b[j]; });
+  for (LL i = 1; i < n; i++) {
     a[l[i]] = a[l[i - 1]] + (b[l[i]] != b[l[i - 1]]);
   }
-  for (int i = 1, x, _x, y, num = 0, _num; i <= m; i++) {
-    if (i == 1 || (v[i].l - 1) / k != (v[i - 1].l - 1) / k) {
-      y = min(((v[i].l - 1) / k + 1) * k - 1, v[i].r), x = _x = y + 1, num = 0;
-      fill(e + 1, e + 200001, e[0]);
+  for (LL i = 0, j, k; i < m; i++) {
+    if (!i || q[i].l / p != q[i - 1].l / p) {
+      k = q[i].l / p * p + p;
+      fill(mn, mn + kMaxN, kInf), fill(_mn, _mn + kMaxN, kInf), fill(mx, mx + kMaxN, -kInf);
     }
-    for (; y < v[i].r;) {
-      y++;
-      e[a[y]].mn = min(e[a[y]].mn, y);
-      e[a[y]].mx = max(e[a[y]].mx, y);
-      num = max(num, e[a[y]].mx - e[a[y]].mn);
+    for (; k <= q[i].r; k++) {
+      mx[a[k]] = k;
+      mn[a[k]] = min(mn[a[k]], k);
+      ans[q[i].d] = max(ans[q[i].d], mx[a[k]] - mn[a[k]]);
     }
-    _num = num;
-    for (; x > v[i].l;) {
-      x--;
-      e[a[x]]._mn = min(min(e[a[x]].mn, e[a[x]]._mn), x);
-      e[a[x]]._mx = max(max(e[a[x]].mx, e[a[x]]._mx), x);
-      num = max(num, e[a[x]]._mx - e[a[x]]._mn);
+    i < m - 1 && q[i + 1].l / p == q[i].l / p && (ans[q[i + 1].d] = ans[q[i].d]);
+    for (j = min(q[i].l / p * p + p - 1, q[i].r); j >= q[i].l; j--) {
+      _mn[a[j]] = j;
+      mx[a[j]] = max(mx[a[j]], j);
+      ans[q[i].d] = max(ans[q[i].d], mx[a[j]] - _mn[a[j]]);
     }
-    ans[v[i].d] = num, num = _num, x = _x;
   }
-  for (int i = 1; i <= m; i++) {
+  for (LL i = 0; i < m; i++) {
     cout << ans[i] << '\n';
   }
   return 0;
