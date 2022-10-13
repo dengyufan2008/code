db9c9cac1f5625da7dc452339467939685621491
dengyufan2008 <2695375004@qq.com>
2022-10-13 16:22:41 +0800

    .

--- AT/ARC70F.cpp
+++ AT/ARC70F.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int a, b, p;
+int a, b;
 bool ans[4001];
 stack<int> s;
 
@@ -21,18 +21,11 @@ int main() {
     cout << "Impossible";
     return 0;
   }
-  for (p = 0; p < a + b - 1 && !Q(p, p + 1); p += 2) {
-  }
-  if (p >= a + b - 1) {
-    s.push(p);
-  } else {
-    s.push(p), s.push(p + 1);
-    for (int i = p + 2; i < a + b; i++) {
-      if (Q(s.top(), i)) {
-        s.push(i);
-      } else {
-        s.pop();
-      }
+  for (int i = 0; i < a + b; i++) {
+    if (s.empty() || Q(s.top(), i)) {
+      s.push(i);
+    } else {
+      s.pop();
     }
   }
   for (int i = 0; i < a + b; i++) {
