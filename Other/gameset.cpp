3bd1c143dc3bee3ca8133c795de3071c4e2cfd57
dengyufan2008 <2695375004@qq.com>
2022-10-31 12:00:26 +0800

    .

--- Other/gameset.cpp
+++ Other/gameset.cpp
@@ -4,18 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#define PII pair<int, int>
 
 using namespace std;
 
 string path, opt;
 class FUNCTION {
  private:
+ public:
   char SmallWrite(char ch) {
     return ch >= 'A' && ch <= 'Z' ? ch + 32 : ch;
   }
 
- public:
-  void HideMouse() {  // Not Recently Used
+  void HideMouse() {
     CONSOLE_CURSOR_INFO cursor_info = {1, 0};
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
   }
@@ -24,7 +25,17 @@ class FUNCTION {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ((BackC & 0x0F) << 4) + (ForgC & 0x0F));
   }
 
-  pair<int, int> Read(int x) {
+  void Goto(int x, int y) {
+    printf("\033[%d;%dH", x, y);
+  }
+
+  void Pause() {
+    cout << "Press C To Continue.\n";
+    while (SmallWrite(getch()) != 'c') {
+    }
+  }
+
+  PII Read(int x) {
     int t = -1;
     for (; x < opt.size() && (opt[x] < '0' || opt[x] > '9'); x++) {
     }
@@ -51,10 +62,9 @@ class FUNCTION {
 class HELP {
  public:
   void Main() {
-    // if (func.Chick("snake", 5)) {
-    //   ;
-    // } else
-    if (func.Chick("minesweeper", 4)) {
+    if (func.Chick("snake", 5)) {
+      cout << "You Can Type \" SNAKE \" Simply.\n";
+    } else if (func.Chick("minesweeper", 4)) {
       cout << "MINESWEEPER line col mine\n\n  line  The Number Of Lines.\n  col   The Number Of Columns.\n  mine  The Number Of Mines.\n\nFor example: MINESWEEPER 10 10 5\n生成一个10*10的地图 其中有5个雷.\n";
     } else {
       if (opt.size() > 4) {
@@ -66,11 +76,185 @@ class HELP {
 } help;
 class SNAKE {
  private:
-  ;
+  const int kMove[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+  int n, m, s, d, t, k, b[101][101], _b[101][101];
+  bool speed;
+  char ch;
+  queue<PII> l;
+
+  void Push(int x, int y) {
+    b[x][y] = 4;
+    l.push({x, y});
+  }
+
+  void Pop() {
+    b[l.front().first][l.front().second] = 0;
+    l.pop();
+  }
+
+  void Print() {
+    func.SetColor(11, 0);
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        if (b[i][j] != _b[i][j] || b[i][j] == 4) {
+          func.Goto(i, j);
+          if (b[i][j] == 0) {
+            cout << ' ';
+          } else if (b[i][j] == 1) {
+            func.SetColor(12, 0);
+            cout << '#';
+          } else if (b[i][j] == 2 || b[i][j] == 3) {
+            func.SetColor(10, 0);
+            cout << '*';
+          } else if (i != l.back().first || j != l.back().second) {
+            func.SetColor(11, 0);
+            cout << 'O';
+          } else {
+            func.SetColor(11, 0);
+            cout << '@';
+          }
+        }
+      }
+      cout << '\n';
+    }
+    func.SetColor(11, 0);
+    copy(&b[0][0], &b[101][0], &_b[0][0]);
+  }
+
+  void D() {
+    while (kbhit()) {
+      ch = getch();
+      if (func.SmallWrite(ch) == 'w') {
+        if (d != 1) {
+          d = 3;
+        }
+      } else if (func.SmallWrite(ch) == 'a') {
+        if (d != 0) {
+          d = 2;
+        }
+      } else if (func.SmallWrite(ch) == 's') {
+        if (d != 3) {
+          d = 1;
+        }
+      } else if (func.SmallWrite(ch) == 'd') {
+        if (d != 2) {
+          d = 0;
+        }
+      } else if (func.SmallWrite(ch) == 'f') {
+        speed ^= 1;
+      }
+    }
+  }
+
+  void SpawnFood() {
+    int x = rand() % n + 1, y = rand() % m + 1;
+    while (b[x][y]) {
+      x = rand() % n + 1, y = rand() % m + 1;
+    }
+    b[x][y] = 2;
+  }
+
+  void SpawnSuperFood() {
+    int c = n * m, _b[101][101] = {};
+    int x = rand() % (n - 1) + 1, y = rand() % (m - 1) + 1;
+    while (c && (b[x][y] || b[x + 1][y] || b[x][y + 1] || b[x + 1][y + 1])) {
+      c -= !_b[x][y], _b[x][y] = 1;
+      x = rand() % (n - 1) + 1, y = rand() % (m - 1) + 1;
+    }
+    b[x][y] = b[x + 1][y] = b[x][y + 1] = b[x + 1][y + 1] = 3;
+  }
+
+  void EraseSuperFood() {
+    t = k + rand() % 25 + 1;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        if (b[i][j] == 3) {
+          b[i][j] = 0;
+        }
+      }
+    }
+  }
+
+  bool Move() {
+    int x = (l.back().first + kMove[d][0] + n - 1) % n + 1, y = (l.back().second + kMove[d][1] + m - 1) % m + 1;
+    if (b[x][y] == 1 || b[x][y] == 4) {
+      return 1;
+    } else if (b[x][y] == 2) {
+      s++;
+      Push(x, y);
+      SpawnFood();
+    } else {
+      if (b[x][y] == 3) {
+        s += 11 - k + t;
+        EraseSuperFood();
+      }
+      Push(x, y);
+      Pop();
+    }
+    return 0;
+  }
+
+  void Prepare() {
+    bool flag;
+    system("cls");
+    func.SetColor(11, 0);
+    cout << "Press WASD To Change Your Direction.\n按下 WASD 以改变移动方向.\nPress F To Change Your Speed.\n按下 F 以切换移动速度.\n";
+    func.Pause();
+    system("cls");
+    n = 20, m = 52, s = 0, d = 0, k = 0, speed = 1;
+    fill(&b[0][0], &b[101][0], 0), fill(&_b[0][0], &_b[101][0], -1);
+    for (int i = 1; i <= 2; i++) {
+      for (int j = 1; j <= 16; j++) {
+        b[i + 3][j + 6] = b[i + 3][j + 30] = b[i + 15][j + 6] = b[i + 15][j + 30] = 1;
+      }
+    }
+    for (int i = 1; i <= 3; i++) {
+      for (int j = 1; j <= 6; j++) {
+        b[i + 5][j + 6] = b[i + 5][j + 40] = b[i + 12][j + 6] = b[i + 12][j + 40] = 1;
+      }
+    }
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 8; j++) {
+        b[i + 8][j + 22] = 1;
+      }
+    }
+    while (!l.empty()) {
+      l.pop();
+    }
+    Push(2, 13), Push(2, 14), Push(2, 15), Push(2, 16);
+    SpawnFood(), t = k + rand() % 25 + 1;
+  }
+
+  void Playing() {
+    for (k = 0;; k++) {
+      Sleep(100 + speed * 100);
+      Print(), D();
+      if (Move()) {
+        while (kbhit()) {
+          getch();
+        }
+        return;
+      } else if (k == t) {
+        SpawnSuperFood();
+      } else if (k == t + 25) {
+        EraseSuperFood();
+      }
+    }
+  }
+
+  void Ending() {
+    system("cls");
+    cout << "Game Over!\nYour Score is " << s << ".\n";
+    func.Pause();
+    system("cls");
+    cout << "LunarPursuer Gameset [Version 10.0.22621.608]\n(c) Seek Lunar Corporation. All Rights Reserved.\nType \"help\" To Check Out Command List.\n输入 \"help\" 以查看命令列表.\n";
+  }
 
  public:
   void Main() {
-    ;
+    Prepare();
+    Playing();
+    Ending();
   }
 } snake;
 class MINESWEEPER {
@@ -149,8 +333,8 @@ class MINESWEEPER {
         }
       }
     }
-    cout << "Done!\nPress Any Key To Continue...\n";
-    getch();
+    cout << "Done!\n";
+    func.Pause();
     system("cls");
   }
 
@@ -220,8 +404,7 @@ class MINESWEEPER {
       system("cls");
       fill(&b[0][0], &b[100][100] + 1, 1);
       Print(1);
-      cout << "Press Any Key To Continue...\n";
-      getch();
+      func.Pause();
     }
     getchar();
     system("cls");
@@ -233,7 +416,7 @@ class MINESWEEPER {
 
  public:
   void Main() {
-    pair<int, int> p;
+    PII p;
     p = func.Read(p.second);
     if ((x = p.first) == -1) {
       cout << "Incorrect Command!\nType \"help minesweeper\" To Check Out Regular.\n输入 \"help minesweeper\" 以查看语法.\n";
@@ -256,9 +439,10 @@ class MINESWEEPER {
 } minesweeper;
 
 int main() {
+  func.HideMouse();
+  srand(time(0));
   system("title Gameset");
   system("cls");
-  srand(time(0));
   cin.tie(0), cout.tie(0);
   path = string(getcwd(NULL, 0));
   func.SetColor(11, 0);
@@ -278,11 +462,11 @@ int main() {
       } else if (func.Chick("minesweeper")) {
         minesweeper.Main();
       } else {
-        cout << "Incorrect Command!\nType \"help\" To Check Out Command List.\nType \"help\" To Check Out Command List.\n输入 \"help\" 以查看命令列表.\n";
+        cout << "Incorrect Command!\nType \"help\" To Check Out Command List.\n输入 \"help\" 以查看命令列表.\n";
       }
     }
   }
-  cout << "Press Any Key To Continue...\n";
-  getch();
+  func.Pause();
+  system("cls");
   return 0;
 }
