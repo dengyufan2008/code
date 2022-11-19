477a9aba08d13a438b0d302c57f736f19f79f05f
dengyufan2008 <2695375004@qq.com>
2022-11-19 23:44:01 +0800

    .

---
+++ LuoGu/P2495.cpp
@@ -0,0 +1,112 @@
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#define LL long long
+
+using namespace std;
+
+ifstream fin("CON");
+ofstream fout("CON");
+
+const LL kMaxN = 25e4 + 1, kInf = 1e18;
+struct E {
+  LL v;
+  vector<pair<LL, LL>> e;
+} e[kMaxN];
+struct V {
+  LL p, d, f[18], g[18];
+  vector<pair<LL, LL>> e;
+} v[kMaxN];
+LL n, m, k, t, h, a[kMaxN], q[kMaxN];
+bool b[kMaxN];
+
+void S(LL f, LL x) {
+  v[x].p = ++t, v[x].d = v[f].d + 1, v[x].f[0] = f;
+  for (auto i : v[x].e) {
+    if (i.first != f) {
+      v[i.first].g[0] = i.second;
+      S(x, i.first);
+    }
+  }
+}
+
+void T(LL x) {
+  for (auto i : e[x].e) {
+    T(i.first);
+    if (b[i.first]) {
+      e[x].v += i.second;
+    } else {
+      e[x].v += min(e[i.first].v, i.second);
+    }
+  }
+}
+
+pair<LL, LL> Lca(LL x, LL y) {
+  LL ans = kInf;
+  if (v[x].d < v[y].d) {
+    swap(x, y);
+  }
+  for (LL i = 17; i >= 0; i--) {
+    if (v[x].d - (1 << i) >= v[y].d) {
+      ans = min(ans, v[x].g[i]);
+      x = v[x].f[i];
+    }
+  }
+  for (LL i = 17; i >= 0; i--) {
+    if (v[x].f[i] != v[y].f[i]) {
+      ans = min(ans, min(v[x].g[i], v[y].g[i]));
+      x = v[x].f[i], y = v[y].f[i];
+    }
+  }
+  return {x == y ? x : v[x].f[0], x == y ? ans : min(ans, min(v[x].g[0], v[y].g[0]))};
+}
+
+void Build() {
+  q[h = 1] = 1, sort(a + 1, a + k + 1, [](LL i, LL j) { return v[i].p < v[j].p; });
+  for (LL i = 1; i <= k; i++) {
+    LL c = Lca(q[h], a[i]).first;
+    if (c != q[h]) {
+      while (v[q[h - 1]].p > v[c].p) {
+        e[q[h - 1]].e.push_back({q[h], Lca(q[h], q[h - 1]).second}), h--;
+      }
+      e[c].e.push_back({q[h], Lca(q[h], c).second}), h--;
+      if (v[q[h - 1]].p != v[c].p) {
+        q[++h] = c;
+      }
+    }
+    q[++h] = a[i];
+  }
+  for (LL i = 1; i < h; i++) {
+    e[q[i]].e.push_back({q[i + 1], Lca(q[i + 1], q[i]).second});
+  }
+}
+
+int main() {
+  fin >> n;
+  fill(&v[0].g[0], &v[0].g[18], kInf), fill(&v[1], &v[250001], v[0]);
+  for (LL i = 1, x, y, z; i < n; i++) {
+    fin >> x >> y >> z;
+    v[x].e.push_back({y, z}), v[y].e.push_back({x, z});
+  }
+  S(0, 1);
+  for (LL i = 1; i <= 17; i++) {
+    for (LL j = 1; j <= n; j++) {
+      v[j].f[i] = v[v[j].f[i - 1]].f[i - 1];
+      v[j].g[i] = min(v[j].g[i - 1], v[v[j].f[i - 1]].g[i - 1]);
+    }
+  }
+  fin >> m;
+  for (LL i = 1; i <= m; i++) {
+    fin >> k;
+    for (LL j = 1; j <= k; j++) {
+      fin >> a[j];
+      b[a[j]] = 1;
+    }
+    Build(), T(1);
+    fout << e[1].v << '\n';
+    for (LL j = 1; j <= k; j++) {
+      b[a[j]] = 0;
+    }
+  }
+  return 0;
+}
