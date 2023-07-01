11b51149bb483be5f20c27e7d0fd2c6f5bf50e91
dengyufan2008 <2695375004@qq.com>
2023-07-01 13:58:40 +0800

    .

--- CF/713C(nlogn).cpp
+++ CF/713C(nlogn).cpp
@@ -1,22 +1,24 @@
-#include <algorithm>
 #include <iostream>
+#include <queue>
 #define LL long long
 
 using namespace std;
 
-const LL kInf = 1e18;
-LL n, a[3001], b[3001] = {-kInf}, l[3001];
+LL n, ans;
+priority_queue<int> q;
 
 int main() {
   cin >> n;
-  for (LL i = 1; i <= n; i++) {
-    cin >> b[i];
-    b[i] -= i, l[i] = i;
+  for (LL i = 1, x; i <= n; i++) {
+    cin >> x;
+    x -= i;
+    q.push(x);             // 此时最右侧 k = 0, b = ans.
+    if (x < q.top()) {     // 影响到了最右侧 b, 需要更新 ans.
+      q.push(x);           // 此时最右侧 k = 1, 次右侧 k = 0.
+      ans += q.top() - x;  // 故最右侧 b := b + (当前位置到最右侧交点的距离).
+      q.pop();             // 最右侧 k = 1, 故应删除.
+    }
   }
-  sort(l + 1, l + n + 1, [](LL i, LL j) { return b[i] < b[j]; });
-  for (LL i = 1; i <= n; i++) {
-    a[l[i]] = a[l[i - 1]] + (b[l[i]] != b[l[i - 1]]);
-  }
-  
+  cout << ans;
   return 0;
 }
