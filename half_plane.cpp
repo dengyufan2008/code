0701406a56cea7f0fdb980865eaa698bfb7ef018
dengyufan2008 <2695375004@qq.com>
2023-07-26 09:55:42 +0800

    .

--- half_plane.cpp
+++ half_plane.cpp
@@ -10,14 +10,16 @@ const int kMaxN = 501;
 const double kEps = 1e-6, kInf = 1145141919.810;
 int n, m = 4, h, t;
 double ans;
-PDD a[kMaxN], q[kMaxN];
+PDD a[kMaxN];
 VEC v[kMaxN] = {{}, {{-kInf, -kInf}, {kInf, -kInf}}, {{kInf, -kInf}, {kInf, kInf}}, {{kInf, kInf}, {-kInf, kInf}}, {{-kInf, kInf}, {-kInf, -kInf}}};
+pair<PDD, VEC> q[kMaxN];
 
 PDD operator+(PDD x, PDD y) { return {x.first + y.first, x.second + y.second}; }
 PDD operator-(PDD x, PDD y) { return {x.first - y.first, x.second - y.second}; }
 double operator*(PDD x, PDD y) { return x.first * y.second - x.second * y.first; }
-double operator-(VEC x) { return abs((x.second - x.first).second) > kEps ? (x.second - x.first).second : (x.second - x.first).first; }  // +:一二象限 -:三四象限
+double operator*(VEC x, PDD y) { return (x.second - x.first) * (y - x.first); }
 double operator*(VEC x, VEC y) { return (x.second - x.first) * (y.second - y.first); }
+double operator-(VEC x) { return abs((x.second - x.first).second) > kEps ? (x.second - x.first).second : (x.second - x.first).first; }  // +:一二象限 -:三四象限
 PDD operator+(VEC x, VEC y) {
   double s1 = (x.second - x.first) * (y.second - x.first), s2 = s1 + (y.first - x.first) * (x.second - x.first);
   return {y.second.first + (y.first - y.second).first * s1 / s2, y.second.second + (y.first - y.second).second * s1 / s2};
@@ -43,41 +45,32 @@ int main() {
     }
     return (x.second - x.first) * (y.second - x.first) < -kEps;
   });
-  h = 1, t = 2, q[1] = {-kInf, v[1].first.second}, q[2] = v[1].second;
+  h = t = 1, q[1] = {{-kInf, v[1].first.second}, v[1]};
   for (int i = 2; i <= m; i++) {
     if (abs(v[i] * v[i - 1]) > kEps) {
-      for (; t - h > 1 && (q[t - 1] - v[i].first) * (v[i].second - v[i].first) > kEps; t--) {
+      for (; h < t && v[i] * q[t].first < -kEps; t--) {
       }
-      if (h > t) {
-        cout << "0.000";
-        return 0;
-      }
-      for (; t - h > 1 && (q[h + 1] - v[i].first) * (v[i].second - v[i].first) > -kEps; h++) {
-      }
-      q[t] = make_pair(q[t - 1], q[t]) + v[i];
-      if (abs((v[i].second - q[t]).first) < kEps && abs((v[i].second - q[t]).second) < kEps) {
-        q[++t] = v[i].first;
-      } else {
-        q[++t] = v[i].second;
+      t++, q[t] = {q[t - 1].second + v[i], v[i]};
+      for (; h + 1 < t && q[t].second * q[h + 1].first < kEps; h++) {
       }
     }
   }
-  for (; t - h > 1 && (q[t - 1] - q[h]) * (q[h + 1] - q[h]) > -kEps; t--) {
+  for (; h + 1 < t && q[h].second * q[t].first < kEps; t--) {
   }
-  if (t - h <= 1) {
+  if (h + 1 >= t) {
     cout << "0.000";
     return 0;
   }
-  h++, q[t] = make_pair(q[h - 1], q[h]) + make_pair(q[t - 1], q[t]);
+  q[h].first = q[h].second + q[t].second;
   for (int i = h; i <= t; i++) {
-    if (abs(abs(q[i].first) - kInf) < kEps || abs(abs(q[i].second) - kInf) < kEps) {
+    if (abs(abs(q[i].first.first) - kInf) < kEps || abs(abs(q[i].first.second) - kInf) < kEps) {
       cout << "Inf";
       return 0;
     }
   }
   for (int i = h; i < t; i++) {
-    ans += q[i] * q[i + 1];
+    ans += q[i].first * q[i + 1].first;
   }
-  cout << fixed << setprecision(3) << (ans + q[t] * q[h]) / 2;
+  cout << fixed << setprecision(3) << (ans + q[t].first * q[h].first) / 2;
   return 0;
 }
