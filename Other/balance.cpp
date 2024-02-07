a605a648eeaeefa0ddc9b69518f8f35e4b461b96
dengyufan2008 <2695375004@qq.com>
2024-02-07 14:58:13 +0800

    .

--- Other/balance.cpp
+++ Other/balance.cpp
@@ -1,27 +1,28 @@
 #include <iomanip>
 #include <iostream>
-#define CD const double
+#define CD constexpr double
 
 using namespace std;
 
 CD kBaseH = 15552, kBaseA = 715;
-CD kH2A = 0.0626;                               // 10级E
+CD kH2A = 0.0626;                   // 10级E
 CD kHPerC = 0.04955, kMPerC = 19.815, kCPPerC = 0.03305, kCDPerC = 0.06605;
-CD kMaxCC = 26.183835327556961445009856507587;  // 能百暴还能配平? 去死吧你
-CD kCC = 19.515152;                             // 0 为无限制
-CD kExtraH1 = 1.45;                             // 护摩1
-CD kExtraH2 = 4778.6;                           // 花
-CD kExtraH3 = 2091.3;                           // 小生命
-CD kExtraA1 = 1.4;                              // 宗室 + 千岩
-CD kExtraA2 = 0.041;                            // 大攻击
-CD kExtraA3 = 310.8;                            // 羽
-CD kExtraA4 = 13.6;                             // 小攻击
-CD kExtraM0 = 186.5;                            // 沙
-CD kExtraM1 = 306.5;                            // 沙 + 教官(按持续时间为 E 的一半计)
-CD kExtraM2 = 1.15;                             // 魔女
-CD kExtraCP = 0.361;                            // 基础 + 头
-CD kExtraCD = 1.546;                            // 基础 + 突破 + 护摩
-CD kExtraH2A = 0.018;                           // 护摩1
+CD kMaxCC = 26.183;                 // 能百暴还能配平? 去死吧你
+CD kCC = 19.515152;                 // 0 为无限制
+CD kExtraH0 = 1. + 0.2;            // 护摩1
+CD kExtraH1 = 0.25;                 // 双水
+CD kExtraH2 = 4778.6;               // 花
+CD kExtraH3 = 2091.3;               // 小生命
+CD kExtraA0 = 1. + 0.041;          // 大攻击
+CD kExtraA1 = 0.2 + 0.2;            // 宗室 + 千岩
+CD kExtraA2 = 310.8;                // 羽
+CD kExtraA3 = 13.6;                 // 小攻击
+CD kExtraM0 = 186.5;                // 沙
+CD kExtraM1 = 120;                  // 教官(按持续时间为 E 的 60% 计)
+CD kExtraM2 = 1. + 0.15;           // 魔女
+CD kExtraCP = 0.05 + 0.311;         // 基础 + 头
+CD kExtraCD = 0.5 + 0.384 + 0.662;  // 基础 + 突破 + 护摩
+CD kExtraH2A = 0.018;               // 半血护摩1
 CD kN = 29.7, kEps = 1e-2;
 double ansch, anscm, ans;
 
@@ -30,16 +31,16 @@ double Sqr(double x) { return x * x; }
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
-  for (double ch = 0.0; ch <= 30.0; ch += kEps) {
-    for (double cm = max(kN - ch - kMaxCC, 0.0); cm <= min(kN - ch, 30.0); cm += kEps) {
+  for (double ch = 0.; ch <= 30.; ch += kEps) {
+    for (double cm = max(kN - ch - kMaxCC, 0.); cm <= min(kN - ch, 30.); cm += kEps) {
       double cc = kN - ch - cm;
       if (abs(kCC) < 0.0001 || abs(cc - kCC) < 0.005) {
-        double h = kBaseH * (kExtraH1 + kHPerC * ch) + kExtraH2 + kExtraH3;
-        double a = kBaseA * (kExtraA1 + kExtraA2) + kExtraA3 + kExtraA4 + h * (kH2A + kExtraH2A);
-        double m0 = (kExtraM0 + kMPerC * cm) / (kExtraM0 + kMPerC * cm + 1400.0);
-        double m1 = (kExtraM1 + kMPerC * cm) / (kExtraM1 + kMPerC * cm + 1400.0);
-        double m = (m0 + m1) * 25.0 / 12.0 + kExtraM2 * 1.5;
-        double c = Sqr(kCPPerC * kCDPerC * cc + kCPPerC * kExtraCD + kCDPerC * kExtraCP) / (kCPPerC * kCDPerC * 4.0) + 1.0;
+        double h = kBaseH * (kExtraH0 + kExtraH1 + kHPerC * ch) + kExtraH2 + kExtraH3;
+        double a = kBaseA * (kExtraA0 + kExtraA1) + kExtraA2 + kExtraA3 + h * (kH2A + kExtraH2A);
+        double m0 = (kExtraM0 + kMPerC * cm) / (kExtraM0 + kMPerC * cm + 1400.);
+        double m1 = (kExtraM0 + kExtraM1 + kMPerC * cm) / (kExtraM0 + kExtraM1 + kMPerC * cm + 1400.);
+        double m = (m0 * 0.4 + m1 * 0.6) * 25. / 6. + kExtraM2 * 1.5;
+        double c = Sqr(kCPPerC * kCDPerC * cc + kCPPerC * kExtraCD + kCDPerC * kExtraCP) / (kCPPerC * kCDPerC * 4.) + 1.;
         double s = a * m * c;
         if (s > ans) {
           ans = s, ansch = ch, anscm = cm;
@@ -49,9 +50,17 @@ int main() {
   }
   double anscc = kN - ansch - anscm;
   double t = (kCPPerC * kCDPerC * anscc + kCPPerC * kExtraCD + kCDPerC * kExtraCP) * 0.5;
-  cout << fixed << setprecision(6) << "H:  " << kBaseH * (kExtraH1 + kHPerC * ansch) + kExtraH2 + kExtraH3 << '\n';
+  cout << "Self:\n";
+  cout << fixed << setprecision(6) << "H:  " << kBaseH * (kExtraH0 + kHPerC * ansch) + kExtraH2 + kExtraH3 << '\n';
+  cout << fixed << setprecision(6) << "A:  " << kBaseA * kExtraA0 + kExtraA2 + kExtraA3 << '\n';
   cout << fixed << setprecision(6) << "M:  " << kExtraM0 + kMPerC * anscm << '\n';
-  cout << fixed << setprecision(6) << "CP: " << t / kCDPerC * 100.0 << "%\n";
-  cout << fixed << setprecision(6) << "CD: " << t / kCPPerC * 100.0 << "%\n";
+  cout << fixed << setprecision(6) << "CP: " << t / kCDPerC * 100. << "%\n";
+  cout << fixed << setprecision(6) << "CD: " << t / kCPPerC * 100. << "%\n";
+  cout << "Team:\n";
+  cout << fixed << setprecision(6) << "H:  " << kBaseH * (kExtraH0 + kExtraH1 + kHPerC * ansch) + kExtraH2 + kExtraH3 << '\n';
+  cout << fixed << setprecision(6) << "A:  " << kBaseA * (kExtraA0 + kExtraA1) + kExtraA2 + kExtraA3 << '\n';
+  cout << fixed << setprecision(6) << "M:  " << kExtraM0 + kExtraM1 + kMPerC * anscm << '\n';
+  cout << fixed << setprecision(6) << "CP: " << t / kCDPerC * 100. << "%\n";
+  cout << fixed << setprecision(6) << "CD: " << t / kCPPerC * 100. << "%\n";
   return 0;
 }
---
+++ Other/damage.cpp
@@ -0,0 +1,37 @@
+#include <iomanip>
+#include <iostream>
+#define CD constexpr double
+
+using namespace std;
+
+CD kA2D = 2.426;
+CD kH2A = 0.0626 + 0.018;                                   // 10级E + 半血护摩1
+CD kExtraA = 0;                                             // 应该胡桃队里目前没有直接加倍率区数值的拐
+CD kExtraD = 1. + 0.33 + 0.466 + 0.15 + 0.075 + 0.3 + 0.2;  // 半血加成 + 火伤杯 + 魔女2 + 魔女4 1层 + 夜兰(平均) + 芙宁娜(平均)
+CD kExtraM = 1. + 0.15;                                     // 魔女4
+CD kLS = 90;                                                // 90级堂主
+CD kLE = 90;                                                // 90级怪, 默认没有无视防 & 加减防
+CD kD = 0.1;                                                // 小宝元素抗性为 10%
+CD kOffD = -0.2;                                            // 钟离盾
+CD kM1 = 120;                                               // 教官 (60%)
+CD kH = 34120.969760;
+CD kA = 1354.715000;
+CD kM0 = 267.345200;
+CD kCP = 88.986058;
+CD kCD = 177.837493;
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  double t = (kA + kH * kH2A) * kA2D * kExtraD * 1.5 * ((kM0 / (kM0 + 1400.) * 0.4 + (kM0 + kM1) / (kM0 + kM1 + 1400.) * 0.6) * 25. / 9. + kExtraM) * (kLS + 100.) / (kLS + kLE + 200.);
+  if (kD + kOffD < 0) {
+    t *= 1. - (kD + kOffD) * 0.5;
+  } else if (kD + kOffD < 0.75) {
+    t *= 1. - (kD + kOffD);
+  } else {
+    t /= 1. + (kD + kOffD) * 4.;
+  }
+  cout << "Expect:   " << fixed << setprecision(6) << t * (kCP * kCD * 0.0001 + 1.) << '\n';
+  cout << "Critical: " << fixed << setprecision(6) << t * (kCD * 0.01 + 1.) << '\n';
+  return 0;
+}
