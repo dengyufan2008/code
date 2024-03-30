fdc771a6f29379ed91f82b484ba688828e315760
dengyufan2008 <2695375004@qq.com>
2024-03-30 15:08:14 +0800

    .

--- Contests/DengYufan20240323/medrcy/medrcy.cpp
+++ Contests/DengYufan20240323/medrcy/medrcy.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -6,77 +7,154 @@ ifstream cin("medrcy.in");
 ofstream cout("medrcy.out");
 
 const int kMaxN = 1001;
-int t, n, m, k, mn, d[kMaxN], c[kMaxN];
-bool e[kMaxN][kMaxN], b[kMaxN], ans[kMaxN];
+int t, n, m, k;
+class G {
+  int s, w, c[kMaxN], b[kMaxN];
+  bool d[kMaxN], t[kMaxN], e[kMaxN][kMaxN], ans[kMaxN];
+  static vector<int> l;
+  static vector<vector<int>> r;
 
-void S(int x) {
-  int mx = 0;
-  for (int i = 1; i <= n; i++) {
-    c[mx] < c[i] && (mx = i);
-  }
-  if (c[mx] == 2) {
-    ;
-    return;
-  } else if (x > k) {
-    return;
-  }
-  mx = 0;
-  for (int i = 1; i <= n; i++) {
-    !b[i] && c[mx] < c[i] && (mx = i);
-  }
-  d[x] = mx;
-  for (int i = 1; i <= n; i++) {
-    if (e[mx][i]) {
-      c[mx]--, c[i]--;
+  int T(int x) {
+    if (b[x] == 1) {
+      return 0;
     }
-  }
-  S(x + 1), b[mx] = 1;
-  for (int i = 1; i <= n; i++) {
-    if (e[mx][i]) {
-      c[mx]++, c[i]++;
-    }
-  }
-  for (int i = 1; i <= n; i++) {
-    if (e[mx][i] && !b[i] && x <= k) {
-      d[x++] = i;
-      for (int j = 1; j <= n; j++) {
-        if (e[i][j]) {
-          c[i]--, c[j]--;
-        }
+    int o = b[x] = 1;
+    for (int i = 1; i <= n; i++) {
+      if (e[x][i] && c[i]) {
+        o += T(i);
       }
-    } else if (e[mx][i]) {
+    }
+    return o;
+  }
+
+  void S(int x, int o) {
+    if (b[x] == 2) {
       return;
     }
-  }
-  S(x);
-}
-
-int main() {
-  cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(0);
-  cin >> t;
-  while (t--) {
+    t[x] |= o;
     for (int i = 1; i <= n; i++) {
-      c[i] = ans[i] = 0;
+      if (e[x][i] && c[i]) {
+        S(i, o ^ 1);
+      }
+    }
+  }
+
+ public:
+  void Clear() {
+    w = kMaxN, l.clear();
+    for (int i = 1; i <= n; i++) {
+      c[i] = d[i] = ans[i] = 0;
       for (int j = 1; j <= n; j++) {
         e[i][j] = 0;
       }
     }
-    cin >> n >> m >> k;
-    for (int i = 1, x, y; i <= m; i++) {
-      cin >> x >> y;
-      c[x]++, c[y]++;
+  }
+
+  void AddEdge(int x, int y) {
+    if (!e[x][y]) {
       e[x][y] = e[y][x] = 1;
+      c[x]++, c[y]++;
     }
-    mn = k + 1, S(1);
-    if (mn > k) {
+  }
+
+  bool Check() {
+    for (int i = 1; i <= n; i++) {
+      if (c[i] > 2) {
+        return 0;
+      }
+    }
+    return 1;
+  }
+
+  void Update() {
+    int o = s;
+    for (int i = 1; i <= n; i++) {
+      b[i] = t[i] = 0;
+    }
+    for (int i = 1; i <= n; i++) {
+      if (!b[i] && c[i] == 1) {
+        int x = T(i);
+        o += x >> 1;
+        S(i, (x & 1 ^ 1) << 1);
+      }
+    }
+    for (int i = 1; i <= n; i++) {
+      if (!b[i] && c[i] == 2) {
+        o += T(i) + 1 >> 1;
+        S(i, 2);
+      }
+    }
+    if (o > w) {
+      return;
+    } else if (o < w) {
+      w = o;
+      for (int i = 1; i <= n; i++) {
+        ans[i] = 0;
+      }
+    }
+    for (int i = 1; i <= n; i++) {
+      ans[i] |= t[i];
+    }
+  }
+
+  int Size() { return s; }
+
+  int FindMax() {
+    int x = 0;
+    for (int i = 1; i <= n; i++) {
+      if (c[i] > c[x]) {
+        x = i;
+      }
+    }
+    return x;
+  }
+
+  void Delete(int x, vector<int> &v = l) {
+    s++, d[x] = 1, v.push_back(x);
+    for (int i = 1; i <= n; i++) {
+      if (e[x][i] && !d[i]) {
+        c[x]--, c[i]--;
+      }
+    }
+  }
+
+  void Undo(vector<int> &v = l) {
+    int x = v.back();
+    s--, d[x] = 0, v.pop_back();
+    for (int i = 1; i <= n; i++) {
+      if (e[x][i] && !d[i]) {
+        c[x]++, c[i]++;
+      }
+    }
+  }
+
+  void Delete2(int x) {
+    static vector<int> v;
+    v.clear();
+    for (int i = 1; i <= n; i++) {
+      if (e[x][i] && !d[i]) {
+        Delete(i, v);
+      }
+    }
+    r.push_back(v);
+  }
+
+  void Undo2() {
+    static vector<int> v;
+    v.swap(r.back()), r.pop_back();
+    for (; !v.empty(); Undo(v)) {
+    }
+  }
+
+  void Print() {
+    if (w > k) {
       cout << -1 << '\n';
     } else {
-      int w = 0;
+      int o = 0;
       for (int i = 1; i <= n; i++) {
-        w += ans[i];
+        o += ans[i];
       }
-      cout << mn << ' ' << w << '\n';
+      cout << w << ' ' << o << '\n';
       for (int i = 1; i <= n; i++) {
         if (ans[i]) {
           cout << i << ' ';
@@ -84,6 +162,33 @@ int main() {
       }
       cout << '\n';
     }
+  }
+} g;
+
+void S() {
+  if (g.Check()) {
+    g.Update();
+    return;
+  } else if (g.Size() == k) {
+    return;
+  }
+  int x = g.FindMax();
+  g.Delete(x), S(), g.Undo();
+  g.Delete2(x), S(), g.Undo2();
+}
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin >> t;
+  while (t--) {
+    g.Clear();
+    cin >> n >> m >> k;
+    for (int i = 1, x, y; i <= m; i++) {
+      cin >> x >> y;
+      g.AddEdge(x, y);
+    }
+    S(), g.Print();
   }
   return 0;
 }
