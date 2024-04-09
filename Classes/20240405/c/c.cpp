1751afa3e9d5d769747b2fec206b2c23373f87f5
dengyufan2008 <2695375004@qq.com>
2024-04-09 20:38:08 +0800

    。

--- Classes/20240405/c/c.cpp
+++ Classes/20240405/c/c.cpp
@@ -6,25 +6,49 @@ ifstream cin("c.in");
 ofstream cout("c.out");
 
 const int kMaxN = 3e5 + 1;
-struct V {
-  int w, t;
-} v[kMaxN * 48];
-int n, m, k, p[kMaxN], s[kMaxN];
+int n, m, k, p[kMaxN], s[kMaxN], v[kMaxN * 48];
 
 void Init() {
-  int a[kMaxN] = {}, d[kMaxN] = {};
+  int a[kMaxN], d[kMaxN];
   cin >> n >> m;
   for (int i = 1; i <= n; i++) {
     cin >> a[i];
   }
+  fill(&d[1], &d[n] + 1, n + 1);
   for (int i = n; i >= 1; i--) {
     p[i] = d[a[i]], d[a[i]] = i;
+  }
+}
+
+void Copy(int &p, int q1, int q2, int l, int r, int x) {
+  ;
+}
+
+void Add(int &p, int l, int r, int _l, int _r, int x) {
+  ;
+}
+
+int Ask(int p, int l, int r, int x) {
+  ;
+}
+
+void Calc() {
+  for (int i = n; i >= 1; i--) {
+    Copy(s[i], s[i + 1], s[p[i] + 1], 1, n, p[i] - 1);
+    Add(s[i], 1, n, i, p[i] - 1, 1);
+  }
+}
+
+void Ans() {
+  for (int i = 1, x, y; i <= m; i++) {
+    cin >> x >> y;
+    cout << Ask(s[x], 1, n, y) << '\n';
   }
 }
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
-  Init();
+  Init(), Calc(), Ans();
   return 0;
 }
--- Contests/DengYufan20240409/c/c.cpp
+++ Contests/DengYufan20240409/c/c.cpp
@@ -1,7 +1,4 @@
-#include <algorithm>
-#include <chrono>
 #include <fstream>
-#include <random>
 #include <vector>
 
 using namespace std;
@@ -11,16 +8,13 @@ ofstream cout("c.out");
 
 const int kMaxN = 3e5 + 1;
 struct V {
-  int d, s, r;
-  bool o;
+  int d;
+  bool o, s[2], f[2];
   vector<int> e;
 } v[kMaxN];
-int t, n, k, l[kMaxN];
-bool sub3;
-vector<int> shit;
-mt19937 Rand(chrono::steady_clock().now().time_since_epoch().count());
+int t, n;
 
-bool Sub1() {
+bool C1() {
   for (int i = 1; i <= n; i++) {
     if (v[i].e.size() == 1 && v[i].o) {
       return 1;
@@ -29,72 +23,97 @@ bool Sub1() {
   return 0;
 }
 
-bool Sub2() {
-  return n & 1 ^ 1;
+bool C2() {
+  for (int i = 1; i <= n; i++) {
+    if (v[i].o) {
+      return n & 1 ^ 1;
+    }
+  }
+  return 0;
 }
 
-bool Sub3() {
-  return sub3;
-}
-
-void T(int f, int x) {
-  v[x].d = v[f].d + 1, v[x].s = 1, v[x].r = ++k;
+void S(int f, int x) {
+  v[x].d = v[f].d + 1;
+  v[x].s[v[x].d & 1] |= v[x].o;
   for (int i : v[x].e) {
     if (i != f) {
-      T(x, i);
-      v[x].s += v[i].s;
+      S(x, i);
+      v[x].s[0] |= v[i].s[0];
+      v[x].s[1] |= v[i].s[1];
     }
   }
 }
+
+bool C3() { return S(0, 1), v[1].s[0] && v[1].s[1]; }
+
+void T(int f, int x) {
+  bool b[2] = {};
+  for (int o = 0; o < v[x].e.size(); o++) {
+    int i = v[x].e[o];
+    if (i != f) {
+      v[i].f[0] |= b[0];
+      v[i].f[1] |= b[1];
+      b[0] |= v[i].s[0];
+      b[1] |= v[i].s[1];
+    }
+  }
+  b[0] = b[1] = 0;
+  for (int o = v[x].e.size() - 1; o >= 0; o--) {
+    int i = v[x].e[o];
+    if (i != f) {
+      v[i].f[0] |= b[0];
+      v[i].f[1] |= b[1];
+      b[0] |= v[i].s[0];
+      b[1] |= v[i].s[1];
+    }
+  }
+  for (int i : v[x].e) {
+    if (i != f) {
+      v[i].f[0] |= v[x].f[0];
+      v[i].f[1] |= v[x].f[1];
+      v[i].f[v[x].d & 1] |= v[x].o;
+      T(x, i);
+    }
+  }
+}
+
+bool R(int f, int x) {
+  for (int i : v[x].e) {
+    if (i != f && R(x, i)) {
+      return 1;
+    }
+  }
+  int s = v[x].f[v[x].d & 1];
+  for (int i : v[x].e) {
+    if (i != f) {
+      s += v[i].s[v[x].d & 1];
+    }
+  }
+  return s >= 3;
+}
+
+bool C4() { return T(0, 1), R(0, 1); }
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
   cin >> t;
   while (t--) {
-    sub3 = 1;
     cin >> n;
     for (int i = 1; i <= n; i++) {
       cin >> v[i].o;
       v[i].e.clear();
+      v[i].s[0] = v[i].s[1] = 0;
+      v[i].f[0] = v[i].f[1] = 0;
     }
     for (int i = 1, x, y; i < n; i++) {
       cin >> x >> y;
-      sub3 &= x == i && y == i + 1;
       v[x].e.push_back(y), v[y].e.push_back(x);
     }
-    if (Sub1()) {
+    if (C1() || C2() || C3() || C4()) {
       cout << "Alice\n";
-    } else if (Sub2()) {
-      bool o = 0;
-      for (int i = 1; i <= n; i++) {
-        o |= v[i].o;
-      }
-      cout << (o ? "Alice\n" : "Bob\n");
-    } else if (Sub3()) {
-      bool o = v[1].o || v[n].o;
-      for (int i = 2; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-          if (v[i].o && v[j].o && (i + j & 1)) {
-            o = 1;
-          }
-        }
-      }
-      cout << (o ? "Alice\n" : "Bob\n");
     } else {
-      k = 0;
-      T(0, 1);
-      shit.clear();
-      for (int i = 1; i <= n; i++) {
-        if (v[i].o) {
-          shit.push_back(i);
-        }
-      }
-      bool fuck[2] = {};
-      for (int i : shit) {
-        fuck[v[i].d & 1] = 1;
-      }
-      cout << (fuck[0] && fuck[1] ? "Alice\n" : "Bob\n");
+      cout << "Bob\n";
     }
   }
   return 0;
