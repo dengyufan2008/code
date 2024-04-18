481ae13199f373ac9a9bf05415b39ea508e3be0a
dengyufan2008 <2695375004@qq.com>
2024-04-18 21:00:09 +0800

    .

--- Classes/20240405/f/f.cpp
+++ Classes/20240405/f/f.cpp
@@ -7,12 +7,12 @@ using namespace std;
 ifstream cin("f.in");
 ofstream cout("f.out");
 
-const int kMaxN = 1e5 + 1;
+const int kMaxN = 1e5 + 1, kInf = 1e9;
 int n, m;
 struct Q {
-  int d, w;
+  int w;
   bool o;
-};
+} q[kMaxN];
 class Ans {
   LL v[kMaxN << 2];
 
@@ -40,56 +40,141 @@ class Seg {
   };
   struct V {
     Lst lst;
-    int chk[2];
+    int chk[2] = {kInf, -kInf};
     LL tag1, tag2;
     bool o;
   } v[kMaxN << 2];
+  int p[kMaxN];
 
- public:
+  void Init(int p, int l, int r) {
+    if (l == r) {
+      v[p].lst.p = l, v[p].lst.o = q[l].o;
+      v[p].chk[q[l].o] = q[l].w;
+      return;
+    }
+    int mid = l + r >> 1;
+    Init(p << 1, l, mid), Init(p << 1 | 1, mid + 1, r);
+  }
+
   void Tag1(int p, LL w) { v[p].tag1 += w; }
 
- private:
+  void Pushdown(int p) {  // CHICK
+    ;
+  }
+
+  void Pushup(int p) {  // CHICK
+    ;
+  }
+
+  bool Check(int p, int d) {
+    ;
+  }
+
+  int FindP(int p, int l, int r, int d) {  // CHICK
+    if (!Check(p, d)) {
+      return 0;
+    } else if (l == r) {
+      return l;
+    }
+    int mid = l + r >> 1;
+    if (mid > d) {
+      int t = FindP(p << 1, l, mid, d);
+      return t ? t : FindP(p << 1 | 1, mid + 1, r, d);
+    } else if (mid < d + 1) {
+      return FindP(p << 1 | 1, mid + 1, r, d);
+    }
+  }
+
+  int CalcLst(int p, int l, int r, int d) {  // CHICK
+    ;
+  }
+
+  void ChangeLst(int p, int l, int r, int d, int lst, bool o) {  // CHICK
+    ;
+  }
+
+  void Add(int p, int l, int r, int d, int lst) {
+    if (l == r) {
+      v[p].lst.w = lst, v[p].o = 1;
+      return;
+    }
+    Pushdown(p);
+    int mid = l + r >> 1;
+    if (mid >= d) {
+      Add(p << 1, l, mid, d, lst);
+    } else {
+      Add(p << 1 | 1, mid + 1, r, d, lst);
+    }
+    Pushup(p);
+  }
+
+  void Remove(int p, int l, int r, int d) {
+    if (l == r) {
+      v[p].o = 0;
+      return;
+    }
+    Pushdown(p);
+    int mid = l + r >> 1;
+    if (mid >= d) {
+      Remove(p << 1, l, mid, d);
+    } else {
+      Remove(p << 1 | 1, mid + 1, r, d);
+    }
+    Pushup(p);
+  }
+
+  void Recover(int p, int l, int r) {  // CHICK
+    ;
+  }
+
  public:
-  void Change(Q q, bool o) {
+  void Init() { Init(1, 1, n); }
+  void Tag() { Tag1(1, 1); }
+
+  void Add(int d) {
+    Add(1, 1, n, d, CalcLst(1, 1, n, d));
+    (p[d] = FindP(1, 1, n, d)) ? ChangeLst(1, 1, n, p[d], d, 0) : void();
   }
-  void Recover() {
+
+  void Remove(int d) {
+    Remove(1, 1, n, d);
+    ChangeLst(1, 1, n, p[d], CalcLst(1, 1, n, p[d]), 1);
   }
+
+  void Recover() { Recover(1, 1, n); }
 } seg;
 class Op {
-  vector<Q> v[kMaxN << 2];
+  vector<int> v[kMaxN << 2];
 
-  void Add(int p, int l, int r, int _l, int _r, Q q) {
+  void Add(int p, int l, int r, int _l, int _r, int d) {
     if (l >= _l && r <= _r) {
-      return v[p].push_back(q);
+      return v[p].push_back(d);
     }
     int mid = l + r >> 1;
     if (mid >= _l) {
-      Add(p << 1, l, mid, _l, _r, q);
+      Add(p << 1, l, mid, _l, _r, d);
     }
     if (mid < _r) {
-      Add(p << 1 | 1, mid + 1, r, _l, _r, q);
+      Add(p << 1 | 1, mid + 1, r, _l, _r, d);
     }
   }
 
   void Calc(int p, int l, int r) {
-    for (Q i : v[p]) {
-      seg.Change(i, 1);
+    for (int i : v[p]) {
+      seg.Add(i);
     }
     if (l == r) {
-      return seg.Tag1(1, 1);
+      return seg.Tag();
     }
     int mid = l + r >> 1;
     Calc(p << 1, l, mid), Calc(p << 1 | 1, mid + 1, r);
-    for (Q i : v[p]) {
-      seg.Change(i, 0);
+    for (int i = v[p].size() - 1; i >= 0; i--) {
+      seg.Remove(v[p][i]);
     }
   }
 
  public:
-  void Add(int l, int r, int d, int w, bool o) {
-    Add(1, 1, n, l, r, {d, w, o});
-  }
-
+  void Add(int l, int r, int d) { Add(1, 1, n, l, r, d); }
   void Calc() { Calc(1, 1, n), seg.Recover(); }
 } op;
 
@@ -99,8 +184,8 @@ int main() {
   cin >> n >> m;
   for (int i = 1, o, x, y, z; i <= m; i++) {
     cin >> o >> x >> y >> z;
-    op.Add(x, y, i, z, o == 1);
+    q[i] = {z, o == 1}, op.Add(x, y, i);
   }
-  op.Calc(), ans.Print();
+  seg.Init(), op.Calc(), ans.Print();
   return 0;
 }
