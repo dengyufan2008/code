be9dbbb007f0a1e03fd00a2aeff1fcd1f418b8a7
dengyufan2008 <2695375004@qq.com>
2024-04-26 19:46:08 +0800

    .

--- Classes/20240405/o/o.cpp
+++ Classes/20240405/o/o.cpp
@@ -8,9 +8,10 @@ using namespace std;
 ifstream cin("o.in");
 ofstream cout("o.out");
 
-const int kMaxN = 2e5 + 1, kMaxM = 1e6 + 1;
+const LL kMaxN = 2e5 + 1, kMaxM = 1e6 + 1, kMaxL = 18, kInf = 1e18;
 struct V {
-  int s;
+  int s, d, f[kMaxL];
+  LL w;
   bool b;
   vector<pair<int, int>> e;
 } v[kMaxN];
@@ -21,6 +22,26 @@ int n, m, s, r;
 LL ans[kMaxM];
 vector<pair<int, LL>> l, t;
 vector<pair<int, int>> w;
+class Seg {
+  LL v[kMaxN];
+
+ public:
+  Seg() { fill(&v[0], &v[kMaxN], kInf); };
+
+  void CheckMin(int x, LL y) {
+    for (int i = x; i <= n; i += i & -i) {
+      v[i] = min(v[i], y);
+    }
+  }
+
+  LL Ask(int x) {
+    LL ans = kInf;
+    for (int i = x; i >= 1; i -= i & -i) {
+      ans = min(ans, v[i]);
+    }
+    return ans;
+  }
+} seg;
 
 void CalcR(int f, int x) {
   int mx = 0;
@@ -74,6 +95,40 @@ void Divide() {
   }
 }
 
+void CalcF(int f, int x) {
+  v[x].d = v[f].d + 1, v[x].f[0] = f;
+  for (int i = 1; i < kMaxL; i++) {
+    v[x].f[i] = v[v[x].f[i - 1]].f[i - 1];
+  }
+  for (auto i : v[x].e) {
+    if (i.first != f) {
+      v[i.first].w = v[x].w + i.second;
+      CalcF(x, i.first);
+    }
+  }
+}
+
+int Lca(int x, int y) {
+  if (v[x].d < v[y].d) {
+    swap(x, y);
+  }
+  for (int i = kMaxL - 1; i >= 0; i--) {
+    if (v[x].d - (1 << i) >= v[y].d) {
+      x = v[x].f[i];
+    }
+  }
+  for (int i = kMaxL - 1; i >= 0; i--) {
+    if (v[x].f[i] != v[y].f[i]) {
+      x = v[x].f[i], y = v[y].f[i];
+    }
+  }
+  return x == y ? x : v[x].f[0];
+}
+
+LL Dis(int x, int y) {
+  return v[x].w + v[y].w - v[Lca(x, y)].w * 2;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
@@ -92,12 +147,13 @@ int main() {
   sort(q + 1, q + m + 1, [](Q i, Q j) {
     return i.l < j.l || i.l == j.l && i.r < j.r;
   });
+  CalcF(0, 1);
   for (int i = n, j = w.size() - 1, k = m; i >= 1; i--) {
     for (; j >= 0 && w[j].first == i; j--) {
       seg.CheckMin(w[j].second, Dis(w[j].first, w[j].second));
     }
     for (; k >= 1 && q[k].l == i; k--) {
-      ans[q[k].d] = seg.Ask(1, q[k].r);
+      ans[q[k].d] = seg.Ask(q[k].r);
     }
   }
   for (int i = 1; i <= m; i++) {
