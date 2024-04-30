84d76daf53570d776a5d3f69e5116c87893af78f
dengyufan2008 <2695375004@qq.com>
2024-04-30 21:48:15 +0800

    .

--- Classes/20240411/d/d.cpp
+++ Classes/20240411/d/d.cpp
@@ -9,7 +9,7 @@ ofstream cout("d.out");
 
 const int kMaxN = 5001, kMod = 998244353;
 int n, m, a[kMaxN];
-LL c[kMaxN][kMaxN], s1[kMaxN][kMaxN], s2[kMaxN][kMaxN];
+LL fact[kMaxN], _fact[kMaxN], s1[kMaxN][kMaxN], s2[kMaxN][kMaxN];
 LL w, f[kMaxN][kMaxN], g[kMaxN], h[kMaxN];
 
 LL Pow(LL x, int y = kMod - 2) {
@@ -23,6 +23,10 @@ LL Pow(LL x, int y = kMod - 2) {
   return ans;
 }
 
+LL C(int x, int y) {
+  return fact[x] * _fact[y] % kMod * _fact[x - y] % kMod;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
@@ -30,18 +34,23 @@ int main() {
   for (int i = 1; i <= n; i++) {
     cin >> a[i], m += a[i];
   }
-  sort(a + 1, a + n + 1);
-  c[0][0] = s1[0][0] = s2[0][0] = f[0][0] = 1;
+  sort(a + 1, a + n + 1), fact[0] = 1;
+  for (int i = 1; i <= n; i++) {
+    fact[i] = fact[i - 1] * i % kMod;
+  }
+  _fact[n] = Pow(fact[n]);
+  for (int i = n; i >= 1; i--) {
+    _fact[i - 1] = _fact[i] * i % kMod;
+  }
+  s1[0][0] = s2[0][0] = f[0][0] = 1;
   for (int i = 1; i <= n; i++) {
     for (int j = 0; j < i; j++) {
-      c[i][j] = c[i - 1][j];
       s1[i][j] = s1[i - 1][j] * (i - 1);
       s2[i][j] = s2[i - 1][j] * j;
       f[i][j] = f[i - 1][j] * (m + i - j);
     }
     s1[i][0] = 0;
     for (int j = 1; j <= i; j++) {
-      c[i][j] = (c[i][j] + c[i - 1][j - 1]) % kMod;
       s1[i][j] = (s1[i][j] + s1[i - 1][j - 1]) % kMod;
       s2[i][j] = (s2[i][j] + s2[i - 1][j - 1]) % kMod;
       f[i][j] = (f[i][j] + f[i - 1][j - 1] * (a[i] + 1)) % kMod;
@@ -54,7 +63,7 @@ int main() {
   }
   for (int i = n; i >= 1; i--) {  // 恰好 i 个环
     for (int j = i + 1; j <= n; j++) {
-      g[i] = (g[i] - g[j] * c[j][i] % kMod + kMod) % kMod;
+      g[i] = (g[i] - g[j] * C(j, i) % kMod + kMod) % kMod;
     }
   }
   for (int i = 1; i <= n; i++) {  // 恰好 i 个集合
@@ -68,16 +77,15 @@ int main() {
   }
   for (int i = n; i >= 1; i--) {  // 恰好 i 个有序的内部无 0 的集合
     for (int j = i + 1; j <= n; j++) {
-      h[i] = (h[i] - h[j] * i % kMod + kMod) % kMod;
+      h[i] = (h[i] - h[j] * C(j - 1, i - 1) % kMod + kMod) % kMod;
     }
   }
   w = 1;
   for (int i = 1, j = 1; i <= n; i = j) {
     for (; j <= n && a[i] == a[j]; j++) {
     }
-    w = w * (j - i) % kMod;
+    w = w * _fact[j - i] % kMod;
   }
-  w = Pow(w);
   for (int i = 1; i <= n; i++) {  // 相同 a_i 不区分
     h[i] = h[i] * w % kMod;
   }
