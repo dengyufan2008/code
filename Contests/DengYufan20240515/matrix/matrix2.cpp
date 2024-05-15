ec29c2ad43721baa9d7a1b62ec01737c8dbd769a
dengyufan2008 <2695375004@qq.com>
2024-05-15 21:01:11 +0800

    。

--- Contests/DengYufan20240515/matrix/matrix2.cpp
+++ Contests/DengYufan20240515/matrix/matrix2.cpp
@@ -71,44 +71,86 @@ int main() {
     for (int i = 1; i <= t; i++) {
       v[i].et = 0;
     }
-    s = n * m * 3 + 1, t = s + 1, k = 1;
+    ans = 0, s = n * m * 3 + 1, t = s + 1, k = 1;
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= m; j++) {
-        AddEdge(s, R(i, j, 2), c[i][j] != 'B' ? kEdge : kReject);
-        AddEdge(R(i, j, 2), t, c[i][j] != 'W' ? kEdge : kReject);
+        if (c[i][j] == 'W') {
+          ans += kEdge;
+          AddEdge(R(i, j, 2), t, kReject);
+        } else if (c[i][j] == 'B') {
+          ans += kEdge;
+          AddEdge(s, R(i, j, 2), kReject);
+        } else {
+          AddEdge(R(i, j, 2), t, c[i][j] != 'W' ? kEdge : kReject);
+          AddEdge(s, R(i, j, 2), c[i][j] != 'B' ? kEdge : kReject);
+        }
       }
     }
     for (int i = 1; i < n; i++) {
       for (int j = 1; j < m; j++) {
-        AddEdge(R(i, j, 0), R(i, j, 1), 1);
+        bool w = 1, b = 1;
         for (int u = 0; u < 4; u++) {
           int _i = i + kMove[u][0], _j = j + kMove[u][1];
-          AddEdge(R(_i, _j, 2), R(i, j, 0), 1);
-          AddEdge(R(i, j, 1), R(_i, _j, 2), 1);
+          if (c[_i][_j] == 'B') {
+            w = 0;
+          } else if (c[_i][_j] == 'W') {
+            b = 0;
+          }
+        }
+        if (w || b) {
+          AddEdge(R(i, j, 0), R(i, j, 1), 1);
+          for (int u = 0; u < 4; u++) {
+            int _i = i + kMove[u][0], _j = j + kMove[u][1];
+            AddEdge(R(_i, _j, 2), R(i, j, 0), 1);
+            AddEdge(R(i, j, 1), R(_i, _j, 2), 1);
+          }
+        } else {
+          ans++;
         }
       }
     }
-    for (ans = 0; Bfs(); ans += Dfs(s, kInf)) {
+    for (; Bfs(); ans += Dfs(s, kInf)) {
     }
     cout << (n - 1) * (m - 1) + n * m * kEdge - ans << '\n';
     for (int i = 1; i <= t; i++) {
       v[i].d = i == s;
     }
     for (q.push(s); !q.empty(); q.pop()) {
-      if (q.front() > n * m * 2 && q.front() < s) {
-        int x = q.front() - n * m * 2;
-        c[(x - 1) / m + 1][(x - 1) % m + 1] = 'B';
-      }
       for (int i = v[q.front()].et; i; i = e[i].p) {
         if (e[i].w && !v[e[i].d].d) {
-          v[e[i].d].d = v[q.front()].d + 1;
+          v[e[i].d].d = 1;
           q.push(e[i].d);
+        }
+      }
+    }
+    for (int i = 2; i < k; i += 2) {
+      if (v[e[i].d].d ^ v[e[i ^ 1].d].d ^ 1) {
+        if (e[i ^ 1].d == s) {
+          int x = (e[i].d - n * m * 2 - 1) / m + 1;
+          int y = (e[i].d - n * m * 2 - 1) % m + 1;
+          for (int u = 0; u < 4; u++) {
+            int _x = x + kMove[u][0], _y = y + kMove[u][1];
+            if (_x <= n && _y <= m) {
+              c[_x][_y] = 'B';
+            }
+          }
+        } else if (e[i].d == t) {
+          int x = (e[i ^ 1].d - n * m * 2 - 1) / m + 1;
+          int y = (e[i ^ 1].d - n * m * 2 - 1) % m + 1;
+          for (int u = 0; u < 4; u++) {
+            int _x = x + kMove[u][0], _y = y + kMove[u][1];
+            if (_x <= n && _y <= m) {
+              c[_x][_y] = 'W';
+            }
+          }
         }
       }
     }
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= m; j++) {
-        cout << (c[i][j] == '?' ? 'W' : c[i][j]);
+        c[i][j] == '?' && (c[i][j] = 'W');
+        i + j & 1 && (c[i][j] ^= 21);
+        cout << c[i][j];
       }
       cout << '\n';
     }
