576b6db16985fafd5ca7c98503ab8f1bf89caa78
dengyufan2008 <2695375004@qq.com>
2024-05-20 22:02:08 +0800

    .

--- Classes/20240411/h/h.cpp
+++ Classes/20240411/h/h.cpp
@@ -8,44 +8,20 @@ using namespace std;
 
 ifstream cin("h.in");
 ofstream cout("h.out");
-
-const int kMaxN = 3e5 + 1, kMaxL = 19, kBase = 15553;
-struct A {
-  string s;
-  int w, d;
-};
-int n, m, p[kMaxN], a[kMaxN], r[kMaxN];
-int l[kMaxL][kMaxN], lg[kMaxN];
-string s[kMaxN], t = "~";
+// const int kMaxN = 3e5 + 2, kMaxL = 19, kBase = 15553;
+const int kMaxN = 65 + 2, kMaxL = 7, kBase = 15553;
+int n, o[kMaxN];
+string s[kMaxN];
 unordered_map<ULL, int> q;
-vector<A> v;
+vector<pair<string, int>> v;
 
-void Init() {
-  for (int i = 2; i < kMaxN; i++) {
-    lg[i] = lg[i >> 1] + 1;
-  }
-  cin >> n;
-  for (int i = 1; i <= n; i++) {
-    cin >> s[i], m += s[i].size();
-  }
-  for (int i = n; i >= 1; i--) {
-    static ULL h;
-    h = s[i][0];
-    for (int j = 1; j < s[i].size(); j++) {
-      if (q.count(h)) {
-        v.push_back({s[i].substr(j), q[h], 0});
-        v.back().d = v.size();
-        p[v.back().d] = t.size();
-        t += v.back().s;
-      }
-      h = h * kBase + s[i][j];
-    }
-    !q.count(h) && (q[h] = 0), q[h]++;
-  }
-}
+namespace SA {
+int m, p[kMaxN], a[kMaxN], r[kMaxN];
+int l[kMaxL][kMaxN], lg[kMaxN];
+string t = "~";
 
 void Sort() {
-  static int b[kMaxN] = {}, c[kMaxN] = {};
+  static int b[kMaxN], c[kMaxN];
   for (int i = 1; i <= m; i++) {
     a[i] = i;
   }
@@ -73,7 +49,7 @@ void Sort() {
       c[i] += c[i - 1];
     }
     for (int i = m; i >= 1; i--) {
-      a[c[r[b[i]]]--] = i;
+      a[c[r[b[i]]]--] = b[i];
     }
     for (int i = 1; i <= m; i++) {
       b[a[i]] = b[a[i - 1]];
@@ -91,6 +67,8 @@ void CalcL() {
       l[0][r[i]] = max(l[0][r[i - 1]] - 1, 0);
       for (int &j = l[0][r[i]]; t[i + j] == t[a[r[i] + 1] + j]; j++) {
       }
+    } else {
+      l[0][r[i]] = 0;
     }
   }
   for (int i = 1; i < kMaxL; i++) {
@@ -103,24 +81,147 @@ void CalcL() {
   }
 }
 
-int Lcp(int x, int y) {
-  x = r[x], y = r[y];
+int Lcp0(int x, int xl, int y, int yl) {
+  x = r[p[x] + xl], y = r[p[y] + yl];
   x > y ? swap(x, y) : void();
-  int o = lg[y - x];
-  return min(l[o][x], l[o][y - (1 << o)]);
+  int u = lg[y - x];
+  return min(l[u][x], l[u][y - (1 << u)]);
 }
 
-int Lcp(int x, int xl, int y, int yl) {
-  return Lcp(p[x] + xl, p[y] + yl);
+bool Cmp0(int x, int xl, int xr, int y, int yl, int yr) {  // [l, r)
+  int lcp = Lcp0(x, xl, y, yl);
+  if (lcp >= min(xr - xl, yr - yl)) {
+    return xr - xl < yr - yl;
+  }
+  return t[p[x] + xl + lcp] < t[p[y] + yl + lcp];
 }
 
-bool Less(int x, int y) {
-  if ()
+int Lcp1(int x, int y) {
+  static int lcp;
+  int lx = p[x + 1] - p[x];
+  int ly = p[y + 1] - p[y];
+  if (lx > ly) {
+    swap(x, y), swap(lx, ly);
+  }
+  if ((lcp = Lcp0(x, 0, y, 0)) < lx) {
+    return lcp;
+  } else if ((lcp = Lcp0(y, 0, y, lx)) < ly - lx) {
+    return lx + lcp;
+  } else if ((lcp = Lcp0(y, ly - lx, x, 0)) < lx) {
+    return ly + lcp;
+  }
+  return kMaxN;
+}
+
+bool Cmp1(int x, int y) {
+  int lcp = Lcp1(x, y), w = 0;
+  int lx = p[x + 1] - p[x];
+  int ly = p[y + 1] - p[y];
+  if (lx > ly) {
+    w = 1, swap(x, y), swap(lx, ly);
+  }
+  if (lcp < lx) {
+    return (t[p[x] + lcp] < t[p[y] + lcp]) ^ w;
+  } else if (lcp < ly) {
+    return (t[p[y] + lcp - lx] < t[p[y] + lcp]) ^ w;
+  } else if (lcp < lx + ly) {
+    return (t[p[y] + lcp - lx] < t[p[x] + lcp - ly]) ^ w;
+  }
+  return 0;
+}
+}  // namespace SA
+using namespace SA;
+
+int W(int x) {
+  static int d[kMaxN] = {};
+  int ans = 0;
+  for (int i = x + 1; i <= n; i += i & -i) {
+    ans += d[i];
+  }
+  for (int i = x; i >= 1; i -= i & -i) {
+    d[i]++;
+  }
+  return ans;
+}
+
+int Calc1() {
+  int w = 0;
+  for (int i = 2; i < kMaxN; i++) {
+    lg[i] = lg[i >> 1] + 1;
+  }
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i], m += s[i].size();
+    p[i] = t.size(), t += s[i], o[i] = i;
+  }
+  p[n + 1] = m + 1, Sort(), CalcL();
+  sort(o + 1, o + n + 1, [](int i, int j) {
+    return Cmp0(i, 0, s[i].size(), j, 0, s[j].size());
+  });
+  a[o[1]] = 1;
+  for (int i = 2; i <= n; i++) {
+    int x = o[i - 1], y = o[i];
+    a[y] = a[x] + Cmp0(x, 0, s[x].size(), y, 0, s[y].size());
+  }
+  for (int i = 1; i <= n; i++) {
+    w += W(a[i]);
+  }
+  return w;
+}
+
+int Z(string s) {
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] != 'z') {
+      return i;
+    }
+  }
+  return s.size();
+}
+
+int Calc2() {
+  int w = 0;
+  t.resize(1), m = 0;
+  for (int i = n; i >= 1; i--) {
+    static ULL h;
+    h = s[i][0];
+    for (int j = 1; j < s[i].size(); j++) {
+      if (q.count(h)) {
+        v.push_back({s[i].substr(j), q[h]});
+      }
+      h = h * kBase + s[i][j];
+    }
+    !q.count(h) && (q[h] = 0), q[h]++;
+  }
+  for (int i = 0; i < v.size(); i++) {
+    p[i] = t.size(), t += v[i].first, m += v[i].first.size(), o[i] = i;
+  }
+  p[v.size()] = m + 1, Sort(), CalcL();
+  sort(o, o + v.size(), Cmp1);
+  for (int i = 0, j = 0; i < v.size(); i++) {
+    j += v[o[i]].second;
+    int lcp = i < v.size() ? Lcp1(o[i], o[i + 1]) : Z(v.back().first);
+    w = min(w, lcp + 1 - j);
+  }
+  return w;
+  // for (int i = 0; o[i] != 5; i++) {
+  //   cout << v[o[i]].second << ' ';
+  // }
+  // cout << v[5].second << '\n';
+  // int _w = -217;
+  // for (int i = 1; i <= n; i++) {
+  //   int __w = 0;
+  //   for (int j = i + 1; j <= n; j++) {
+  //     __w += s[i] + 'r' > s[j] + 'r';
+  //   }
+  //   _w += __w;
+  //   cout << __w << " \n"[i == n];
+  // }
+  // return _w;
 }
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
-  Init(), Sort(), CalcL();
+  cout << Calc1() + Calc2() << '\n';
   return 0;
 }
