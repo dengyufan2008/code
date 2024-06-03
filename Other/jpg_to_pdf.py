55c3a050d3f787fdc2b5fedc6249b91863013865
dengyufan2008 <2695375004@qq.com>
2024-06-03 22:01:56 +0800

    .

--- Other/jpg_to_pdf.py
+++ Other/jpg_to_pdf.py
@@ -9,15 +9,15 @@ output_pdf = "D:\\Files\\Desktop\\new_10\\result.pdf"
 
 # 确保所有图片都存在，可读
 for image in images:
-    if not os.path.isfile(image):
-        print("File not exist: ", image)
-        images.remove(image)
+  if not os.path.isfile(image):
+    print("File not exist: ", image)
+    images.remove(image)
 
 # 使用 img2pdf 将图片列表合并为 pdf
 pdf_bytes = img2pdf.convert([open(img_path, 'rb').read() for img_path in images])
 
 # 将生成的 pdf 写入文件中
 with open(output_pdf, 'wb') as f:
-    f.write(pdf_bytes)
+  f.write(pdf_bytes)
 
 print("Images successfully converted to PDF:", output_pdf)
