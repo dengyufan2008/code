d58ddd6fead3361134abcc3779be24bcfeb761aa
dengyufan2008 <2695375004@qq.com>
2024-06-19 20:50:11 +0800

    .

--- Classes/20240521-0621/t/t.cpp
+++ Classes/20240521-0621/t/t.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <fstream>
+#include <unordered_map>
 #define LL long long
 
 using namespace std;
@@ -6,79 +8,144 @@ using namespace std;
 ifstream cin("t.in");
 ofstream cout("t.out");
 
-const int kMaxN = 2e5 + 1, kMod = 998244353;
-int n, k, a[kMaxN];
+const LL kMaxN = 2e5 + 1, kMaxL = 18, kInf = 1e18;
+struct P {
+  int x, y, o;
+} p[kMaxN];
+struct A {
+  int l, r, o;
+  void operator+=(A a) {
+    int w = (r < a.l) - (l > a.r);
+    if ((o == w || l == r) && (a.o == w || a.l == a.r)) {
+      o = w;
+    } else {
+      o = 0;
+    }
+    l = min(l, a.l), r = max(r, a.r);
+  }
+  bool operator&(A a) {
+    return l == a.r + 1 || a.l == r + 1;
+  }
+} a[kMaxL][kMaxN];
+int n;
+unordered_map<LL, LL> s;
 
 void Init() {
-  int s = 0;
-  char c;
-  cin >> n >> k;
+  int h[kMaxN] = {};
+  cin >> n;
   for (int i = 1; i <= n; i++) {
-    cin >> c;
-    a[i] = c == 'L' ? -1 : 1;
-    s += a[i];
+    cin >> p[i].x >> p[i].y;
+    p[i].o = h[i] = i;
   }
-  if (s < 0) {
-    for (int i = 1; i <= n; i++) {
-      a[i] = -a[i];
-    }
-    for (int i = 1, j = n; i < j; i++, j--) {
-      swap(a[i], a[j]);
-    }
-  }
+  sort(p + 1, p + n + 1, [](P i, P j) {
+    return i.x < j.x;
+  });
+  sort(h + 1, h + n + 1, [](int i, int j) {
+    return p[i].y < p[j].y;
+  });
   for (int i = 1; i <= n; i++) {
-    a[i] += a[i - 1];
+    a[0][h[i]] = {i, i};
+  }
+  for (int i = 1; i < kMaxL; i++) {
+    for (int j = 1; j <= n; j++) {
+      a[i][j] = a[i - 1][j];
+      if (j + (1 << i - 1) <= n) {
+        a[i][j] += a[i - 1][j + (1 << i - 1)];
+      }
+    }
   }
 }
 
-LL CalcL() {
-  int mx = 0, l = 0;
-  for (int i = 1; i <= n; i++) {
-    mx = max(mx, a[i]), l += a[i] < a[i - 1];
-  }
-  if (mx == 0) {
-    return 0;
-  }
-  LL ans = (k - 1LL) * l;
-  for (int i = 1, s = 0; i <= n; i++) {
-    if (s || a[i] > 0) {
-      s = 1, ans += a[i] < a[i - 1];
+int Dis(int x, int y) {
+  return abs(p[x].x - p[y].x) + abs(p[x].y - p[y].y);
+}
+
+LL R(int l, int r, bool o) {
+  return 2LL * n * (l - 1) + 2LL * (r - 1) + o;
+}
+
+A Ask(int l, int r) {
+  A t = a[0][l];
+  for (int i = kMaxL - 1; i >= 0; i--) {
+    if (l + (1 << i) <= r) {
+      t += a[i][l + 1], l += 1 << i;
     }
   }
-  return ans;
+  return t;
 }
 
-LL CalcR() {
-  int c[kMaxN << 1] = {};
-  LL ans = 0;
-  for (int i = 1; i <= n; i++) {
-    c[a[i] + kMaxN] += a[i] > a[i - 1];
+int FindL(int x, A t) {
+  int l = 1, r = x - 1;
+  while (l <= r) {
+    int mid = l + r >> 1;
+    A _t = Ask(mid, x - 1);
+    if (_t.r - _t.l == x - mid - 1) {
+      if ((_t.o == 1 || _t.l == _t.r) && t.l - 1 == _t.r) {
+        r = mid - 1;
+      } else if ((_t.o == -1 || _t.l == _t.r) && t.r + 1 == _t.l) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    } else {
+      l = mid + 1;
+    }
   }
-  for (int i = 1; i < kMaxN << 1; i++) {
-    c[i] += c[i - 1];
-  }
-  for (int i = 1; i <= k; i++) {
-    int s = min(1LL * i * a[n], 1LL * n);
-    ans += c[n + kMaxN] - c[s + kMaxN];
-  }
-  return ans << 1;
+  return l;
 }
 
-LL Calc() {
-  int ans = 0;
-  for (int i = 1; i <= n; i++) {
-    ans += a[i] >= (a[i] > a[i - 1]);
+int FindR(int x, A t) {
+  int l = x + 1, r = n;
+  while (l <= r) {
+    int mid = l + r >> 1;
+    A _t = Ask(x + 1, mid);
+    if (_t.r - _t.l == mid - x - 1) {
+      if ((_t.o == 1 || _t.l == _t.r) && t.r + 1 == _t.l) {
+        l = mid + 1;
+      } else if ((_t.o == -1 || _t.l == _t.r) && t.l - 1 == _t.r) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    } else {
+      r = mid - 1;
+    }
   }
-  return 1LL * ans * k;
+  return r;
+}
+
+LL Calc(int l, int r, bool o) {
+  if (s.count(R(l, r, o))) {
+    return s[R(l, r, o)];
+  }
+  A t = Ask(l, r);
+  int _l = FindL(l, t), _r = FindR(r, t);
+  LL ans = kInf;
+  if (_l < l) {
+    ans = min(ans, Calc(_l, r, 0) + Dis(_l, o ? r : l));
+  }
+  if (_r > r) {
+    ans = min(ans, Calc(l, _r, 1) + Dis(_r, o ? r : l));
+  }
+  if (_l == l && _r == r) {
+    ans = l - r;
+  }
+  return s[R(l, r, o)] = ans;
+}
+
+void Ans() {
+  LL ans[kMaxN] = {};
+  for (int i = 1; i <= n; i++) {
+    ans[p[i].o] = Calc(i, i, 0);
+  }
+  for (int i = 1; i <= n; i++) {
+    cout << ans[i] << '\n';
+  }
 }
 
 int main() {
   cin.tie(0), cout.tie(0);
-  Init();
-  if (a[n]) {
-    cout << (CalcL() + CalcR()) % kMod << '\n';
-  } else {
-    cout << Calc() % kMod << '\n';
-  }
+  ios::sync_with_stdio(0);
+  Init(), Ans();
   return 0;
 }
