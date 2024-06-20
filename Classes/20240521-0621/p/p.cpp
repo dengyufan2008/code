578d058d08775027bcd391db04b4919ba038e2eb
dengyufan2008 <2695375004@qq.com>
2024-06-20 20:30:22 +0800

    .

---
+++ Classes/20240521-0621/p/p.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("p.in");
+ofstream cout("p.out");
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  
+  return 0;
+}
