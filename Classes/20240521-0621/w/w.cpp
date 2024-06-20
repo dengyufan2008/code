b31064251e846bdaab2e66b103965d9a4f622d62
dengyufan2008 <2695375004@qq.com>
2024-06-20 14:46:40 +0800

    .

---
+++ Classes/20240521-0621/w/w.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("w.in");
+ofstream cout("w.out");
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  
+  return 0;
+}
