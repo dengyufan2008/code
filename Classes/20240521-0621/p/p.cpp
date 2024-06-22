eb17d5970c48f5872b5cd3afb1b4c6f325a828aa
dengyufan2008 <2695375004@qq.com>
2024-06-22 12:00:36 +0800

    .

--- Classes/20240521-0621/p/p.cpp
+++ Classes/20240521-0621/p/p.cpp
@@ -9,12 +9,9 @@ using namespace std;
 ifstream cin("p.in");
 ofstream cout("p.out");
 
-const int kMaxN = 1e6 + 1, kMaxL = 20;
-struct H {
-  int pw;
-  LL fact, _fact;
-} h[kMaxN];
+const int kMaxN = 1e6 + 1;
 int t, p, n, a[4], d[4][4];
+LL fact[kMaxN], _fact[kMaxN];
 
 void Add(LL &x, LL y) { x = (x + y) % p; }
 
@@ -41,14 +38,10 @@ class P {
   }
 
   P operator<<(LL x) {
-    if ((x & 3) == 0) {
-      return P(r, i);
-    } else if ((x & 3) == 1) {
-      return P(-i, r);
-    } else if ((x & 3) == 2) {
-      return P(-r, -i);
+    if (x & 1) {
+      return x & 2 ? P(i, -r) : P(-i, r);
     } else {
-      return P(i, -r);
+      return x & 2 ? P(-r, -i) : P(r, i);
     }
   }
 
@@ -72,55 +65,21 @@ class P {
   }
 } ans;
 
-void CalcFact() {
-  int _r;
-  LL pw[kMaxL], s[kMaxN], t[kMaxN];
-  pw[0] = 1;
-  for (int &i = _r = 1; i < kMaxL; i++) {
-    pw[i] = pw[i - 1] * p;
-    if (pw[i] >= kMaxN) {
-      break;
-    }
-  }
-  h[0] = {0, 1, 1};
-  for (int i = 1; i < kMaxN; i++) {
-    int l = 0, r = _r;
-    while (l <= r) {
-      int mid = l + r >> 1;
-      if (mid <= _r && !(i % pw[mid])) {
-        l = mid + 1;
-      } else {
-        r = mid - 1;
-      }
-    }
-    h[i] = h[i - 1], h[i].pw += r;
-    h[i].fact = h[i].fact * (i / pw[r]) % p;
-  }
-  s[0] = 1;
-  for (int i = 1; i < kMaxN; i++) {
-    s[i] = s[i - 1] * h[i].fact % p;
-  }
-  t[kMaxN - 1] = Pow(s[kMaxN - 1], p - 2);
-  for (int i = kMaxN - 1; i > 0; i--) {
-    t[i - 1] = t[i] * h[i].fact % p;
-  }
-  for (int i = 1; i < kMaxN; i++) {
-    h[i]._fact = s[i - 1] * t[i] % p;
-  }
-}
-
-void CalcAns(int o) {
+void CalcAns(bool o) {
   LL b[4] = {};
-  P w(o, 0);
+  P w(1, 0);
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
       b[j] += d[i][j];
     }
   }
+  if (!o && b[0] <= 0 && b[1] <= 1 && b[2] <= 0 && b[3] <= 1) {
+    return;
+  }
   for (int i = 0; i < 4; i++) {
-    w.r = w.r * h[a[i]].fact % p;
+    w.r = w.r * fact[a[i]] % p;
     for (int j = 0; j < 4; j++) {
-      w.r = w.r * h[d[i][j]]._fact % p;
+      w.r = w.r * _fact[d[i][j]] % p;
     }
   }
   int q = (d[1][3] + d[3][1]) + 3 * (d[1][1] + d[3][3]) & 3;
@@ -145,7 +104,7 @@ void CalcAns(int o) {
   ans = ans + w;
 }
 
-void S(int x, int b0, int b1, int b2, int b3, int o) {
+void S(int x, int b0, int b1, int b2, int b3, bool o) {
   if (x == 4) {
     CalcAns(o);
     return;
@@ -154,9 +113,7 @@ void S(int x, int b0, int b1, int b2, int b3, int o) {
     for (int &j = d[x][1] = 0; j <= b1 && i + j <= a[x]; j++) {
       for (int &k = d[x][2] = 0; k <= b2 && i + j + k <= a[x]; k++) {
         int &l = d[x][3] = a[x] - i - j - k;
-        if (l <= b3 && h[i].pw + h[j].pw + h[k].pw + h[l].pw == h[a[x]].pw) {
-          S(x + 1, b0 - i, b1 - j, b2 - k, b3 - l, o);
-        }
+        l <= b3 ? S(x + 1, b0 - i, b1 - j, b2 - k, b3 - l, o) : void();
       }
     }
   }
@@ -166,13 +123,17 @@ int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
   cin >> t >> p;
-  CalcFact();
+  fact[0] = 1;
+  for (int i = 1; i < kMaxN; i++) {
+    fact[i] = fact[i - 1] * i % p;
+  }
+  _fact[kMaxN - 1] = Pow(fact[kMaxN - 1], p - 2);
+  for (int i = kMaxN - 1; i >= 1; i--) {
+    _fact[i - 1] = _fact[i] * i % p;
+  }
   while (t--) {
     cin >> n >> a[0] >> a[1] >> a[2] >> a[3];
-    ans = P();
-    S(0, 0, 1, n, 1, 1);
-    S(0, n, 1, 0, 1, 1);
-    S(0, 0, 1, 0, 1, -1);
+    ans = P(), S(0, 0, 1, n, 1, 1), S(0, n, 1, 0, 1, 0);
     int q = p - 1 - 1LL * n * (n + 3) / 2 % (p - 1);
     cout << (ans.r + p) * Pow(2, q) % p << '\n';
   }
--- Classes/20240521-0621/y/y.cpp
+++ Classes/20240521-0621/y/y.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 
 using namespace std;
@@ -5,9 +6,23 @@ using namespace std;
 ifstream cin("y.in");
 ofstream cout("y.out");
 
+const int kMaxN = 2.5e5 + 1;
+int u, n, s, t, a[kMaxN], f[2][kMaxN][2];
+char c;
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
-  
+  cin >> u;
+  while (u--) {
+    cin >> n >> s >> t;
+    for (int i = 1; i <= n; i++) {
+      cin >> c, a[i] = (c == '0') + (c == '1') * 2 - 1;
+    }
+    for (int o : {0, 1}) {
+      ;
+      reverse(a + 1, a + n + 1);
+    }
+  }
   return 0;
 }
