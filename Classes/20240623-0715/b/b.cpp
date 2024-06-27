b347456bf00ad070b3d57a3902f0619e91dbf879
dengyufan2008 <2695375004@qq.com>
2024-06-27 17:41:38 +0800

    .

---
+++ Classes/20240623-0715/b/b.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("b.in");
+ofstream cout("b.out");
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  
+  return 0;
+}
