b2b27f2921f05e1b1c30434ab39e97e21fca2f37
dengyufan2008 <2695375004@qq.com>
2024-07-13 09:58:21 +0800

    .

---
+++ Classes/20240623-20240714 WHK2/g/g.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("g.in");
+ofstream cout("g.out");
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  
+  return 0;
+}
