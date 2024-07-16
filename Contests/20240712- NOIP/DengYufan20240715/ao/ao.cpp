82af618701b128756109a9548f93adfefefc3655
dengyufan2008 <2695375004@qq.com>
2024-07-16 14:51:47 +0800

    .

--- Contests/20240712- NOIP/DengYufan20240715/ao/ao.cpp
+++ Contests/20240712- NOIP/DengYufan20240715/ao/ao.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-#include <unordered_map>
+#include <vector>
 #define LL long long
 
 using namespace std;
@@ -7,18 +7,45 @@ using namespace std;
 ifstream cin("ao.in");
 ofstream cout("ao.out");
 
-const int kMaxN = 2001;
+const LL kMaxN = 2001, kMaxM = 1 << 13, kInf = 1e18;
 int o, n, c, p, nt[kMaxN][2];
 LL m, ans, s[kMaxN], t[kMaxN][2];
-unordered_map<LL, LL> f[kMaxN];
 
-void Update(int x, LL y, LL z) {
-  if (f[x].count(y)) {
-    f[x][y] = min(f[x][y], z);
-  } else {
-    f[x][y] = z;
+class Umap {
+  short Hash(unsigned LL x) {
+    return (x >> 2 ^ x << 4 ^ x >> 8 ^ x << 16 ^ x >> 32) & kMaxM - 1;
   }
-}
+
+ public:
+  pair<LL, LL> v[kMaxM];
+  vector<short> l;
+
+  Umap() {
+    l.clear();
+    for (short i = 0; i < kMaxM; i++) {
+      v[i] = {-1, kInf};
+    }
+  }
+
+  void Clear() {
+    l.clear();
+    for (short i = 0; i < kMaxM; i++) {
+      v[i] = {-1, kInf};
+    }
+  }
+
+  LL &operator[](LL x) {
+    short y = Hash(x);
+    for (; v[y].first != x && v[y].first != -1; y = y + 1 & kMaxM - 1) {
+    }
+    if (v[y].first == -1) {
+      v[y].first = x, l.push_back(y);
+    }
+    return v[y].second;
+  }
+} f[kMaxN];
+
+void Update(LL &x, LL y) { x = min(x, y); }
 
 int main() {
   cin.tie(0), cout.tie(0);
@@ -27,7 +54,7 @@ int main() {
   while (o--) {
     cin >> n >> c >> p >> m, p += c;
     for (int i = 1; i <= n; i++) {
-      cin >> s[i], s[i] += s[i - 1], f[i].clear();
+      cin >> s[i], s[i] += s[i - 1], f[i].Clear();
     }
     for (int i = 0; i <= n; i++) {
       nt[i][0] = nt[i][1] = i;
@@ -50,20 +77,22 @@ int main() {
         t[i][1] += (s[nt[j][1]] - s[j] + c) % p - c;
       }
     }
-    Update(0, 0, 0);
+    Update(f[0][0], 0);
     for (int i = 0; i < n; i++) {
-      for (auto [o, w] : f[i]) {
+      for (int l : f[i].l) {
+        LL o = f[i].v[l].first, w = f[i].v[l].second;
         int j = nt[i][o % p > 0];
         if (j <= n) {
           LL r = o + s[j] - s[i];
-          Update(j, r - r % p + c, w + r % p - c);
-          Update(j, r - r % p + p, w);
+          Update(f[j][r - r % p + c], w + r % p - c);
+          Update(f[j][r - r % p + p], w);
         }
       }
     }
     ans = 1e18;
     for (int i = 0; i < n; i++) {
-      for (auto [o, w] : f[i]) {
+      for (int l : f[i].l) {
+        LL o = f[i].v[l].first, w = f[i].v[l].second;
         LL v = t[i][o % p > 0];
         if (w + v <= m) {
           LL r = o + s[n] - s[i] - m + w;
