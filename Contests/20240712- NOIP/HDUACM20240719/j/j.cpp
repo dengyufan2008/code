cb6b3e877c2a8cfb8279ae82cf2a375774132047
dengyufan2008 <2695375004@qq.com>
2024-07-19 21:52:43 +0800

    .

--- Contests/20240712- NOIP/HDUACM20240719/j/j.cpp
+++ Contests/20240712- NOIP/HDUACM20240719/j/j.cpp
@@ -1,114 +1,84 @@
 #include <fstream>
-#include <map>
-#include <vector>
+#include <queue>
 #define LL long long
+
+using namespace std;
 
 ifstream cin("j.in");
 ofstream cout("j.out");
 
-using namespace std;
+const int kMaxN = 2e5 + 1, kMaxL = 18, kMaxB = 601;
+int o, n, m, t, ans, a[kMaxN], p[kMaxL][kMaxN], lg[kMaxN];
+LL w, h;
+pair<LL, int> d[kMaxN];
+struct V {
+  int l, r, p;
+  LL Calc() {
+    return 1LL * (p - l + 1) * (r - p + 1);
+  }
+  bool operator<(const V &v) const {
+    return a[p] < a[v.p] || a[p] == a[v.p] && p > v.p;
+  }
+} v;
+priority_queue<V> q;
 
-const int kMaxN = 2e5 + 2, kMaxM = 601;
-int u, n, m, o, a[kMaxN], r[kMaxN], p[kMaxN], q[kMaxN];
-int pt[kMaxN], qt[kMaxN], ans[kMaxN];
-LL hs, s[kMaxN], t[kMaxN], mx[kMaxN], w[kMaxN];
-pair<int, int> f[kMaxN];
-map<int, int> h;
-vector<int> v[kMaxN];
+int Max(int l, int r) {
+  int t = lg[r - l + 1];
+  int x = p[t][l], y = p[t][r - (1 << t) + 1];
+  return a[x] < a[y] ? y : x;
+}
+
+void Update(int x, LL y) {
+  if (d[x].second < t) {
+    d[x] = {0, t};
+  }
+  d[x].first += y;
+  if (d[x].first > w) {
+    ans = x, w = d[x].first;
+  }
+}
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
-  cin >> u;
-  while (u--) {
-    m = hs = 0, h.clear();
-    cin >> n >> o;
+  p[0][1] = 1;
+  for (int i = 2; i < kMaxN; i++) {
+    lg[i] = lg[i >> 1] + 1, p[0][i] = i;
+  }
+  cin >> o;
+  while (o--) {
+    cin >> n >> m;
     for (int i = 1; i <= n; i++) {
-      cin >> a[i], h[a[i]] = 0;
+      cin >> a[i];
     }
-    for (int i = 1; i <= o; i++) {
-      cin >> f[i].first >> f[i].second;
-      mx[i] = ans[i] = 0, w[i] = 1LL * (f[i].second - f[i].first + 1) * (f[i].second - f[i].first + 2) / 2;
-    }
-    for (auto &i : h) {
-      i.second = ++m, r[m] = i.first;
-    }
-    for (int i = 1; i <= n; i++) {
-      a[i] = h[a[i]];
-    }
-    for (int i = 1; i <= n; i++) {
-      p[i] = a[i] > max(m - kMaxM, 0) ? i : p[i - 1];
-    }
-    for (int i = n; i >= 1; i--) {
-      q[i] = a[i] > max(m - kMaxM, 0) ? i : q[i + 1];
-    }
-    for (int i = 1; i <= n; i++) {
-      if (a[i] > max(m - kMaxM, 0)) {
-        t[i] = t[p[i]] + 1LL * (i - p[i] - 1) * (i - p[i]) / 2;
-      }
-    }
-    for (int i = m; i > max(m - kMaxM, 0); i--) {
+    for (int i = 1; i < kMaxL; i++) {
       for (int j = 1; j <= n; j++) {
-        pt[j] = pt[j - 1];
-        a[j] >= i && (pt[j] = j);
-      }
-      qt[n + 1] = n + 1;
-      for (int j = n; j >= 1; j--) {
-        qt[j] = qt[j + 1];
-        a[j] >= i && (qt[j] = j);
-      }
-      for (int j = 1; j <= n; j++) {
-        if (a[j] >= i) {
-          int k = pt[j - 1];
-          s[j] = s[k] + 1LL * (j - k - 1) * (j - k) / 2;
-        }
-      }
-      for (int j = 1, l, r; j <= o; j++) {
-        static LL ww;
-        l = f[j].first, r = f[j].second;
-        int _l = qt[l], _r = pt[r];
-        if (_l <= _r) {
-          ww = s[_r] - s[_l];
-          ww += 1LL * (_l - l) * (_l - l + 1) / 2;
-          ww += 1LL * (r - _r) * (r - _r + 1) / 2;
-          if (w[j] - ww > mx[j]) {
-            mx[j] = w[j] - ww, ans[j] = i;
-          }
-          w[j] = ww;
-        }
-      }
-    }
-    for (int i = 1, l, r; i <= o; i++) {
-      static LL ww;
-      l = f[i].first, r = f[i].second;
-      if (q[l] <= p[r]) {
-        w[i] = t[p[r]] - t[q[l]];
-        w[i] += 1LL * (r - p[r]) * (r - p[r] + 1) / 2;
-        w[i] += 1LL * (q[l] - l) * (q[l] - l + 1) / 2;
-      } else {
-        w[i] = 1LL * (r - l + 1) * (r - l + 2) / 2;
-      }
-      if (w[i] > mx[i]) {
-        for (int j = l; j <= r; j++) {
-          v[a[j]].push_back(j);
-        }
-        for (int j = m; j >= 1; j--) {
-          if (!v[j].empty()) {
-            int c = l - 1;
-            ww = 0, v[j].push_back(r + 1);
-            for (int k : v[j]) {
-              ww += 1LL * (k - c - 1) * (k - c) / 2, c = k;
-            }
-            if (w[i] - ww > mx[i]) {
-              mx[i] = w[i] - ww, ans[i] = j;
-            }
-            w[i] = ww, v[j].clear();
+        p[i][j] = p[i - 1][j];
+        if (j + (1 << i - 1) <= n) {
+          if (a[p[i][j]] < a[p[i - 1][j + (1 << i - 1)]]) {
+            p[i][j] = p[i - 1][j + (1 << i - 1)];
           }
         }
       }
-      hs ^= 1LL * i * ::r[ans[i]];
     }
-    cout << hs << '\n';
+    for (int i = 1, l, r; i <= m; i++) {
+      cin >> l >> r;
+      ans = w = 0, t++, priority_queue<V>().swap(q);
+      q.push({l, r, Max(l, r)});
+      for (LL s = 1LL * (r - l + 1) * (r - l + 2) / 2, c; s > w;) {
+        for (int t = a[q.top().p]; a[q.top().p] == t && (ans != t || s > w);) {
+          v = q.top(), q.pop(), c = v.Calc(), s -= c, Update(a[v.p], c);
+          if (v.p > v.l) {
+            q.push({v.l, v.p - 1, Max(v.l, v.p - 1)});
+          }
+          if (v.p < v.r) {
+            q.push({v.p + 1, v.r, Max(v.p + 1, v.r)});
+          }
+        }
+      }
+      h ^= 1LL * i * ans;
+    }
+    cout << h << '\n', h = 0;
   }
   return 0;
 }
