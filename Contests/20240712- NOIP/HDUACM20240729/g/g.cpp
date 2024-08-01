5d57a6c331aa1f0ea2bdb02c8baf1be96a0bda15
dengyufan2008 <2695375004@qq.com>
2024-08-01 19:09:40 +0800

    .

--- Contests/20240712- NOIP/HDUACM20240729/g/g.cpp
+++ Contests/20240712- NOIP/HDUACM20240729/g/g.cpp
@@ -1,13 +1,58 @@
+#include <algorithm>
+#include <cmath>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 ifstream cin("g.in");
 ofstream cout("g.out");
 
+const int kMaxN = 2.5e5 + 1;
+int t, n, m, w, a[kMaxN], b[kMaxN], l[kMaxN];
+long long ans;
+vector<int> v, s, _s;
+
+void Update(int &x, int y) {
+  ans -= x, x = max(x, y), ans += x;
+}
+
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
-  
+  cin >> t;
+  while (t--) {
+    ans = 0, s.clear(), v.clear();
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+      cin >> a[i];
+    }
+    for (int i = 1; i <= n; i++) {
+      cin >> b[i];
+    }
+    for (int i = 1; i <= n; i++) {
+      ans += a[i], l[i] = i;
+    }
+    sort(l + 1, l + n + 1, [](int i, int j) {
+      return b[i] > b[j];
+    });
+    w = b[l[(int)sqrt(n)]];
+    for (int i = 1; i <= n; i++) {
+      a[i] <= w ? s.push_back(i) : void();
+      b[i] > w ? v.push_back(i) : void();
+    }
+    for (int i = 1, x; i <= m; i++) {
+      cin >> x;
+      for (int j : s) {
+        Update(a[j], b[j + x > n ? j + x - n : j + x]);
+        a[j] <= w ? _s.push_back(j) : void();
+      }
+      s.swap(_s), _s.clear();
+      for (int j : v) {
+        Update(a[j - x < 1 ? j - x + n : j - x], b[j]);
+      }
+      cout << ans << '\n';
+    }
+  }
   return 0;
 }
---
+++ Resources/cos & tan.md
@@ -0,0 +1,22 @@
+$$
+\mu = \begin{cases}
+\frac{1}{\tan \theta} \space (\theta \in [0, \pi] \setminus \{\frac{\pi}{2}\})
+\\
+-\frac{1}{\tan \theta} \space (\theta \in [\pi, 2\pi] \setminus \{\frac{3\pi}{2}\})
+\end{cases}
+$$
+
+---
+
+$$\frac{\mu}{\sqrt{1+\mu^2}} = \frac{\frac{1}{\tan \theta}}{\frac{\sqrt{\tan^2 \theta+1}}{\tan \theta}}$$
+$$\space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space \space = \frac{1}{\sqrt{\frac{\sin^2 \theta+\cos^2 \theta}{\cos^2 \theta}}}$$
+$$\space \space \space \space \space \space \space \space \space = \cos \theta$$
+
+---
+
+$$\cos^2 \theta = \frac{\sin^2 \theta}{\tan^2 \theta} = \frac{1-\cos^2 \theta}{\tan^2 \theta}$$
+$$(\tan^2 \theta+1)\cos^2 \theta = 1$$
+$$\cos \theta = \sqrt{\frac{1}{\tan^2 \theta + 1}}$$
+$$\space \space \space \space \space \space \space \space = \frac{1}{\sqrt{\tan^2 \theta+1}}$$
+$$\space \space \space \space \space \space \space \space \space \space \space \space \space \space \space = \frac{1}{\tan \theta \sqrt{1+\frac{1}{\tan^2 \theta}}}$$
+$$\space \space = \frac{\mu}{\sqrt{1+\mu^2}}$$
