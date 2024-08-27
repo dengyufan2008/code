8b74887aaa4da31a8207f5e91242025d7e53f884
dengyufan2008 <2695375004@qq.com>
2024-08-27 10:22:10 +0800

    。

--- Contests/20240712- NOIP/HDUACM20240818/h/h.cpp
+++ Contests/20240712- NOIP/HDUACM20240818/h/h.cpp
@@ -1,13 +1,20 @@
-#include <fstream>
-
+// by wls
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
-
-ifstream cin("h.in");
-ofstream cout("h.out");
-
-int main() {
+const int N = 2e5 + 10;
+int n, k;
+signed main() {
+  ios::sync_with_stdio(false);
   cin.tie(0), cout.tie(0);
-  ios::sync_with_stdio(0);
-  
+  int T;
+  cin >> T;
+  while (T--) {
+    cin >> n >> k;
+    if (n == k || (k == 1 && (n & 1))) {
+      cout << "A";
+    } else
+      cout << "B";
+  }
   return 0;
 }
--- Contests/20240712- NOIP/HDUACM20240818/j/j.cpp
+++ Contests/20240712- NOIP/HDUACM20240818/j/j.cpp
@@ -1,13 +1,32 @@
-#include <fstream>
-
+// by lsl
+#include <bits/stdc++.h>
+#define ll unsigned int
 using namespace std;
-
-ifstream cin("j.in");
-ofstream cout("j.out");
-
-int main() {
-  cin.tie(0), cout.tie(0);
+const int N = 3e5 + 10;
+ll a[N], b[N];
+ll ans;
+int q, TE;
+ll check(ll x, int p) {
+  for (int i = 1; i <= q; i++) x = ((a[i] ^ x) + (b[i] ^ x)) % (1ll << (p + 1));
+  return x;
+}
+void work() {
+  ans = 0;
+  cin >> q;
+  for (int i = 1; i <= q; i++) cin >> a[i] >> b[i];
+  for (int i = 0; i < 32; i++) {
+    if (check(ans, i) == ans) continue;
+    ans += (1ll << i);
+  }
+  for (int i = 1; i <= q; i++) {
+    ans = ((a[i] ^ ans) + (b[i] ^ ans));
+    cout << ans << '\n';
+  }
+}
+signed main() {
   ios::sync_with_stdio(0);
-  
+  cin.tie(0), cout.tie(0);
+  cin >> TE;
+  while (TE--) work();
   return 0;
 }
