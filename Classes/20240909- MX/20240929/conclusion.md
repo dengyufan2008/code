c3e71f9cafc9e035642852fd1bc65c5f54e05fe9
dengyufan2008 <2695375004@qq.com>
2024-09-29 21:43:06 +0800

    .

--- Classes/20240909- MX/20240929/conclusion.md
+++ Classes/20240909- MX/20240929/conclusion.md
@@ -2,4 +2,4 @@
 
 - [reminder] T2 如 20240228 T2.
 
-- [reminder] T9 [未使用滚动数组, TLE](https://atcoder.jp/contests/arc184/submissions/58274623) & [使用滚动数组, AC](https://atcoder.jp/contests/arc184/submissions/58274888).
+- [reminder] T9 [未使用滚动数组, TLE](https://atcoder.jp/contests/arc184/submissions/58274623) & [使用滚动数组, AC](https://atcoder.jp/contests/arc184/submissions/58275675).
--- Classes/20240909- MX/20240929/i/i.cpp
+++ Classes/20240909- MX/20240929/i/i.cpp
@@ -15,25 +15,22 @@ int Calc(int x) {
   if (px == x) {
     return pans;
   }
-  int n = log2(x) / log2(3) + 1, o = 1;
-  f[0] = 0, fill(&f[1], &f[1 << n], kInf);
+  int n = log2(x) / log2(3) + 1, nn = 1 << n, o = 1;
+  f[0] = 0, fill(&f[1], &f[nn], kInf);
   for (int i = x; i; i /= 2) {
-    for (int j = i, r = 0; j; j /= 3, r++) {
-      for (int s = 0; s < 1 << n; s++) {
-        if (s >> r & 1 ^ 1) {
-          int f0 = f[s], f1 = f[s | 1 << r];
-          f[s | 1 << r] = min(f0, f1) + 1;
-          if (r && (s >> r - 1 & 1)) {
-            f[s] = min(f0, f1);
-          } else {
-            f[s] = f1;
-          }
+    for (int j = i, r = 0, rr = 1; j; j /= 3, r++, rr <<= 1) {
+      for (int s = rr; s < nn; s = s + 1 | rr) {
+        f[s ^ rr] = min(f[s ^ rr], f[s]);
+        if (s >> r - 1 & 1) {
+          f[s] = f[s ^ rr] + 1;
+        } else {
+          swap(f[s], ++f[s ^ rr]);
         }
       }
     }
   }
   px = x, pans = kInf;
-  for (int s = 0; s < 1 << n; s++) {
+  for (int s = 0; s < nn; s++) {
     pans = min(pans, f[s]);
   }
   return pans;
@@ -43,10 +40,10 @@ int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
   cin >> m;
-  for (int i = 1, j = 5; i <= m; i += 6, j += 6) {
+  for (int i = 1; i <= m; i += 6) {
     ans += Calc(i);
-    if (j <= m) {
-      ans += Calc(j);
+    if (i + 4 <= m) {
+      ans += Calc(i + 4);
     }
   }
   cout << ans << '\n';
