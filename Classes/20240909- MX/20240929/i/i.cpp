4bb9b8d64328ef43f82082ec333ff47631bc0054
dengyufan2008 <2695375004@qq.com>
2024-09-29 21:49:17 +0800

    .

--- Classes/20240909- MX/20240929/i/i.cpp
+++ Classes/20240909- MX/20240929/i/i.cpp
@@ -10,12 +10,17 @@ const int kMaxN = 19, kInf = 1e9;
 int m, ans, f[1 << kMaxN];
 
 int Calc(int x) {
-  static int px = 0, pans = 0;
-  x = m / x;
-  if (px == x) {
+  static int pc = 0, pans = 0, c;
+  x = m / x, c = 0;
+  for (int i = x; i; i /= 2) {
+    for (int j = i; j; j /= 3) {
+      c++;
+    }
+  }
+  if (pc == c) {
     return pans;
   }
-  int n = log2(x) / log2(3) + 1, nn = 1 << n, o = 1;
+  int n = log2(x) / log2(3) + 1, nn = 1 << n;
   f[0] = 0, fill(&f[1], &f[nn], kInf);
   for (int i = x; i; i /= 2) {
     for (int j = i, r = 0, rr = 1; j; j /= 3, r++, rr <<= 1) {
@@ -29,7 +34,7 @@ int Calc(int x) {
       }
     }
   }
-  px = x, pans = kInf;
+  pc = c, pans = kInf;
   for (int s = 0; s < nn; s++) {
     pans = min(pans, f[s]);
   }
