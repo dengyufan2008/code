be05506824e3862735d0edf8980f25a1372dfbaa
dengyufan2008 <2695375004@qq.com>
2024-09-30 21:51:34 +0800

    .

--- Classes/20240623-20240714 WHK2/q/q.cpp
+++ Classes/20240623-20240714 WHK2/q/q.cpp
@@ -6,11 +6,16 @@ using namespace std;
 ifstream cin("q.in");
 ofstream cout("q.out");
 
-// const int kMaxN = 301, kMod = 1e9 + 7;
-const int kMaxN = 5, kMod = 1e9 + 7;
+const int kMaxN = 301, kMod = 1e9 + 7;
 int n, m, p, a[kMaxN];
-LL ans, f[kMaxN][kMaxN], g[kMaxN][kMaxN], h[kMaxN][kMaxN];
 LL fact[kMaxN], _fact[kMaxN];
+LL f[kMaxN][kMaxN];  // i 个点实线连成 j 棵树使得每棵树不超过 m 且最后用虚线连成 1 棵树的方案数
+LL g[kMaxN][kMaxN];  // g_{i, 1} 为容斥后 i 个点连成 1 棵树的方案数 \ 
+                        容斥的效果为若干 g_{i, 1} 分别以 i 个标号再次连成 1 棵树的方案数恰好为 f_{i, 1} \
+                        g_{i, j} 为 i 个点 j 个 g_{x, 1} 的并集的方案数 (即 Exp) \
+                        且为了之后统计答案每个 g_{x, 1} 带 x 的权
+LL h[kMaxN][kMaxN];  // 考虑了颜色 i, 形成了 j 个 g_{x, 1} 的并集的方案数, 同样带权
+LL ans;              // 将 h_{?, i} 的 i 个 g_{x, 1} 连成 1 棵树, 统计答案
 
 LL Pow(LL x, int y = kMod - 2) {
   LL ans = 1;
@@ -23,6 +28,8 @@ LL Pow(LL x, int y = kMod - 2) {
   }
   return ans;
 }
+
+LL C(int x, int y) { return fact[x] * _fact[y] % kMod * _fact[x - y] % kMod; }
 
 void Add(LL &x, LL y) { x = (x + y) % kMod; }
 
@@ -41,12 +48,13 @@ int main() {
   for (int i = 1, x; i <= n; i++) {
     cin >> x, a[x]++;
   }
-  f[0][0] = 1;
   for (int i = 1; i <= m; i++) {
-    LL w = Pow(i, i - 1);
-    for (int j = 0; j + i <= n; j++) {
-      for (int k = 0; k < n; k++) {
-        Add(f[j + i][k + 1], f[j][k] * w);
+    f[i][1] = Pow(i, i - 1);
+  }
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j <= i; j++) {
+      for (int k = 1; k <= m && i + k <= n; k++) {
+        Add(f[i + k][j + 1], f[i][j] * C(i + k - 1, i) % kMod * f[k][1]);
       }
     }
   }
@@ -61,28 +69,26 @@ int main() {
     }
     g[i][1] = g[i][1] * i % kMod;
   }
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j + i <= n; j++) {
-      for (int k = 1; k < n; k++) {
-        Add(g[j + i][k + 1], g[j][k] * g[i][1]);
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j <= i; j++) {
+      for (int k = 1; i + k <= n; k++) {
+        Add(g[i + k][j + 1], g[i][j] * C(i + k - 1, i) % kMod * g[k][1]);
       }
     }
   }
   h[0][0] = 1;
   for (int i = 1; i <= n; i++) {
     if (a[i]) {
-      for (int j = 1; j <= a[i]; j++) {
-        for (int k = 0; k <= n - j; k++) {
-          Add(h[i][k + j], h[p][k] * g[a[i]][j]);
+      for (int j = 0; j <= n; j++) {
+        for (int k = 1; k <= a[i] && j + k <= n; k++) {
+          Add(h[i][j + k], h[p][j] * g[a[i]][k]);
         }
-        g[a[i]][j];
       }
       p = i;
     }
   }
   for (int i = 1; i <= n; i++) {
-    h[p][i] = h[p][i] * Pow(n, i - 2) % kMod;
-    Add(ans, h[p][i]);
+    Add(ans, h[p][i] * Pow(n, i - 2));
   }
   cout << ans << '\n';
   return 0;
