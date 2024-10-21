a1ee8cb95278f603b07c078deac5a43850822a5d
dengyufan2008 <2695375004@qq.com>
2024-10-21 20:44:08 +0800

    .

---
+++ Classes/20240623-20240714 WHK2/u/u.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("u.in");
+ofstream cout("u.out");
+
+int t, n, x, y, z, a, b, h;
+
+int Gcd(int x, int y) {
+  return y ? Gcd(y, x % y) : x;
+}
+
+int main() {
+  cin.tie(0), cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin >> t;
+  while (t--) {
+    cin >> y >> n >> x >> h, y %= x;
+    if (y == 0) {
+      cout << "YES\n";
+      continue;
+    } else if (y * 2 < x && n < x / y) {
+      cout << (h >= y ? "YES\n" : "NO\n");
+      continue;
+    }
+    a = 1, b = 0, z = Gcd(x, y);
+    if (n >= x / z) {
+      cout << (h >= z ? "YES\n" : "NO\n");
+      continue;
+    }
+    for (;;) {
+      int w = x / y;
+      if (n >= w * a) {
+        x -= w * y, b += w * a, n -= w * a;
+        swap(x, y), swap(a, b);
+      } else {
+        w = n / a, x -= w * y, b += w * a;
+        cout << (h >= x ? "YES\n" : "NO\n");
+        break;
+      }
+    }
+  }
+  return 0;
+}
