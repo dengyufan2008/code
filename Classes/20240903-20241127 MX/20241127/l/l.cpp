11d39b8033fef9fd90e3f6bf8adac47bd807500d
dengyufan2008 <2695375004@qq.com>
2024-12-20 11:28:53 +0800

    .

--- Classes/20240903-20241127 MX/20241127/l/l.cpp
+++ Classes/20240903-20241127 MX/20241127/l/l.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#define LL long long
 
 using namespace std;
 
@@ -6,12 +7,30 @@ ifstream cin("l.in");
 ofstream cout("l.out");
 
 const int kMaxN = 2e5 + 1, kMod = 998244353;
-int n, m, q;
+LL n, m, q, _n, _m, inv_n, inv_m, ans;
+
+LL Pow(LL x, int y = kMod - 2) {
+  LL ans = 1;
+  for (int i = 1; i <= y; i <<= 1) {
+    if (i & y) {
+      ans = ans * x % kMod;
+    }
+    x = x * x % kMod;
+  }
+  return ans;
+}
 
 int main() {
   cin.tie(0), cout.tie(0);
   ios::sync_with_stdio(0);
   cin >> n >> m >> q;
-  ;
+  _n = n * (n + 1) / 2 % kMod, _m = m * 2 + 1;
+  inv_n = Pow(_n), inv_m = Pow(_m);
+  for (int j = 1; j <= n; j++) {
+    LL p = j * (n - j + 1) % kMod * inv_n % kMod;
+    ans = (ans + _m * (Pow(1 - m * inv_m % kMod * p % kMod, q) - 1) + m * q % kMod * p) % kMod;
+  }
+  ans = ans * (m - 1) % kMod * Pow(m * 2) % kMod * inv_m % kMod;
+  cout << (ans + kMod) * Pow(_n * _m % kMod, q) % kMod << '\n';
   return 0;
 }
